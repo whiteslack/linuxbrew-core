4fb151aa9a756a7821e8760c93a8b18db55886aa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:48:38 +0000

    thefuck: update 3.30_1 bottle.

--- Formula/thefuck.rb
+++ Formula/thefuck.rb
@@ -13,6 +13,7 @@ class Thefuck < Formula
     sha256 "9101f88f96d66615970b00405f06430176fb827922363530abb33f3d22341ff3" => :catalina
     sha256 "fde50d03f97cf18dbdf89d86469f7c7c2794d40e639ad3c2c926466b98e455ee" => :mojave
     sha256 "272cc963d34074daf27ca24dcb16f7f2a47aaafe955ff0e259dd38191274d1d1" => :high_sierra
+    sha256 "56abac8e520f4e3608ba77cb3e230a9aae4cb9429864fe46deb1ce8ae401d462" => :x86_64_linux
   end
 
   depends_on "python@3.8"
