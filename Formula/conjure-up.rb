5ce9a676d70d336f151b69924d31b3de227110de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:41:07 +0000

    conjure-up: update 2.6.9_4 bottle.

--- Formula/conjure-up.rb
+++ Formula/conjure-up.rb
@@ -12,6 +12,7 @@ class ConjureUp < Formula
     sha256 "e9e7df0108dd3be03a9391a95befbbfa66950bbbb48ae8fd28f4855ae6c69932" => :catalina
     sha256 "dc221c6071b5a37760a530da1981a96a1f404fa461d44fee7a217897e9a01077" => :mojave
     sha256 "b20ebbe1aa0c30c713e58c03ac167196f7c3b5635f95c1ce8687e6bfad9a0294" => :high_sierra
+    sha256 "abcb88b800c672924ce6f4666e3f3261480a4b373076f55e228b7e1be63e5bc9" => :x86_64_linux
   end
 
   depends_on "awscli"
