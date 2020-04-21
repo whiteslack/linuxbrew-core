2acaf3cde22b017ac10f466a8a3d9b4276d49151
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-21 18:43:09 +0000

    gprof2dot: update 2019.11.30_3 bottle.

--- Formula/gprof2dot.rb
+++ Formula/gprof2dot.rb
@@ -13,6 +13,7 @@ class Gprof2dot < Formula
     sha256 "974b8737cdc5d07b94c0292b27e015085ab8475757ea95b6cb42067fcafd5418" => :catalina
     sha256 "c7ff44d15b136ccc9618d0f1e2363a75983bc6587924826e00fb283099edf235" => :mojave
     sha256 "5626ffada5afc1e44178f1da5a4e041d733f6abe684e666de9679a791247d224" => :high_sierra
+    sha256 "c35ecf522c1c7d33980e2d1ca43f3b88b7635eafb506e308f7dd8ddea9e86830" => :x86_64_linux
   end
 
   depends_on "graphviz"
