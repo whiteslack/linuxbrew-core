cf866eceb97c27d1ea23f6dab6ac1965eeacb7c1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-23 21:00:35 +0000

    babeld: update 1.9.2 bottle.

--- Formula/babeld.rb
+++ Formula/babeld.rb
@@ -10,6 +10,7 @@ class Babeld < Formula
     sha256 "d65c7dd41ac16cb2f791a80f3cbebdb6616321e106874504644b0ab5cd37da24" => :catalina
     sha256 "14f512383b868d8c9752414328fd4681de70d6aa37992cdcb55be61406bcb08a" => :mojave
     sha256 "6b920612afb160b31950f28dad5b38880689cb3f52a23be723e8dd680370fca8" => :high_sierra
+    sha256 "bb268a068c9424beeddb9933a57aa9593c6cd4ffceb58dab58d14368509081eb" => :x86_64_linux
   end
 
   def install
