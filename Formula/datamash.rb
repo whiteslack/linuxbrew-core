664d7fd0ceaa4faea2abe8b6e5efd5483e71401b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-24 19:20:17 +0000

    datamash: update 1.7 bottle.

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -9,6 +9,7 @@ class Datamash < Formula
     sha256 "f592c4bda737ef924fb4c1642fb381db54c9ce246eb51d03a145dd28a8391406" => :catalina
     sha256 "6533f0decc607d6e3ce1ad1fdb7f5b30f99bbbcbacbba1bcd880486eef648189" => :mojave
     sha256 "b6100d066c3cf9d91b2bd4a8d8bcdc5fa453c6eb6a28d7cacb06659baa358e46" => :high_sierra
+    sha256 "b6f4deeb3882c4a6cd44f5b7be736a12c38d6a2ed810d6bf376904804beddfa1" => :x86_64_linux
   end
 
   head do
