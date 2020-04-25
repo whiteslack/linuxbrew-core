757f91d3fb2023d789333355402b8639134045fc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-25 07:07:24 +0000

    nift: update 2.3.5 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -9,6 +9,7 @@ class Nift < Formula
     sha256 "59c78ce930e5fd55149cd8ad8fe6b2ea57364d6ae038db4e08d4242e49847ff5" => :catalina
     sha256 "54054a65d96046bde9d901a9f140e96752f0a3c7f14e73e05e90e40cc906283a" => :mojave
     sha256 "1d314159a7133ab707da24005fb2a9edb81bc05d5b8b9649c8a16909b0fd0ac0" => :high_sierra
+    sha256 "a2b69b03fd8aaff9dc56521f32ca21488fd03fe7405317ccdb028343c82dd589" => :x86_64_linux
   end
 
   depends_on "luajit" unless OS.mac?
