276023c98ce829fd28028d1aef3f8775c5bc0b24
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-26 22:11:47 +0000

    mkl-dnn: update 1.4 bottle.

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -9,6 +9,7 @@ class MklDnn < Formula
     sha256 "28d3e88f803aecac82aaf70c6fb5f9ec5372ce68446bb71cd38e75edc5fa97b8" => :catalina
     sha256 "70128d845c6d9ca4c9d2798bed1c2f5ed19cb73e4da841e4f7d8e06462eca5fb" => :mojave
     sha256 "c3a25f4b25ff821a136cf9c7eee62e38185314a287bc9fcff2596b7c1f31dcfc" => :high_sierra
+    sha256 "0396b701a13789370aae86e02ca78d88a3858b512f2a45855cb3672bc4c00a70" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
