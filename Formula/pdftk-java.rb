5c48463546cdb064cb7c0aa5ea50d744b89dd698
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-27 07:20:41 +0000

    pdftk-java: update 3.1.0 bottle.

--- Formula/pdftk-java.rb
+++ Formula/pdftk-java.rb
@@ -10,6 +10,7 @@ class PdftkJava < Formula
     sha256 "9f2bad4b0697701837747055309581a2f722140e619f8211e8af3b60399311d0" => :catalina
     sha256 "86540c3aea1b817a1423e3c7b8c1af0e1c01d2579f4f8bf22aaaa7fe2da5602b" => :mojave
     sha256 "05fc4aff36077b66e3bc768c7dddb309e45d976f3a996a75a9191c8a88f8c0f6" => :high_sierra
+    sha256 "f85c0ae900231235b959fc3e77b22fd3640410a119ac0dd4c8f97dbe44c98c42" => :x86_64_linux
   end
 
   depends_on "gradle" => :build
