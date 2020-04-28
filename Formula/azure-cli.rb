9370fb2551eeca7b6643deb83e507b458e07e7df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-28 20:58:57 +0000

    azure-cli: update 2.5.0 bottle.

--- Formula/azure-cli.rb
+++ Formula/azure-cli.rb
@@ -13,6 +13,7 @@ class AzureCli < Formula
     sha256 "61a368e63dfe60c5847dc668812a927d53a9de70524ee7efb2a348bceb823df1" => :catalina
     sha256 "ea4e52bff8f730eba24c8f88e51503d697914ef565b6503e68d24c6916a596c2" => :mojave
     sha256 "4d0383061da5cc1e18a11b4a83a718b4907396a541dc83e7d4df5e53ace11b73" => :high_sierra
+    sha256 "b7b385ec2c544915c1da232ab625c9c7fd39828c62b977052f955d34bd08e21c" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
