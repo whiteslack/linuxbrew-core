ab12abd0008212c175380bfcaae65e41cb02f665
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-28 06:05:55 +0000

    cups: update 2.3.3 bottle.

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -8,6 +8,7 @@ class Cups < Formula
     sha256 "1bed38f7aa9ee2c76fed5393e308cf7de5dd6d443debb0455ee7739e010b0e20" => :catalina
     sha256 "19ea09cef986e1c66f1736409f4e7917914100bb68525d8118e705500fbf28e0" => :mojave
     sha256 "0d3328bddece686cfa54e63b61f68c68fed1b1f8553cd45ed194d8a80e56f9c9" => :high_sierra
+    sha256 "0a7ab2887648d19d46f47447bb7e1545fb2ecc5438dc7df4deaa79b7fbb4f510" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
