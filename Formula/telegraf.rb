785b6ee0fc6aeb6fbf6b45b9194b1a161b7a7764
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-29 15:21:21 +0000

    telegraf: update 1.14.2 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -10,6 +10,7 @@ class Telegraf < Formula
     sha256 "292f04fec61657c5fcc095b34fd6ee6d74c2a26ee2692bbe08bd135b475413aa" => :catalina
     sha256 "90e5f9f62569e961490c967111121b7b9b23c5bea0c43523ccc276e0d3902f80" => :mojave
     sha256 "32c510b90b627270659ae8654f8a4fd7a6fa0cfeab1960ce55ceb872fc7a4ac6" => :high_sierra
+    sha256 "235d18aa01f77ca78cb680a3fdcc3c39b1c354ba529ecf7f32581b615c3b7cbc" => :x86_64_linux
   end
 
   depends_on "go" => :build
