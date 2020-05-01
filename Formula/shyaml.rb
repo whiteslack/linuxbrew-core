588f2352279e6ce12da264095b739029eeca606f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-01 21:03:17 +0000

    shyaml: update 0.6.1_2 bottle.

--- Formula/shyaml.rb
+++ Formula/shyaml.rb
@@ -13,6 +13,7 @@ class Shyaml < Formula
     sha256 "510026ded6fc62625ee9bd2cab4d94d03155e94270ae0aa38947b894282c2808" => :catalina
     sha256 "c850436b06215814c879e02db80331c3b94e63ccfa03df8bd25915ef4dfac668" => :mojave
     sha256 "d52705a99a5f376d41d2ead578d73f5eb309b0efa595215364ea42cb4191eed6" => :high_sierra
+    sha256 "6868de73b9fedf20e0d1fd59cb17a52419b984823b52bc1545a796876e17c7be" => :x86_64_linux
   end
 
   depends_on "libyaml"
