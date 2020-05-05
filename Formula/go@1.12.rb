15e5278b2b3d3e849deb885f3898ba573036c9c6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-05 05:54:46 +0000

    go@1.12: update 1.12.17 bottle.

--- Formula/go@1.12.rb
+++ Formula/go@1.12.rb
@@ -10,6 +10,7 @@ class GoAT112 < Formula
     sha256 "44d6c83a39c231cae86af05b3689cb2ec03be389562c1cc5e84a9f68ed09af80" => :catalina
     sha256 "dc3b90a9ba13c31928c92227957d530656bab53d5ca3a35bfab02038118a964e" => :mojave
     sha256 "e42c02a42a4d2df97cba11e80729e8439e7476745548bd7ee23d72858c22a3f1" => :high_sierra
+    sha256 "c7e6efda32a76006aa9caa222311d6926896b6fad320f0e7d33ffc3c1b51e347" => :x86_64_linux
   end
 
   keg_only :versioned_formula
