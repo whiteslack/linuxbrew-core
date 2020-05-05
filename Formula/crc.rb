2e94a087a1b73ba7551562dc5c0a18884e37f914
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-05 21:32:32 +0000

    crc: update 1.10.0 bottle.

--- Formula/crc.rb
+++ Formula/crc.rb
@@ -11,6 +11,7 @@ class Crc < Formula
     sha256 "14180b123d70eb87176d98d4953b3a6f9965ffaf789be18643bbeaed9dd26a0e" => :catalina
     sha256 "8dfd8898768fef9aa17d6d3348aefad034a678ed7c3f0d162ebae696041c920b" => :mojave
     sha256 "cd8bc4a64283ed6e3dd0a52d35f196d9f690f555671390de00a4fe0c95dbf767" => :high_sierra
+    sha256 "655e5a5582c3e7a694856aec6c2ccaf899216f63167571bc41f85f3ceaa0b375" => :x86_64_linux
   end
 
   depends_on "go" => :build
