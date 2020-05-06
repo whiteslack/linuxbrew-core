db91362b341aae833b26545e1d121f6c1f414316
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-06 06:10:06 +0000

    cargo-c: update 0.6.5 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -9,6 +9,7 @@ class CargoC < Formula
     sha256 "b677282c113016d5a2292870ff316daa99dcee19adbbbfd466340f88a66e0794" => :catalina
     sha256 "2e781da01fa6295a8cb8ad4240bccdc0148af0dfd712475d26e373a19493c758" => :mojave
     sha256 "89570707a2c183c68c78b146e4d77d1c58ca323be2455b3a31f5e60624b06138" => :high_sierra
+    sha256 "96d09fad3dad4d48a1ab1d86ce5b7c85387382842e7ab7f8c906ace296c70a65" => :x86_64_linux
   end
 
   depends_on "rust" => :build
