860b32dcfd6a5baea52daab2c8d7ea0ceca0a6e8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-06 21:15:21 +0000

    rargs: update 0.3.0 bottle.

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -9,6 +9,7 @@ class Rargs < Formula
     sha256 "37d5a3c2a5608eb4a10df0814a1334b88602a7200fdf99db60113f7aea598489" => :catalina
     sha256 "1c24f60f8b91301cd167b0040e2c9ec7895fe818eeb21f13d40fca94e6f4f08b" => :mojave
     sha256 "9cea3ec1abc342281b94649496e0d28275eead691238a2d03e47c2621afc9801" => :high_sierra
+    sha256 "25eba46b8fd020e3428667688f69136105c3dd92eff27b399e9cda3c61754127" => :x86_64_linux
   end
 
   depends_on "rust" => :build
