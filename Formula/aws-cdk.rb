4cbce6f150dba173256210835a935248317beb33
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-07 21:50:54 +0000

    aws-cdk: update 1.37.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -11,6 +11,7 @@ class AwsCdk < Formula
     sha256 "8ecaece6b48f9eb2eb06aec0cad1880ee5e132af81def418e8a0ee29baa57e42" => :catalina
     sha256 "51207568ac19f589ae524082de4148b6ce30133684be119a3221645d5d105422" => :mojave
     sha256 "69647bb1c4126c5a775afd47607ab2d43b3d058e80dc29e8dc74b2cece2a4ca1" => :high_sierra
+    sha256 "ad2941a9b9d6a2a97a58d926c4ca2f0f2ad246be15d47a059cf5f8c3168e38e8" => :x86_64_linux
   end
 
   depends_on "node"
