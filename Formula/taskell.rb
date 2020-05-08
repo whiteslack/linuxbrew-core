4e779456ef43cf04e9d06e80f08ea176d64a0456
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-08 14:48:23 +0000

    taskell: update 1.9.3 bottle.

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -14,6 +14,7 @@ class Taskell < Formula
     sha256 "2f049bf834c2ab324f6413c8ab99e10fb4c83fd2aa26ef22ace2014e40c653b9" => :catalina
     sha256 "68b52580165c2f43647a7208b9c1c005ad1c349d6de8c7d2f81f8aee3ec5244f" => :mojave
     sha256 "cd5af708aa5738e8e7fe85250f3490a8f8f552d9a385c30b885d6e77795b3cc1" => :high_sierra
+    sha256 "6c2734a0d98761259a41e99482c987ed71d482d737e964f5e7661905d2d9f8a3" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
