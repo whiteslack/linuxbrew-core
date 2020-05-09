78827cda91aee99c2615bf362053289ba86a948b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-09 08:46:05 +0000

    lxc: update 4.1 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -9,6 +9,7 @@ class Lxc < Formula
     sha256 "8c81f98fdefc6872a51f4943592c7e2675652e4bd365ca9e2559b78c190ad7e7" => :catalina
     sha256 "c5a4556c87b59b85b05558f7e05491ec9022cc8efb1b333387f0873f45e1c656" => :mojave
     sha256 "4bc9b9f2328fb0525df2f45e70e0aa91de02d8ce6559d37d0721629ad04c6b4c" => :high_sierra
+    sha256 "d4a429988e0862cc61a8598ab19874122e9586e1fcade87cc5f827bca0bfa790" => :x86_64_linux
   end
 
   depends_on "go" => :build
