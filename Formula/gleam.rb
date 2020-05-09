497d7980c88236d5ba875915943b256b985ba587
Michka Popoff <michkapopoff@gmail.com>
2020-05-09 12:20:41 +0200

    gleam: add missing dependency for Linuxbrew (#20317)

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -15,6 +15,8 @@ class Gleam < Formula
   depends_on "erlang"
   depends_on "rebar3"
 
+  depends_on "pkg-config" => :build unless OS.mac?
+
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
