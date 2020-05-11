600aee15b14b196d90ec854f0165d58b25e85247
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-11 17:35:57 +0000

    jansson: update 2.13.1 bottle.

--- Formula/jansson.rb
+++ Formula/jansson.rb
@@ -9,6 +9,7 @@ class Jansson < Formula
     sha256 "e6a942f77821fd65810d4bc20e6938364a5e40cd7c8510c4b090731573bd0088" => :catalina
     sha256 "587acdadd1ea8bcf22c316f55a32084f530280a7e24f0864e0e420718d0d1b7f" => :mojave
     sha256 "38085c147eb40d58df8a91a44e7544d4ceb248aa25f54bdd8a3b10c1a214d9e9" => :high_sierra
+    sha256 "743d1b06e991514968c94226924cf14a199d530fc63bd000972e04081d4a6063" => :x86_64_linux
   end
 
   def install
