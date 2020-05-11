819d86bb12dbfe843270734425e3c57acb745c84
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-11 17:41:59 +0000

    xrootd: update 4.12.0 bottle.

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -10,6 +10,7 @@ class Xrootd < Formula
     sha256 "ca8bf41840e9be4351c25f309af3c51075c1c7ec34a8ac1d36e53d5956a3f5d1" => :catalina
     sha256 "43f4f1fbb893a02accef9afbcec430b6045ab88fa25cf3990b38450c47027a42" => :mojave
     sha256 "21f2b97f720a1d70142cdd945130cfcc75b8d5b2f56d867aac527c2485928eb1" => :high_sierra
+    sha256 "6fabfd0b0f349f19dbe8ae2986130ef215b0aeeb0f7bf70301f56ba299d996f4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
