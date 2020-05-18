47b6207a135cb6782d3f9dce47a6db3e8ee8b710
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-18 09:35:53 +0000

    xgboost: update 1.1.0 bottle.

--- Formula/xgboost.rb
+++ Formula/xgboost.rb
@@ -10,6 +10,7 @@ class Xgboost < Formula
     sha256 "7641d5ec78f41c5970703a62d6a06cf75ca8859b98e0d6820119672c4b985942" => :catalina
     sha256 "50e6661f93dc3d8c87801b61e22f1de87fed8e50c92fc724c920035d7bd6724e" => :mojave
     sha256 "fe443009e2572f31ab0d5f0e7138d3f62fbf24f148e12cc7558ae012cf72a242" => :high_sierra
+    sha256 "f9842ddcf7a717939f9e3a4e86e2061fba1fe81931252805d8518788e34f4740" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
