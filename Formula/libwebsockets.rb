1fcee33a63340da8cb93ca2f337cfa947d45c353
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-18 09:34:23 +0000

    libwebsockets: update 4.0.10 bottle.

--- Formula/libwebsockets.rb
+++ Formula/libwebsockets.rb
@@ -9,6 +9,7 @@ class Libwebsockets < Formula
     sha256 "4ae831d568f8f1aa9f8bfe1310effdb495e7e6be2b2a7411da7007e5bd470179" => :catalina
     sha256 "c5a57112ec5d73630fdab62f261ba1861227d1a28c71060aab134f8c9ad43571" => :mojave
     sha256 "8dabeb5415cac60b71f1817688ee608b778879b8b6fc8e03c6448e8954b9706f" => :high_sierra
+    sha256 "240c73ae34fb0428aba030bff6c3532da23dc350bf42e1f1f276affd50649407" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
