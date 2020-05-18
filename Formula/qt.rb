be8cd12ae8744d6be3cb92e01f8b9a62a2e595e8
Michka Popoff <michkapopoff@gmail.com>
2020-05-18 20:35:51 +0200

    qt: disable for CI (#20370)

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -19,6 +19,8 @@ class Qt < Formula
 
   keg_only "Qt 5 has CMake issues when linked"
 
+  disable! if ENV["CI"]
+
   depends_on "pkg-config" => :build
   depends_on :xcode => :build if OS.mac?
   depends_on :macos => :sierra if OS.mac?
