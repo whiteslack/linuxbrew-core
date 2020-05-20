9b3720a80d1dacafaf0aa49271e88190896b8af1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-20 05:57:30 +0000

    goose: update 2.6.0 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -9,6 +9,7 @@ class Goose < Formula
     sha256 "c61983ec470b8ca810e5f4d084ed1c03518281290e0e0f873efd0a703fdd3657" => :catalina
     sha256 "121541b4371c54909eb3d0e3c20c99d60166ce4eab54521e6e8e2a42f0c4e71e" => :mojave
     sha256 "0e8c6ed483b244eac2370dad2d7fa59a6a7f1075305577553aac66463c7b0062" => :high_sierra
+    sha256 "c8de282b2486ea10080e3a21c15c909facd38a77069f907e835a1721d06ac8bb" => :x86_64_linux
   end
 
   depends_on "dep" => :build
