4383ddc69779e86a36e88700897c976f83634213
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-20 17:07:30 +0000

    posh: update 0.14.1 bottle.

--- Formula/posh.rb
+++ Formula/posh.rb
@@ -9,6 +9,7 @@ class Posh < Formula
     sha256 "9a30988f801e9c31ad6fefd48a232a5c95990300eb396a4c32a991176f8350b6" => :catalina
     sha256 "20157fe0e9ff5389d07f85079a3137112cd6ad5bff5081d247e8778a082281c8" => :mojave
     sha256 "bfee90257c267d2bd68ec3501887901179f4464d3e6d5b9afb42580ef1db4677" => :high_sierra
+    sha256 "ea130ddbdedb8a054c9ab270dd4b0ff98cdf4efff8d62f9f90e4792e9252c8b1" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
