eac38d1e4740a994d430be9bfa77240bdd8371ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-21 08:35:03 +0000

    grpc: update 1.28.1_1 bottle.

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -12,6 +12,7 @@ class Grpc < Formula
     sha256 "bdb8519b5595e4d74fc4689d6fa2c97758a4f0194b1a24b4ffb346cb6eb5287a" => :catalina
     sha256 "b2b40661f62b3981e715b59138647495a6ee25e10d1c5bccf7250265df21d33b" => :mojave
     sha256 "b7a71a42560e79a8b8120ab10bb71aa939b2001ece72f408ab62bb46fd332c3a" => :high_sierra
+    sha256 "10edd36b5c94b32c23dadb3bc734dab1a01c8103544ecf40d7227414c149b670" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
