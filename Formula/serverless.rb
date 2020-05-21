320c8d89cf4d55a28d283412d94c1d5864661890
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-21 20:46:47 +0000

    serverless: update 1.71.3 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -11,6 +11,7 @@ class Serverless < Formula
     sha256 "708246ca0d0a053dbd1f2024f3f118a3c2bd5c08109d91c61ae10dbc7a78637e" => :catalina
     sha256 "04e6eae83af9fb6bf8dbf8b334529f269a763350796abcc88589d6d5b48d32f1" => :mojave
     sha256 "f0565923a259c21c34e955c2195d6b8affa9aea43d4ecfea79fc3de0faa2ab07" => :high_sierra
+    sha256 "fcd2151cd0b9c08f67edaa08b24b02b35bd0044a00dc9f322b3e12e1c41822f7" => :x86_64_linux
   end
 
   depends_on "node"
