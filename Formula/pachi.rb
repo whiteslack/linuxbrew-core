3ce8aba4030b24644fff01e413840e6b9ab632b8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-22 12:08:37 +0000

    pachi: update 12.50 bottle.

--- Formula/pachi.rb
+++ Formula/pachi.rb
@@ -10,6 +10,7 @@ class Pachi < Formula
     sha256 "e1cac19564a176a50d27f08b4e395a53ff4144dc17fd93dcaa013adfc8cca83a" => :catalina
     sha256 "76edc1b521dfb93e8c6573b689c8bc5a01103888f2f7310fd46aa53d8b6ea0dc" => :mojave
     sha256 "476509041b907edfd0380bc91a3fd4fc41b359bae58524ab4ab8017df4f61fe0" => :high_sierra
+    sha256 "00166e67158925ae246d80895cc4594c341e58bfe57bfab3ca7652d9e2dcacbe" => :x86_64_linux
   end
 
   fails_with :clang if MacOS.version == :mavericks
