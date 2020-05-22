33cc10806a003639e3f7d8228345910f58f2010d
Michka Popoff <michkapopoff@gmail.com>
2020-05-22 12:35:39 +0200

    Revert "ccls: bump revision for gcc"
    This reverts commit 55f15e6ae2e5ff417acbcac3bd9953f13c5e27f6.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -3,20 +3,20 @@ class Ccls < Formula
   homepage "https://github.com/MaskRay/ccls"
   url "https://github.com/MaskRay/ccls/archive/0.20190823.6.tar.gz"
   sha256 "83dd45120e9674319f91e4379013831e124c0858e050bbc3521e3f8aebe5c95b"
-  revision 1 unless OS.mac?
   head "https://github.com/MaskRay/ccls.git"
 
   bottle do
     sha256 "826b74e4e2f5ed44734b1776e960fb4485bbd20ee6383dc9bc426ca2fed9ba03" => :catalina
     sha256 "f32c94ebdd1b65fa75a32701573dfb1c87d16a339f57f261712ead133caeab3f" => :mojave
     sha256 "fe0edff63df20a3c4624ce335c73276299f60d5bfad836c1acba32ce7435de18" => :high_sierra
+    sha256 "342494b296a491c055769bdcfdaac545951c867897ec7cd418368090cd37216b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   depends_on "rapidjson" => :build
   depends_on "llvm"
   depends_on :macos => :high_sierra # C++ 17 is required
-  depends_on "gcc" unless OS.mac? # C++17 is required
+  depends_on "gcc@9" unless OS.mac? # C++17 is required
 
   fails_with :gcc => "4"
   fails_with :gcc => "5"
