c92ed7aa230453fbf1b15c5616de181b7cac26d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-22 10:32:36 +0000

    postgresql@9.5: update 9.5.22_1 bottle.

--- Formula/postgresql@9.5.rb
+++ Formula/postgresql@9.5.rb
@@ -9,6 +9,7 @@ class PostgresqlAT95 < Formula
     sha256 "9ad944bf158f180a3a3c8771b6f3d179b96b099b9921069d47842126d9a41766" => :catalina
     sha256 "e26219b804f08bbadd8fda68d067d2cf06b50217745baf7929a437727379c5e5" => :mojave
     sha256 "3f64e3cd2e6c8506cc899e2647c059f0f217b1786458e7171ec0d2221a7812ea" => :high_sierra
+    sha256 "6a011b2bc8919c50a1dd4100b4a623870f38a5fe327e20474ce97297022a2e10" => :x86_64_linux
   end
 
   keg_only :versioned_formula
