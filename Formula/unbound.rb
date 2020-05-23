d23e05a5f859bcb8604783e44cec7be684a0b999
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-23 16:57:38 +0000

    unbound: update 1.10.1 bottle.

--- Formula/unbound.rb
+++ Formula/unbound.rb
@@ -9,6 +9,7 @@ class Unbound < Formula
     sha256 "7f66893bc42c776fb8437ae87c27b744fa30ebbea4dc349cb43aa72114b79937" => :catalina
     sha256 "04dfb0c9becb94b0e76ad11ba6b0ba70fae5bf5101533e57ac4ec2f9c8d31f5d" => :mojave
     sha256 "ba37bc57cd6a16c16e10ecd79c159849ffd16ef90845d0d50253c45b958fb8b3" => :high_sierra
+    sha256 "f0d42e97902625627bf4235c9bb3c9cb9a2405dc7fc5582561df9ac395d4965a" => :x86_64_linux
   end
 
   depends_on "libevent"
