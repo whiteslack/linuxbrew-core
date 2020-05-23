4e0bc7f376d8c62755b22908751ce9cdb51c88e1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-23 10:21:31 +0000

    zstd: update 1.4.5 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -9,6 +9,7 @@ class Zstd < Formula
     sha256 "2375c206a934090c4ba53362d038e4e191d8dd09eec734e8e72106089aa24e9d" => :catalina
     sha256 "86b04bfd318315486d772b29d30b361e734a74269ae48805eeb3eae1d562b984" => :mojave
     sha256 "61de5a45183f4d029c66024d645ad44b0a625d58f9f583b47af42346a7c90fe5" => :high_sierra
+    sha256 "3b6354ada22830f32dd0ed5a6c0cc6f1e1c8de50833db6004c58874533495df7" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
