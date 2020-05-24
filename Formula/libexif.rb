f2a574b47a7ce36c64f471efcdaf01b737e83214
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-24 10:20:57 +0000

    libexif: update 0.6.22_1 bottle.

--- Formula/libexif.rb
+++ Formula/libexif.rb
@@ -9,6 +9,7 @@ class Libexif < Formula
     sha256 "7379f6990018006122bba69098864e8877e8e6e7be3af535f7e301d8ff097e98" => :catalina
     sha256 "c20d311fbd1846ce2603950ec9ad9b3b6e8202bf2f97e9aab328c05dc568fcfe" => :mojave
     sha256 "8b1c7cf6ec777090ce22ccf5c426867948a54da9378e0c9b91d85175eaea4f81" => :high_sierra
+    sha256 "7139c14be05e7c75cce2b0fd3d58dc95dffe6b3b815396ce0648566cd3000369" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
