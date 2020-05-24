ecb733177213279ba38f6a442cd6d8799442b5df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-24 13:55:42 +0000

    purescript: update 0.13.8 bottle.

--- Formula/purescript.rb
+++ Formula/purescript.rb
@@ -9,6 +9,7 @@ class Purescript < Formula
     sha256 "f9ae94071a81b727b303b9030db5bba1e6bdd62d9197f11792b36c63c24ea8f4" => :catalina
     sha256 "722db0bdf3894e87af760e1f04e1ac1c450d9e522c104471457b04675b07f62c" => :mojave
     sha256 "9daf0153a5ad5ae5a99030416c6d7f8cf6fb13f16aef52823f1d0c9f7682d6da" => :high_sierra
+    sha256 "e1035d59586d06e7b032493977f35a38fbe8c66fee41ebc4cec7674d40bafad0" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
