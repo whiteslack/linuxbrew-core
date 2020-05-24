9d76a9b0800dd640d8a0f8401691fd7cfc9478e8
Michka Popoff <michkapopoff@gmail.com>
2020-05-24 08:28:32 +0200

    postgresql@11: rebuild with right glibc version (#20419)

--- Formula/postgresql@11.rb
+++ Formula/postgresql@11.rb
@@ -3,13 +3,12 @@ class PostgresqlAT11 < Formula
   homepage "https://www.postgresql.org/"
   url "https://ftp.postgresql.org/pub/source/v11.8/postgresql-11.8.tar.bz2"
   sha256 "eaf2f4329ccc349c89e950761b81daf8c99bb8966abcab5665ccd6ee95c77ae2"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     sha256 "0b7485f02a4645bc0c835d5bfd5766c871c27282acbcc0dedfce8652c318cae8" => :catalina
     sha256 "c5775463f96853e3ca58ff447c7bf7bc811991ecb9fb3e0a0c75a5cbf90ea2aa" => :mojave
     sha256 "86cbfc8de2354ba85063acdd12d0493e0a5c9af5498d4f2174af7e9019962d11" => :high_sierra
-    sha256 "828bd63b3303bcf85d72edac60390d8bfcd48d9150d4cfff74f8cca94d1d5537" => :x86_64_linux
   end
 
   keg_only :versioned_formula
