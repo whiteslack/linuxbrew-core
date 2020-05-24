f25e15f594604b1002a980e57baf28f7112f3398
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-24 14:06:18 +0000

    postgrest: update 7.0.1 bottle.

--- Formula/postgrest.rb
+++ Formula/postgrest.rb
@@ -9,6 +9,7 @@ class Postgrest < Formula
     sha256 "691546e89701fd582d47c697dc27551ef3284ee21933a5912f406e6fee4dd272" => :catalina
     sha256 "34c0413e71a41bc8550b7ea5286e0330aa888990d2e2a8fe6d81b57152c83d61" => :mojave
     sha256 "6ca3bb9cd14c9ab4ddd028493e4ffd70ddae571be74723997b677c6c67542c87" => :high_sierra
+    sha256 "eebd15397089be906915d5839a5e1b115f41c78f440158c92826f5d9a3c82765" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
