616617fec9be1fd3cab2382c3e1112ccc4a67909
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-24 13:31:04 +0000

    console_bridge: update 0.5.1 bottle.

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -9,6 +9,7 @@ class ConsoleBridge < Formula
     sha256 "e98131216958db0f3933e7fee94f37d5e356e599802e828f06423930fae66412" => :catalina
     sha256 "f79326ff43c19d1fe5cb51af9728a52dd74953ab69c24712fcd8c7c1cc0586f0" => :mojave
     sha256 "f1bc8a6b2fd18aa320dec8239f0a2f2082518289fe29da765600e728fa9e2765" => :high_sierra
+    sha256 "14411c3ecb48a3c8f73cede0c235458938100dbde51a7340ee591029300cba1a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
