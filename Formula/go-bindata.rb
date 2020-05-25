4a2eba43c75d5bb1897d55a54c1d76da0e6cc25e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-25 05:45:38 +0000

    go-bindata: update 3.21.0 bottle.

--- Formula/go-bindata.rb
+++ Formula/go-bindata.rb
@@ -9,6 +9,7 @@ class GoBindata < Formula
     sha256 "2b83e2976473f2310d488db6c3b20c27c844ce0eae86d2787fb1f5081da7fc98" => :catalina
     sha256 "658b8c27208d0e31241c811ea52fde269354d79804e95db8fc6bf9b9dd5f89c5" => :mojave
     sha256 "e2c10e8a03c3393d7500bd085e2a9f36285ccd75cd38a989517c2aaba55f5586" => :high_sierra
+    sha256 "bd947656acb1d50643f87a7c6fc374e7779ee9ce0879e3005a34af7b5eaba7cc" => :x86_64_linux
   end
 
   depends_on "go"
