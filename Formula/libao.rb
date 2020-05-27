797a64640f1b6f4c96601a80f9d09d8e01513cea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-27 17:30:14 +0000

    libao: update 1.2.2 bottle.

--- Formula/libao.rb
+++ Formula/libao.rb
@@ -10,6 +10,7 @@ class Libao < Formula
     sha256 "703bfcae17a364ad0e526d5556b3583d1864c6db4c52ba85ef64dc0600039372" => :catalina
     sha256 "932b3a41565e678489471dae66b29fea1ca2de6013c2559f9c34cc5e9bd5a33f" => :mojave
     sha256 "d7144edd6dc64b987d9a9d584799fe20a76ed92f2b1b18c074a6846926f23169" => :high_sierra
+    sha256 "15cc75f026413a6866aab48694f9f72eaa751247a934a950d3624213f09644f3" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
