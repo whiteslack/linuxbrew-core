ded3dbf024fe63af1f6874d6785c0f64594d61a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-28 21:31:29 +0000

    hiredis: update 0.14.1_1 bottle.

--- Formula/hiredis.rb
+++ Formula/hiredis.rb
@@ -11,6 +11,7 @@ class Hiredis < Formula
     sha256 "4b089bee868b4fe533bea75917f21a42a11a9e04d5b396ec5a94d5623372ec0b" => :catalina
     sha256 "8974f8bacac7ce018f6f238915c6c5f07751c5183283337634a1715624d344b8" => :mojave
     sha256 "5ed94f51e6dbe91f7d195be5a5d085c7929f82bafeecba8645dd2315a94dad8d" => :high_sierra
+    sha256 "485e1830b65f243acfed9989791282f7973625b50b781cbd722ec27dbf3bb322" => :x86_64_linux
   end
 
   def install
