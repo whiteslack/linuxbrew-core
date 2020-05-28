449d8f9b2ecc0527504ff73ed9b949e0bff6f79f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-28 10:17:09 +0000

    glew: update 2.1.0_2 bottle.

--- Formula/glew.rb
+++ Formula/glew.rb
@@ -11,6 +11,7 @@ class Glew < Formula
     sha256 "590c10bf98e6263d8e573720e5521584d836f28c52f30f97840614e97c16ccfd" => :catalina
     sha256 "66638564b5b9d2d915b97841ef1cc117f701c7ec34707734fa1ce11919c28821" => :mojave
     sha256 "1d3b4e7938d3c1fc7e16f78a506163046da105b443498b7ca1a3cca78f232739" => :high_sierra
+    sha256 "776e4ec7562f544a6325890beeba1c059d028d2e7aab96e298c77955773e6114" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
