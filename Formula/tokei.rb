316f4f99f05faef27f11dfee909111aaabfcbed4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-29 11:26:13 +0000

    tokei: update 11.2.1 bottle.

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -9,6 +9,7 @@ class Tokei < Formula
     sha256 "0023434f2186b2e1fc29dc0ce0b0eebf90834c1db0994a3e186f8b7fab87276c" => :catalina
     sha256 "858b572bfb3eff1e5c5f68a5a4d99bcbf1c515e1c2191c2995c58c367488c166" => :mojave
     sha256 "19a72a404bab167c3ed801d8032b692f1d32f3baea3d5f0c1437e8eef50ac03e" => :high_sierra
+    sha256 "211a6370a12d16f91b25cddd5937e68ceb4163ebdff135a9d57dd063e7b020eb" => :x86_64_linux
   end
 
   depends_on "rust" => :build
