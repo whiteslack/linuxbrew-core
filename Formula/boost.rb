30211277ef78b69163ffe8b39877e437da4afef7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-30 14:32:38 +0000

    boost: update 1.72.0_3 bottle.

--- Formula/boost.rb
+++ Formula/boost.rb
@@ -24,6 +24,7 @@ class Boost < Formula
     sha256 "2e0d985997503d0f069ca63a7bcd1bc516d81341754de984f1953390d5540b76" => :catalina
     sha256 "3a89882d5d2bb7ab3bd55f99f07020f8cc880df3e8f01a1996b6288df05bf93f" => :mojave
     sha256 "9527177a4b362d9090526ab4b6c934a17a39ecbd2fe1349a00f352f41be82a91" => :high_sierra
+    sha256 "42c1b6f2b749385d6e0ad734c817a4b4fd6e1ebd745173342859b106ae5fa2ce" => :x86_64_linux
   end
 
   depends_on "icu4c" if OS.mac?
