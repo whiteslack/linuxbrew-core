29489c2acd8e2c6b450cb93a7cc75219b7ffca6b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-05-30 06:43:35 +0000

    gnu-getopt: update 2.35.2 bottle.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -9,6 +9,7 @@ class GnuGetopt < Formula
     sha256 "4eda655b18149121dd39d19928b64b63f7310469ad1d64a232996b61afb5016c" => :catalina
     sha256 "ff402c05420ff975374689ce0f185c8d9f9578f3e9113e935cf3030f0743c31c" => :mojave
     sha256 "a9c00a4e5483a0636b9a8dc6def5cc73c53f7b6ef16a476af5d470d4d6e1e5c4" => :high_sierra
+    sha256 "5f070c1fc35679873e9c2e9a63ffded1bbb1f56fc4449128ec6881bc7eddc133" => :x86_64_linux
   end
 
   keg_only (OS.mac? ? :provided_by_macos : "this formula conflicts with util-linux")
