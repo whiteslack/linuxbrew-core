dd3afa41d4e32168bb2114a1df1bec78629c8418
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-01 08:56:43 +0000

    pandoc-crossref: update 0.3.6.3 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -12,6 +12,7 @@ class PandocCrossref < Formula
     sha256 "c1fc9b7e27079de950cd5926404b928ab794fbea680373ff2de879d53e70c853" => :catalina
     sha256 "b3479f76133f44c081c6f3d7aa6ff68e2cba04bee2f83ec84d82b80b2e5d89ba" => :mojave
     sha256 "d71d84afc93ff2099ab6bfadf71cfbc36e57d5262f2f66e4102fe4f5609828a9" => :high_sierra
+    sha256 "d6f0a71f63fcc0c99e5fc9808f52745aa116a89c16ad735f110b3fa7004a8a48" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
