5306892d72b13539f2bb0cb91227ddb7bbbbf254
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-02 05:42:14 +0000

    gpp: update 2.26 bottle.

--- Formula/gpp.rb
+++ Formula/gpp.rb
@@ -9,6 +9,7 @@ class Gpp < Formula
     sha256 "7b18d4755835ab3cad89e5d90b459475dd499695d461a6c8ac4df35f07a11b1b" => :catalina
     sha256 "d83bbb08210bd763f300a44c9cae8cf8bad7070fcd0e8064e2c81f6c385937b8" => :mojave
     sha256 "c45c620602411620fa60c1b780a0a9b6172606f9f03c9e7d18e31e9c1276b172" => :high_sierra
+    sha256 "e11d64d9facd5bbe0db2ad12af8b0e54dcebe0e0ecb3a9f6a4827f76f26853a5" => :x86_64_linux
   end
 
   def install
