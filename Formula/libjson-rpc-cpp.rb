8b29890674adb13e5684c7f0f5e162ee55d9cedf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-02 05:44:39 +0000

    libjson-rpc-cpp: update 1.3.0_1 bottle.

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -11,6 +11,7 @@ class LibjsonRpcCpp < Formula
     sha256 "b8a99a97b7ebbcb40cc19ad3f4805a93270e376fe7ef4ccbc02235c0f94f4c74" => :catalina
     sha256 "15fbc327291d065a0a691f14ea90367923296da22a4012247a2083ebec8eca3b" => :mojave
     sha256 "d3e24865583d50b44a833b08fb90093a2db62bda4615e3c734ffe5cd2bda23ff" => :high_sierra
+    sha256 "8d960b30033558dd03da4428d0b8ff8b007e94bdf83cd5bbd2b317bf478ed8b7" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
