a115e8be18322b0005a7bef669f97b04de44254f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-02 05:45:28 +0000

    pastel: update 0.8.0 bottle.

--- Formula/pastel.rb
+++ Formula/pastel.rb
@@ -9,6 +9,7 @@ class Pastel < Formula
     sha256 "e2478748ed0561c76af8f68b4067f42cd365ad7735a9d44dda159e9bd35a1c1d" => :catalina
     sha256 "3d9fcb9c4f2e70010681b88f0ceca5795773b849e7bc6f59689e90ad969a673c" => :mojave
     sha256 "06f87da95e1d1b0f53fbcd9c9ff36e4c8a0d3ea85825ac7bc648b3ab445e61ee" => :high_sierra
+    sha256 "921f26cbdd5e4fedce06655961f8c8786bf0b35677f82348909d1a6d0af03198" => :x86_64_linux
   end
 
   depends_on "rust" => :build
