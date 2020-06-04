4374f621bb9ad67e940a289b249bdb17791442fc
Dawid Dziurla <dawidd0811@gmail.com>
2020-06-04 15:20:12 +0200

    pre-commit: rebuild (#20488)

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -5,13 +5,13 @@ class PreCommit < Formula
   homepage "https://pre-commit.com/"
   url "https://github.com/pre-commit/pre-commit/archive/v2.4.0.tar.gz"
   sha256 "0bfb890aac9a91b7ea232d9a160755c5e9e01bf0e4cbcd7981de56b970b3d624"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "1d73a6c0cc2dc9869e415095454a41b224a6093c60194fe8dead9aba328ba872" => :catalina
     sha256 "d82e9f3265020af2af98aac3d79cda31be8213742c9a9cf0e8e0700a3771852c" => :mojave
     sha256 "4affd7ab48dbe342d22ff1382a32c2569189dadf2e5610026efdf768598fd18e" => :high_sierra
-    sha256 "68091ab7a3c787258d1069b19cb84839079b7533516a829eeb985a2013255bbe" => :x86_64_linux
   end
 
   depends_on "libyaml"
