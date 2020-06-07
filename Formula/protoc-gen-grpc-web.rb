8b81444ad68db5a96c61f243d4eeb9c185652c48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-07 08:37:54 +0000

    protoc-gen-grpc-web: update 1.1.0 bottle.

--- Formula/protoc-gen-grpc-web.rb
+++ Formula/protoc-gen-grpc-web.rb
@@ -11,6 +11,7 @@ class ProtocGenGrpcWeb < Formula
     sha256 "97b4e68ca6ccc980466325c4966e8a5b24f04ca13b11216d1ac5598035fd9671" => :catalina
     sha256 "1ecc3a9c5846b855ca44c32a4facd8862480ee614bf842710fb87441d840ac96" => :mojave
     sha256 "de02fd2cf1bc495852ba7208e4d95317633cd9bf0a5fcaf9ec567652917eb47e" => :high_sierra
+    sha256 "e461eb3efcaee06109a8553d620d62353d20b55fcce223e19520110a1c8fd1bc" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
