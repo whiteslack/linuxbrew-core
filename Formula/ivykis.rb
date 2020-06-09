bad2a96b62e6c85d4fb8ef0a25b13a45f7747197
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-09 12:44:11 +0000

    ivykis: update 0.42.4 bottle.

--- Formula/ivykis.rb
+++ Formula/ivykis.rb
@@ -10,6 +10,7 @@ class Ivykis < Formula
     sha256 "5da36891f20e60db1a94b7eafeaf35605a0a4b18e833721aec01ab68399653a3" => :catalina
     sha256 "dd4fa86f2988dd4c913fc443131ce519ebf034ff492b4760f323ca663fb1744c" => :mojave
     sha256 "1409aa60298ac27959cf5370b70d158843524e5f5638e28e9607ac7e8783b11e" => :high_sierra
+    sha256 "29416c373dd9f32504a9f4c206026b0184922f5bfbf89d1d3f368ea8cc2cb849" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
