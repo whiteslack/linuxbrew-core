47ca93e1bfb9161320866943598cd947883b8f73
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-09 05:37:46 +0000

    tox: update 3.15.2 bottle.

--- Formula/tox.rb
+++ Formula/tox.rb
@@ -11,6 +11,7 @@ class Tox < Formula
     sha256 "af095330ca9abf8daf3342dac5f92a1a97398d004a0dd0f05526ae5cee9a92aa" => :catalina
     sha256 "5a93a4c767e9b5311eca3411c2900a92643dfc7b2ab2666daae33b0d89fa6147" => :mojave
     sha256 "878b5ca97993b458713a609f1e5326b9a79d1ea333b48be1f59dc1558a7a225e" => :high_sierra
+    sha256 "7a67e8093455e662ac8053dfaa3cdb8fe8dee9e40d587ba82a4f14620adb339f" => :x86_64_linux
   end
 
   depends_on "python@3.8"
