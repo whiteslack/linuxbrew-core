42a49717e46315cd1afedd23202745699dbd2e4f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-10 21:19:01 +0000

    dwatch: update 0.1.1 bottle.

--- Formula/dwatch.rb
+++ Formula/dwatch.rb
@@ -9,6 +9,7 @@ class Dwatch < Formula
     sha256 "c79f51f4329569d682357a97014bd67a14ac1444e4fb983abd3a9e96339ba87a" => :catalina
     sha256 "69b3cb7cc60c1635c3134a0cd5e9dd884b3e28f52955e62da9beb0605e43cff5" => :mojave
     sha256 "fdf97f373c4bb18a3025d0f4acd9e16c826eca19cb60c9abd59d59bee8741c0f" => :high_sierra
+    sha256 "1d0a738bdfb6cf47f3b6d582d3e0bf8607689334cf80948c2b903e1c6f189199" => :x86_64_linux
   end
 
   def install
