fae99556915c4d6f891473b3806617bc262037a9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-11 10:40:14 +0000

    uni: update 1.1.1 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -9,6 +9,7 @@ class Uni < Formula
     sha256 "d6a995e94c8bc6f9b74a1eb370b5aa348a7377e9170453820400da83641cef66" => :catalina
     sha256 "1a2f04a3dd21f6d2c2ea16d3683f936e1d8310dd4b2d503aa8a60d67b6f24367" => :mojave
     sha256 "e4b8e98523d14eb9cba3991946deb0cea86c09acfb65d175c04d4233ec32eceb" => :high_sierra
+    sha256 "9846eaccc0cafdb44225000c57b7c2164bb3af48a5ae0a07e73b28f12b2381fc" => :x86_64_linux
   end
 
   depends_on "go" => :build
