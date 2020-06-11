d249736af778a59908b1049013b878318a965462
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-11 15:38:33 +0000

    cacli: update 1.3.2 bottle.

--- Formula/cacli.rb
+++ Formula/cacli.rb
@@ -9,6 +9,7 @@ class Cacli < Formula
     sha256 "63f761d1b56137cdb4a2d94e5894c7a43ac28f8d9f7f36c2011da7ea21445c9e" => :catalina
     sha256 "6b8148ab93f63cc8342a2b77356c1154d875f710edceacaac4258d36d1ccb108" => :mojave
     sha256 "6dbca926050f4ca29a073d05591e818690d9a3d3cae0dffc7d658aab9afef02d" => :high_sierra
+    sha256 "7b818e31128751aae6a9ee92dcb411e9cb77f7168b0b254c40a0789228389995" => :x86_64_linux
   end
 
   depends_on "go" => :build
