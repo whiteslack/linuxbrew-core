b887e5c1493c7249d25af60dbf9c7d543ac43b70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-11 06:05:35 +0000

    convox: update 3.0.20 bottle.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -10,6 +10,7 @@ class Convox < Formula
     sha256 "9ed3384832d3d98805443b5611a7ce5d0c06a766247c25b6d4a24f3455217708" => :catalina
     sha256 "3d26506312f0e517a0f0ce3c93477017c53427fc79084e628499887544fcc921" => :mojave
     sha256 "f8759c5f2e96c8ef61081875ad82eee7eb350dd30582058d9eadaf6097754fee" => :high_sierra
+    sha256 "afdfb6aebf0570dc23758274cc0097009e20dec0b3750a221dca04b03362d801" => :x86_64_linux
   end
 
   depends_on "go" => :build
