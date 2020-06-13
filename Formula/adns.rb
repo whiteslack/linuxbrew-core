71d3331ef3fd0a7b2ab3c32bc20443feea216148
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-13 12:52:07 +0000

    adns: update 1.6.0 bottle.

--- Formula/adns.rb
+++ Formula/adns.rb
@@ -10,6 +10,7 @@ class Adns < Formula
     sha256 "d9cc50eec8ac243148a121049c236cba06af4a0b1156ab397d0a2850aa79c137" => :catalina
     sha256 "7cf73e25044783cd93ecd28e2e8bfb84f0b2fff3343acf39dff3c5fe68d1c5be" => :mojave
     sha256 "6cbe64a32b077c9abd61337c51c4e17a2286f9bee04b33f24a5dd762125798d1" => :high_sierra
+    sha256 "fd38f96459e3ebeaef84eb335a08e2a9fe0d8fe37d4f621bed4b11d35a919b5c" => :x86_64_linux
   end
 
   uses_from_macos "m4" => :build
