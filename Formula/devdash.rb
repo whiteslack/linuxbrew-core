4ec8285502e69fbe995a208609614dc400f2f8d9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-13 09:52:09 +0000

    devdash: update 0.4.0 bottle.

--- Formula/devdash.rb
+++ Formula/devdash.rb
@@ -9,6 +9,7 @@ class Devdash < Formula
     sha256 "86085a11dbf43d1edafd9e9fd7e47443ec11ab692b9b48a80c5ffc58a2f1b39a" => :catalina
     sha256 "a15bcbbc6085b0420bb9bc863acfffbd4a8fd05956dc66dd163caa8ddf731fc3" => :mojave
     sha256 "ef75c99c9d174815f7fb244f8b63866e0b27e22d5c35c9e37190da3293c7678b" => :high_sierra
+    sha256 "206bd004980a01c0b63a85d9a7ff2cf6b13e5275bbf375cb1e6a8f6133be75b6" => :x86_64_linux
   end
 
   depends_on "go" => :build
