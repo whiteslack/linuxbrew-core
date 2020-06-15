9708bea92d0942fbb2899166b67a6aa61236b43a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-15 12:28:20 +0000

    sqliteodbc: update 0.9997 bottle.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -10,6 +10,7 @@ class Sqliteodbc < Formula
     sha256 "c49e1d843386a98770b7862f8cff4c6e8329c1c164ddf5fbe775fa7ae51a8cdf" => :catalina
     sha256 "ced11b3b38c9c2cc8b6f4271a07f77015d2a412f2c8cdcae77b6d93139e0b31d" => :mojave
     sha256 "eb9cc3938701ae4b37413b9ed457ee64e0d5ee0534451fc3f3c755b5bae8bace" => :high_sierra
+    sha256 "1974308e036e51466381e13bbe6342a289a110746a53ca1a766fa920b4aac1b4" => :x86_64_linux
   end
 
   depends_on "sqlite"
