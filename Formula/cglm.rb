22f84c222f1bd27357b8ae075fe62341ff8e1918
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-15 10:50:53 +0000

    cglm: update 0.7.6 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -9,6 +9,7 @@ class Cglm < Formula
     sha256 "71acb4380ab467ed76133c76d7b971a9914c0ba07b41ad51ee7e5b74334b6861" => :catalina
     sha256 "253894ff185a4f8499253543a8cd246dc8958f65746f3319f1e311c0f81feabc" => :mojave
     sha256 "683def15df6b8bf55a4954a3427291b559204975b5cdc35d74a18170d4e7db58" => :high_sierra
+    sha256 "50ba2936befa7cbfd7a802d3b5a6f55d4d5c1ab0b959ab7d2708c53c5c4da38d" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
