beff1272a0026b09ca379fd58d2f2131947e7940
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-15 21:19:06 +0000

    swig: update 4.0.2 bottle.

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -8,6 +8,7 @@ class Swig < Formula
     sha256 "530e80b7e7dcd28469b52fc3b668683a97b72642ebf2b6d4e6708d14f05e7286" => :catalina
     sha256 "50afb5930cb37af2e400f0369f6da15b1d4922c1f72f45d13e7e3f8bd9d6d27b" => :mojave
     sha256 "8bab440005b048ce454a3dd50ba608e1f85391edd73e9e40510269e923cad238" => :high_sierra
+    sha256 "39c044d1f12dfc0fd0f6cb02eb4a7a3830f91f252c09fe82fe3c03379f3635d1" => :x86_64_linux
   end
 
   head do
