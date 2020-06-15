809c4a4e175af27e74adb857c556aefae299decf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-15 16:58:34 +0000

    ott: update 0.31 bottle.

--- Formula/ott.rb
+++ Formula/ott.rb
@@ -10,6 +10,7 @@ class Ott < Formula
     sha256 "1cf56f59cd4fa2cd4c1e067fdcd9c63d9287ea700b9ced54af926222b43dfea0" => :catalina
     sha256 "f96a67f19f9eea3bebd7144e126b37aceae2853d2f3b91fb6a06b3dec533ab30" => :mojave
     sha256 "0383486c69d738578cd580b9e9b4de2274909ce86cad1fc5274c5c13b2bcabd7" => :high_sierra
+    sha256 "972d7d63b695f71da5431b5ad9739c44350e97788c847943288723ed2c433871" => :x86_64_linux
   end
 
   depends_on "ocaml" => :build
