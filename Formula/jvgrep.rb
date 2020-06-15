a69208c503244237dc567835b59c79482fb143c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-15 10:47:46 +0000

    jvgrep: update 5.8.8 bottle.

--- Formula/jvgrep.rb
+++ Formula/jvgrep.rb
@@ -10,6 +10,7 @@ class Jvgrep < Formula
     sha256 "fa73d4fa22c06a91abae4fb577d8000897a95687de102fa09258a2f726801791" => :catalina
     sha256 "fa73d4fa22c06a91abae4fb577d8000897a95687de102fa09258a2f726801791" => :mojave
     sha256 "fa73d4fa22c06a91abae4fb577d8000897a95687de102fa09258a2f726801791" => :high_sierra
+    sha256 "9ce50e79263cd67bc65562d830cdd0b8fc16050ad54856d215c8d0d6bc71b256" => :x86_64_linux
   end
 
   depends_on "go" => :build
