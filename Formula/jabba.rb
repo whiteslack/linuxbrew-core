776157340b325b013d54d2a5b52dd34e4a4fe044
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-16 21:12:15 +0000

    jabba: update 0.11.2 bottle.

--- Formula/jabba.rb
+++ Formula/jabba.rb
@@ -11,6 +11,7 @@ class Jabba < Formula
     sha256 "7eddb409c7bb2784db21756e624a18b19977bb4df53ab547eaedd8abe876651e" => :catalina
     sha256 "3101ea25ce49c3ed96b3c8595a5441fec3aeb536b56eca21c1dea56f6c1fd86b" => :mojave
     sha256 "8454f5aa9b8832908b1c889531118ea058b2e675ef7f7f37eeb282f454aeec1e" => :high_sierra
+    sha256 "237bab11bbcc4434a0b48c8d03a6f8634d12ec011f9357f26d8965f523fedad2" => :x86_64_linux
   end
 
   depends_on "glide" => :build
