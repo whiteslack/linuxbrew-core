d8e9c5aff721ba2e0a768ff05641386ba23a6316
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-17 16:52:48 +0000

    opa: update 0.21.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -10,6 +10,7 @@ class Opa < Formula
     sha256 "ad97bf77534ea5b72012148ec96e08700b1762b89f1222738e5c51f43c74f5d8" => :catalina
     sha256 "8ef806fc00001368ac78a50df08024c0b52d76438b1f7963ab9d71e02c045b4f" => :mojave
     sha256 "271f1584b341382fd5f9b5f601707d4fcb0efe96b66d82bf958221f091a287a9" => :high_sierra
+    sha256 "5ed911165ca9b61aac74f4908917bb4d8f09aaf27c20d153073558c8ce310ff9" => :x86_64_linux
   end
 
   depends_on "go" => :build
