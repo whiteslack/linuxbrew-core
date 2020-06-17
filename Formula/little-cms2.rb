ecbcfdd281f7e72b9143b5d26455c7821b2041b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-17 16:51:27 +0000

    little-cms2: update 2.11 bottle.

--- Formula/little-cms2.rb
+++ Formula/little-cms2.rb
@@ -11,6 +11,7 @@ class LittleCms2 < Formula
     sha256 "b0fe7486871b0fb0e34012f48bce09e96229e5e2985d64e7a0164c2847e41975" => :catalina
     sha256 "e05f0a487d2243411eeb9fd9909f875517d7b27feb3cb914117acd9c60b76fcc" => :mojave
     sha256 "928d1b8b8292a2d7950d0ef1381c70996bcde325f0124d7dcb68059090544dac" => :high_sierra
+    sha256 "4f8c6ff23401b88d771fa28d80d3ef3d4f0dfc5fc838d8a73b2a6b540e176d66" => :x86_64_linux
   end
 
   depends_on "jpeg"
