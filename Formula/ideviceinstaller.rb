014e9d34ee75626dc9db5a126415c9c86eb9e132
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-17 16:56:07 +0000

    ideviceinstaller: update 1.1.1 bottle.

--- Formula/ideviceinstaller.rb
+++ Formula/ideviceinstaller.rb
@@ -9,6 +9,7 @@ class Ideviceinstaller < Formula
     sha256 "6ee12db78e8c224c0eb0cf88eb4f43242eb1ba672eb006636273b99b75b02a87" => :catalina
     sha256 "6ed5e4f7ace33fd5f4d1b4c6b9f0fd519836080e170b981e63942087698351c6" => :mojave
     sha256 "0dfe944eaa47cad87ad22f70dbbcefdb6b27bbeb83ca1f7a229827c03054c07c" => :high_sierra
+    sha256 "c189692eaa026504af0410a6c8e50a7a94a6e316c5da2cdb8f70b4f9479d2e81" => :x86_64_linux
   end
 
   head do
