ee7bdcec4aaafc3290e6b9deb881ad02a789394d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-17 16:56:07 +0000

    libimobiledevice: update 1.3.0 bottle.

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -9,6 +9,7 @@ class Libimobiledevice < Formula
     sha256 "eb7f28d86797461d5ef859d00629176e1ce3234790ef17b9ee3f9c9990a664e2" => :catalina
     sha256 "5143eaf34011a22dd1951f10495a7568e77a2e862fb9f4dbae9bab2f784f926e" => :mojave
     sha256 "072d224a0fa2a77bccde27eee39b65300a387613b41f07fc677108a7812ec003" => :high_sierra
+    sha256 "4aee409e60e657edc0f5127aafd198cdf4a09aece3c296ad55702644572518f3" => :x86_64_linux
   end
 
   head do
