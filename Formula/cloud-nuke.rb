9b271b0e8588de935c642f0ec6042423ccadaa64
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-18 07:38:12 +0000

    cloud-nuke: update 0.1.20 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -10,6 +10,7 @@ class CloudNuke < Formula
     sha256 "1bd062358ee79ddd80e6aa82b70586c35e5db0f5578e828534186a2945114be2" => :catalina
     sha256 "00d0e25987130eef854be69b476785b5c9520c010462b152435aa6a406098c2a" => :mojave
     sha256 "a7a6c5ebabdab4c70e408d12dac4798d675dcb067de4e342d08c9d8a894ea9b9" => :high_sierra
+    sha256 "7d570c656c8ae737e58b20205dfeed640dd085c834cd67268f2253a47f92a3fd" => :x86_64_linux
   end
 
   depends_on "go" => :build
