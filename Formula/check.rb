b3b95c9b56615132294bd44026b1d4fac132bde1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-22 19:32:27 +0000

    check: update 0.15.0 bottle.

--- Formula/check.rb
+++ Formula/check.rb
@@ -9,6 +9,7 @@ class Check < Formula
     sha256 "dbc7dd0d84f86a9ad3813a4ad45b076903120f0e3d0cb949c46f2dfc448e7d3c" => :catalina
     sha256 "9820eed7640c7e834672ea0a378b37ebc8825b0513de1b06b6321eb51c799686" => :mojave
     sha256 "f1f7a1f81a08235f922124a0e833fd9862e9e53931c5113ad543b42c4636a2b6" => :high_sierra
+    sha256 "3d23c9dde99765d537b865cc6de56a9765f7c195fa0127d02d3cb0b080ca0572" => :x86_64_linux
   end
 
   on_linux do
