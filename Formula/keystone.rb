83e38571ae83359e87419811f649bae4ae0c3cb0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-22 18:06:39 +0000

    keystone: update 0.9.2 bottle.

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -10,6 +10,7 @@ class Keystone < Formula
     sha256 "84cdef2aa8a5697ce2fc62e6ae1316f2dcca6fcd0f92d2bba68b399af9c48440" => :catalina
     sha256 "814feeee85e111a21fdd287fbed3fca3e1cd86be396dcba7612c3e1aec7dd4d3" => :mojave
     sha256 "77740af9b9e48baaf0a7d1dc4d74b883c1babbaab6a7e9bf65a00035b59c546d" => :high_sierra
+    sha256 "675adee0f281231477110542b03ef0631784938e6928cdbe2678b0e4cd67cee9" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
