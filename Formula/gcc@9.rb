8e29b41f0b0a69caa8ded9fb584ab2c640aaf466
Rylan Polster <rslpolster@gmail.com>
2020-06-23 08:19:00 -0400

    gcc@9: use Utils.safe_popen_read

--- Formula/gcc@9.rb
+++ Formula/gcc@9.rb
@@ -75,7 +75,7 @@ class GccAT9 < Formula
 
       # Set the search path for glibc libraries and objects, using the system's glibc
       # Fix the error: ld: cannot find crti.o: No such file or directory
-      ENV.prepend_path "LIBRARY_PATH", Pathname.new(Utils.popen_read(ENV.cc, "-print-file-name=crti.o")).parent
+      ENV.prepend_path "LIBRARY_PATH", Pathname.new(Utils.safe_popen_read(ENV.cc, "-print-file-name=crti.o")).parent
     end
 
     args += [
@@ -136,7 +136,7 @@ class GccAT9 < Formula
   def post_install
     unless OS.mac?
       gcc = bin/"gcc-#{version_suffix}"
-      libgcc = Pathname.new(Utils.popen_read(gcc, "-print-libgcc-file-name")).parent
+      libgcc = Pathname.new(Utils.safe_popen_read(gcc, "-print-libgcc-file-name")).parent
       raise "command failed: #{gcc} -print-libgcc-file-name" if $CHILD_STATUS.exitstatus.nonzero?
 
       glibc = Formula["glibc"]
@@ -146,7 +146,7 @@ class GccAT9 < Formula
       crtdir = if glibc_installed
         glibc.opt_lib
       else
-        Pathname.new(Utils.popen_read("/usr/bin/cc", "-print-file-name=crti.o")).parent
+        Pathname.new(Utils.safe_popen_read("/usr/bin/cc", "-print-file-name=crti.o")).parent
       end
       ln_sf Dir[crtdir/"*crt?.o"], libgcc
 
@@ -166,14 +166,14 @@ class GccAT9 < Formula
         system_header_dirs << glibc.opt_include
       else
         # Locate the native system header dirs if user uses system glibc
-        target = Utils.popen_read(gcc, "-print-multiarch").chomp
+        target = Utils.safe_popen_read(gcc, "-print-multiarch").chomp
         raise "command failed: #{gcc} -print-multiarch" if $CHILD_STATUS.exitstatus.nonzero?
 
         system_header_dirs += ["/usr/include/#{target}", "/usr/include"]
       end
 
       # Save a backup of the default specs file
-      specs_string = Utils.popen_read(gcc, "-dumpspecs")
+      specs_string = Utils.safe_popen_read(gcc, "-dumpspecs")
       raise "command failed: #{gcc} -dumpspecs" if $CHILD_STATUS.exitstatus.nonzero?
 
       specs_orig.write specs_string
