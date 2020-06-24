e9f736852d3c33569edbc18f88aec17d5d3409d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-24 18:06:30 +0000

    git-extras: update 6.0.0 bottle.

--- Formula/git-extras.rb
+++ Formula/git-extras.rb
@@ -11,6 +11,7 @@ class GitExtras < Formula
     sha256 "6bec6d92d79cabaac6e99d15c8bf1542cf97dcb190b7e77ad4eaa8e381cff055" => :catalina
     sha256 "6bec6d92d79cabaac6e99d15c8bf1542cf97dcb190b7e77ad4eaa8e381cff055" => :mojave
     sha256 "6bec6d92d79cabaac6e99d15c8bf1542cf97dcb190b7e77ad4eaa8e381cff055" => :high_sierra
+    sha256 "a3a68b43efd6453e8dd7292b63e712143bedf015b6cd9339035c3b51a6269843" => :x86_64_linux
   end
 
   depends_on "bsdmainutils" => :build unless OS.mac?
