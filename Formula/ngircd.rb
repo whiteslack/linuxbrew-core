3008b89f8f3a284b9690d69ffd4c253f2684381f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-25 20:03:59 +0000

    ngircd: update 26 bottle.

--- Formula/ngircd.rb
+++ Formula/ngircd.rb
@@ -10,6 +10,7 @@ class Ngircd < Formula
     sha256 "129bf331b86f1d54735cf773a36b402323af0667afc1e2a1da4554899d05e8e3" => :catalina
     sha256 "7fdce95437555b6de33aa1b0c9d3e3441405b5d3c9ec2f728a7b28f35983402d" => :mojave
     sha256 "b58cacea0c009487e46b48d7220623001e49ad31722704ee805fd7029313f5d6" => :high_sierra
+    sha256 "aa396991d5dba0e4223b1335dad39f3d1f9db97440be0dea857574a933765d17" => :x86_64_linux
   end
 
   depends_on "libident"
