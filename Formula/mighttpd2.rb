c78111041ba8615e1b98ab2fb0c4c2eb05bda333
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-26 20:41:04 +0000

    mighttpd2: update 3.4.6_1 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -9,6 +9,7 @@ class Mighttpd2 < Formula
     sha256 "bcea435a9feba47df19b64d9fac972a1df8f580647204b07a73b2ade2e14c479" => :catalina
     sha256 "68e563757fb405de41a4312c03f7b72da99586430ea8f0aff98fdab48213635f" => :mojave
     sha256 "7b033c6ce128310465134a09bae1ef3df9cb630db732167a06028c1a5773576e" => :high_sierra
+    sha256 "7652478fdda8d75605a033d776ef5020bb8e5a231ba0ab2991906feab9a80af9" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
