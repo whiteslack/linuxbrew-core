c8013d68c9507cf15261cbf45e1e7c0dc73205c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-26 19:46:31 +0000

    skaffold: update 1.12.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -11,6 +11,7 @@ class Skaffold < Formula
     sha256 "556ed4c247daa2984c5ca579762165498010001c69be13dec946ef1c74a4497e" => :catalina
     sha256 "1fcbae9dbdfe5df2a10366e3894dbb21c973ab76a7972bb76be026d5550ee059" => :mojave
     sha256 "a5663b3146e187620e2e78205106334a73ddc7fd70513e6eb61f4cd4a6f8f047" => :high_sierra
+    sha256 "a72f71eb8b2de076410ce68952f4bf3ccf28836d2378bade91310d84c37b3880" => :x86_64_linux
   end
 
   depends_on "go" => :build
