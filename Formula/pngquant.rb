14f98f4da22216e8bb7c68de43a68811cdf03b79
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-26 20:29:25 +0000

    pngquant: update 2.12.5 bottle.

--- Formula/pngquant.rb
+++ Formula/pngquant.rb
@@ -11,6 +11,7 @@ class Pngquant < Formula
     sha256 "6212657919215e9f140239aa7df37b97d1b9820543e884bc4af45ef0deda523a" => :catalina
     sha256 "0c90b318acdfa38b8fb7a0bea76c5932a9c1bfb00f2a866bbd30781631876e33" => :mojave
     sha256 "da0955f28f17ef1b50dc15256f80b5742c0c940d1c6ce683990edde6fffd642a" => :high_sierra
+    sha256 "b0b78bc304a4931cb4d240e047babe60a3e34c10607097d8c64d65da3bc2e160" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
