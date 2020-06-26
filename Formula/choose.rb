57ad03c24e39f0dd921b420313972c99f744b100
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-26 20:28:31 +0000

    choose: update 0.1.0_2 bottle.

--- Formula/choose.rb
+++ Formula/choose.rb
@@ -12,6 +12,7 @@ class Choose < Formula
     sha256 "add76ddd5165ad17bfc784dff7237777eac7ee898379bd55d76228e6f6897d6d" => :catalina
     sha256 "74170f2582ab4a29bffcc34583017f97f39c5b600a5f5cfebacd6d66ecd5c818" => :mojave
     sha256 "388b64e8072a54f01006ab9cad297789eda36fe0bec83b45759cfe80dbf8cb2c" => :high_sierra
+    sha256 "2f389f87ab1391b4adea1d0ec7040eacbe47564e5aeef6c8e747b4e36c3b9853" => :x86_64_linux
   end
 
   depends_on "python@3.8"
