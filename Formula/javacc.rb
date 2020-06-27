1ae887b1c3086482e31ee24578c9f6abddc306f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-27 20:49:33 +0000

    javacc: update 7.0.8 bottle.

--- Formula/javacc.rb
+++ Formula/javacc.rb
@@ -9,6 +9,7 @@ class Javacc < Formula
     sha256 "a52693a4ca5d13098e48f4325cb97504d1e9637c02b4bacca815c8ed123e585a" => :catalina
     sha256 "c908d04a9ae71a75b685f437c6f3810aa5ac909682a2bba7d3ba72c0ea272871" => :mojave
     sha256 "2492365ba9e457270b724001cf3393c5399c18b43b95d36d268c8d8d648a754d" => :high_sierra
+    sha256 "218a5e1a71518b420decb96a302aa35abe04ffd081b55e8c60210cc175b2125a" => :x86_64_linux
   end
 
   depends_on "ant" => :build
