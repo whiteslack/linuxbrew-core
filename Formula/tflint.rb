4e7e6c4fa279d91ed7052d78a1890c86937c4009
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-27 20:46:05 +0000

    tflint: update 0.17.0 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -10,6 +10,7 @@ class Tflint < Formula
     sha256 "e8418093a3b01343484b145321c0f88dcea704b4755433a16918b30c37036080" => :catalina
     sha256 "81e76a5716c6a32316c45496cad502e20bdc7adc77fe7b175057ad0a3b87f2c5" => :mojave
     sha256 "71703bb43d3a7b2f3e798a07a8494e30cc06b3cb533b41e01c8c6b36781338b2" => :high_sierra
+    sha256 "51510a7a4e10e4700f09b97e3370a2394940812cf2d3e54d530e5df9648d6832" => :x86_64_linux
   end
 
   depends_on "go" => :build
