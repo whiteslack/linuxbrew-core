cdee0fd101b6b8f0a2521071ccef635c15618f68
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-28 21:06:08 +0000

    git-revise: update 0.6.0 bottle.

--- Formula/git-revise.rb
+++ Formula/git-revise.rb
@@ -12,6 +12,7 @@ class GitRevise < Formula
     sha256 "644bd8885dfc27b0cb84294b7195a296b1c1d56af5b870db7af109a93d80e2a0" => :catalina
     sha256 "a9a61b083fc459b407488d8ef7e2439059702e89eaa19c9a5a8c0e1fb8f1a7c7" => :mojave
     sha256 "1d15213e6e0220d5e97dd8991b4545d0789a3b907fff1a0996255ea675a1a94b" => :high_sierra
+    sha256 "ee7329fdff58943c5d080cae3a9d45050f32cf529799e5e8de94077581a1bd6e" => :x86_64_linux
   end
 
   depends_on "python@3.8"
