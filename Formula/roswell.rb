b709c901e1a49aa62133c9a2fd7f8913d28152a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-30 21:29:24 +0000

    roswell: update 20.06.14.107 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -9,6 +9,7 @@ class Roswell < Formula
     sha256 "dcd9cf15c21d13724f36fd736529faba40dc0c2af2008c970b94049796d6d171" => :catalina
     sha256 "2d28ef4f1a1cbf94fba0132f7e1f05abb87854af6ce65f70c71877c7152d5622" => :mojave
     sha256 "a6d5d3ddafc4afefbb84d077b2157a1e7101c5c057bc7fe25714c19b83659017" => :high_sierra
+    sha256 "d1c77ef13b21cf70f86f15a729ea60c3a22c5d1231cb57e43b4924ab1814e2c5" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
