7fb8ec7314488a9202c373d68c957f5bd90ed923
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-30 17:56:24 +0000

    berglas: update 0.5.3 bottle.

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -9,6 +9,7 @@ class Berglas < Formula
     sha256 "d895f86692ef19159a769e2c30742f694ffbd54dbe272f58a9c755a097bbb3a8" => :catalina
     sha256 "56c43d970f6af85431a1a40b6c249de56c2e22f74c385919118351de749a4a7a" => :mojave
     sha256 "cde26510e8ea539500c5b76a3b7130a4f29d03c1776eb36e4e1043f065f52dbb" => :high_sierra
+    sha256 "acac871c5f7986bd9fb685cab2d58bf31073bde6f4e677457c858e1ddbddde14" => :x86_64_linux
   end
 
   depends_on "go" => :build
