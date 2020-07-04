39237b1849197dd5faacc334e14696606829e2f1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-04 16:25:16 +0000

    plplot: update 5.15.0_2 bottle.

--- Formula/plplot.rb
+++ Formula/plplot.rb
@@ -9,6 +9,7 @@ class Plplot < Formula
     sha256 "c37878dbf7a1805da137aa6b9f1222050b6b62eebd36834597279b8f58f5f4d6" => :catalina
     sha256 "c647d868b1fd01db2a760965665d9ff22192fefc0ad02641a2f89c4ccf35a37f" => :mojave
     sha256 "bc07655baabc8c4faf3f76def152ad8f55a92670cd289a0d10fd33ae550f537a" => :high_sierra
+    sha256 "7784e00b80ef9ed6c20aab63cd4333f935583fcadd509e30711031804d6e66b2" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
