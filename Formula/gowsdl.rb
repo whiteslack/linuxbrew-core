780bd51a64e019dabb9738cc5444c41cb9992bc5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-05 20:15:01 +0000

    gowsdl: update 0.4.0 bottle.

--- Formula/gowsdl.rb
+++ Formula/gowsdl.rb
@@ -11,6 +11,7 @@ class Gowsdl < Formula
     sha256 "ac79869fae62091277dad289c73f337cee0c9d92a42b38af5dc4d59b53f59885" => :catalina
     sha256 "650f3a704f6918e069c60d64807ddffe3fd327b6ef2e5688768d08f646ccfa59" => :mojave
     sha256 "64d483de68fc8f23043ee9652addf1a918e79a3c68c70324652ab5d0ac4b1c64" => :high_sierra
+    sha256 "897249913a0844a0b85dda8af216710be050fdb0bebdec9d3f8fa6673aaad582" => :x86_64_linux
   end
 
   depends_on "go" => :build
