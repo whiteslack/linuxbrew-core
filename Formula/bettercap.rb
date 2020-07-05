ac5bccc3be5f2c0bd49010104c1b9066dd024b22
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-05 20:50:52 +0000

    bettercap: update 2.28 bottle.

--- Formula/bettercap.rb
+++ Formula/bettercap.rb
@@ -9,6 +9,7 @@ class Bettercap < Formula
     sha256 "0614862741982083f1629e32b87d84116917e218cac936a078061b898a1e3f04" => :catalina
     sha256 "805fbdc7281828c316c6fc91454c7f101ab7be69b235b1e32aa78dbaf55da8d3" => :mojave
     sha256 "6709b0ce6657bc3732dee9079d7635dbab2450d233c57f82e5758e2d0978a38e" => :high_sierra
+    sha256 "41606ab6698564a4e273a0bb754e06972d9df07e05e332b75350f9798aa4161c" => :x86_64_linux
   end
 
   depends_on "dep" => :build
