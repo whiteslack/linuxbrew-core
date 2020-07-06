9cfe76a6cccca977b38a813eebd5500272573279
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-06 19:15:39 +0000

    linkerd: update 2.8.1 bottle.

--- Formula/linkerd.rb
+++ Formula/linkerd.rb
@@ -12,6 +12,7 @@ class Linkerd < Formula
     sha256 "e7935ba4364476f17408883ee28616ef184313141b3bf867741909abcd350715" => :catalina
     sha256 "5e68a496869ae3c8aef8c99331f573e2a49c7fa831584737f133e5efa7784a69" => :mojave
     sha256 "341c693291213280a51bf4ad81281e8428a3c264301be1074ae09395b1b09e66" => :high_sierra
+    sha256 "86e0cacd0b612dbf3ef797974feacbb6fffff065f0292dfd1c2bf6e68cbc71b8" => :x86_64_linux
   end
 
   depends_on "go" => :build
