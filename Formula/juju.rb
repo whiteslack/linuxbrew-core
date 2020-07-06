824cebfd3fa4c023621bfb4077718879558a35db
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-06 20:59:16 +0000

    juju: update 2.8.1 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -10,6 +10,7 @@ class Juju < Formula
     sha256 "299b35b832c49540bcc373cab47ed16f066280927877e0712d914e6b6e981a70" => :catalina
     sha256 "a896b2f3489774392f4d0c6d640d0ea222d99f54d7b74b583ec35d81e01dc81a" => :mojave
     sha256 "6b37fe65c0e41cce1ac0089d8b4fc55c4dee87acd9a3c5e342233801551b6666" => :high_sierra
+    sha256 "ad2a71b5b7722c3352e9923eebd8f5b03b9a56436c80aecae90d2e12c90a225c" => :x86_64_linux
   end
 
   depends_on "go" => :build
