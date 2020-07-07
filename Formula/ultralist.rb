4bde4474633f1bc53bb6b38ebcda5d76e0fde52a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-07 05:36:08 +0000

    ultralist: update 0.9.7 bottle.

--- Formula/ultralist.rb
+++ Formula/ultralist.rb
@@ -9,6 +9,7 @@ class Ultralist < Formula
     sha256 "8c1c484137baf1be16e5582285b8b66bc4e29b324609a577740af3c969b0a21e" => :catalina
     sha256 "a31c67179fa04dbd69aa933012cc966963f8f70409af0f6c52526c092364ff58" => :mojave
     sha256 "ce141acbf7f825124d9b129787f5add4e3653ac21119efb1188624b586fd17fe" => :high_sierra
+    sha256 "1a210ec77fc2d678714490f65318b743de8b5eb927fd164f453dfcf8eefdec81" => :x86_64_linux
   end
 
   depends_on "go" => :build
