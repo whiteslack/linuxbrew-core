67088ba341189d1fdb47c476fd397280e54222fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-07 15:36:54 +0000

    ser2net: update 4.1.8 bottle.

--- Formula/ser2net.rb
+++ Formula/ser2net.rb
@@ -9,6 +9,7 @@ class Ser2net < Formula
     sha256 "c0133e4f6e044e683024ebf11fc81ceb6b38f19da808dea1b6aaa7219cfdf735" => :catalina
     sha256 "575504b003d96ed94cc054507cebdb89d8db23a381ff325e308c87fb49187079" => :mojave
     sha256 "3de3c5f9099400e4d4411c39ec33f959279c3d0c9ef03603f6b0128826070bcd" => :high_sierra
+    sha256 "3fb81953d96e6d76b220f98b7e7ce284a0fafb9b9f9b152056e8c698aa658430" => :x86_64_linux
   end
 
   depends_on "libyaml"
