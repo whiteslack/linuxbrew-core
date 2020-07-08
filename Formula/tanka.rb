72d1a5b68466dee6cb6f405dfa612d5880d334c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-08 21:03:53 +0000

    tanka: update 0.11.0 bottle.

--- Formula/tanka.rb
+++ Formula/tanka.rb
@@ -12,6 +12,7 @@ class Tanka < Formula
     sha256 "f6a510b8eedb26c43c2a9bf45883bf1d4b690861fe65c5949ab8f330a7269aa7" => :catalina
     sha256 "4aed423fb5e06697f27df4b145498f5d477290845400e43e540faf809c9d82eb" => :mojave
     sha256 "34e4ec5a9364f12701bdedb164eca8ffc11776cb915d738e572e6679937a868e" => :high_sierra
+    sha256 "87e9bf8f70202e677f899ef9f8eb00d2552ea5a066435aa3778c74b250977b0a" => :x86_64_linux
   end
 
   depends_on "go" => :build
