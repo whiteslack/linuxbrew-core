2548fe3af27985e4e25d9580e52f4efbaa36b1c1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-09 19:36:28 +0000

    jdupes: update 1.18.1 bottle.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -9,6 +9,7 @@ class Jdupes < Formula
     sha256 "da7bc84d87ece56b7892c5a4f37cb0e1e655101958d2976ecb4670b262408f60" => :catalina
     sha256 "985df42db7e61d0fc3b5fcb4228b0de86bbdebeddfb716e996c552d87ac83d7e" => :mojave
     sha256 "9d7f49fb217514118c62e3437960955a6ce1acc3925a2cdb561f2861a0355545" => :high_sierra
+    sha256 "c6de1387ef1c5d7ef33a58179cdbab0e0601128597545db8c8f92f4ea0db7b3c" => :x86_64_linux
   end
 
   def install
