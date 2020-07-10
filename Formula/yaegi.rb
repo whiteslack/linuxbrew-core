c6b20c340dd1f2c67294ae3a461a02c6a1899cb8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-10 16:53:24 +0000

    yaegi: update 0.8.12 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -11,6 +11,7 @@ class Yaegi < Formula
     sha256 "bf45292147ea66f36838dc93f1360a8b72a79762c707534e06cd5733c5d17b50" => :catalina
     sha256 "7d44dda2f0ef5548de5d7c4981b1f559a4817590fd6ce1912c98efaf1065490b" => :mojave
     sha256 "ab2308bd0be899ca01d5e1b5eaef2d6a580b50fa5cba7a80b5f231cbae57e561" => :high_sierra
+    sha256 "5b9d3e56c7353e0fe33782c16c313f5191fdf79141473eb25a243abf08023497" => :x86_64_linux
   end
 
   depends_on "go" => :build
