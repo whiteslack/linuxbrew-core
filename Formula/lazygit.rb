fa1d0f75eece61ef0f39bc4fed786e8deb9cb3dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-10 17:44:23 +0000

    lazygit: update 0.20.5 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -10,6 +10,7 @@ class Lazygit < Formula
     sha256 "3b966aedf1215797f6dc81ceed0d1621c6f8103b45c5f0066594fcb9f34db383" => :catalina
     sha256 "39bed96de6223fd46770bdfcd8140cac5c35ae3ae58ec8802fc8904063d3dbb7" => :mojave
     sha256 "6bca7f61de13a5f4847e17d1e0a707ae8af8b9708f55ba452c93359688f2a1dd" => :high_sierra
+    sha256 "98924e95d7b102c3524be5442c63dc75c886d372d2c005bf3f2d71f40cfa7375" => :x86_64_linux
   end
 
   depends_on "go" => :build
