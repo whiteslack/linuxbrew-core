5981a72da141b6b4116808478990e05ee50fe429
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-13 13:57:21 +0000

    neko: update 2.3.0_2 bottle.

--- Formula/neko.rb
+++ Formula/neko.rb
@@ -11,6 +11,7 @@ class Neko < Formula
     sha256 "a2c2e95b27fbe6a15ce0efc03a40655e2a283e3be08acdb0cc398a9367ec76a2" => :catalina
     sha256 "bb0f7ece136bfa89ac5d690f936f3ba0b34bd8e3a256f73260297e2a5f8e67eb" => :mojave
     sha256 "7697cb00ffbca3583c0633d042959beda280c6a5e9c3b802d0c14883a1cead88" => :high_sierra
+    sha256 "ab1fe2e7b70d180c7f306a993fd955db62ec19f55f1f7e3535d53f2639e4640b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
