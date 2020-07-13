0bbf4e71c9a60ccfd44547516f979ba558b333cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-13 12:27:23 +0000

    meson: update 0.55.0 bottle.

--- Formula/meson.rb
+++ Formula/meson.rb
@@ -11,6 +11,7 @@ class Meson < Formula
     sha256 "a2f25cb6f38a6e9d8dad19da2420d8efb1641e9ef2e2e5fa8f473e8707ba8bde" => :catalina
     sha256 "a2f25cb6f38a6e9d8dad19da2420d8efb1641e9ef2e2e5fa8f473e8707ba8bde" => :mojave
     sha256 "a2f25cb6f38a6e9d8dad19da2420d8efb1641e9ef2e2e5fa8f473e8707ba8bde" => :high_sierra
+    sha256 "beadc40642df1c9198e6106d8536f173869274c7f44f9a822d0772557dd21129" => :x86_64_linux
   end
 
   depends_on "ninja"
