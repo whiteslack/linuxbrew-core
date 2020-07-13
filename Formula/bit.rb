28e0f3883e8822830ebfe0e5e19b3ea40f19ba53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-13 14:05:08 +0000

    bit: update 14.8.8 bottle.

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -11,6 +11,7 @@ class Bit < Formula
     sha256 "bc1b85c6100f4c5166eda34de5a92b66d73f45336536ed08921926dbb90ef6d8" => :catalina
     sha256 "c9fe18470becb44f6580e36bd3e9bc52219a1d4f111d271382942304c435cd86" => :mojave
     sha256 "2e2f871d7759adb7d2772a8ec319c3762c3e54e58625172f4ad44132cbdf3b2b" => :high_sierra
+    sha256 "f8b61647b1d0bbd903b2503d96b1618ba6c8a298dd523dae133d33b7d7c3a093" => :x86_64_linux
   end
 
   depends_on "node"
