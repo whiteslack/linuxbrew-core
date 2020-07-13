e4184dd247921fa0d71169f9961258ede09b7890
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-13 12:13:17 +0000

    sshuttle: update 1.0.3 bottle.

--- Formula/sshuttle.rb
+++ Formula/sshuttle.rb
@@ -13,6 +13,7 @@ class Sshuttle < Formula
     sha256 "f44682243fd2b0ffd1f463f8eca6a93a0575b8b52e27d14c2bbb0d5cdee72f32" => :catalina
     sha256 "4a16f0170fc41f77af6a8994dc587cc5510de7be1b7fab4ad5968fb6d402587f" => :mojave
     sha256 "d466f1fb7d5dd3ae8f2ceec36a5d2348c124c25847db7d7156db59554c81ea44" => :high_sierra
+    sha256 "12929138ce89044466d8671551a7d0869905541834d6cfb3fdd1d895186513b8" => :x86_64_linux
   end
 
   depends_on "python@3.8"
