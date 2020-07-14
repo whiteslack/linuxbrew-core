758799deb1b554bbda85ab7129bb191d53cec296
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-14 08:23:38 +0000

    nnn: update 3.3 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -11,6 +11,7 @@ class Nnn < Formula
     sha256 "0974658c7c0315c30083f3db92cc37b34bbc2600c4b80392c6fa0124eb96509a" => :catalina
     sha256 "0fb61b265b7f5a99ce30b930141531b55d789959de46d625b209c2238529df33" => :mojave
     sha256 "5c04648426900cf44dbf1207f0fd36f8a003e30a6362a6606ae7a9bb58a495b0" => :high_sierra
+    sha256 "c7f53a67e550622ebf91ea529a9835ef7634e41ca4367e302242ed216b3f5268" => :x86_64_linux
   end
 
   depends_on "readline"
