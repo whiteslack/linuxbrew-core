9321536022e74832e0b1c3941a15ee6511222b1c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-17 10:53:18 +0000

    scamper: update 20200717 bottle.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -10,6 +10,7 @@ class Scamper < Formula
     sha256 "2777883e811d43e44eeb5b749ba5fed240fb771951a55fd31ba1a3fff378c440" => :catalina
     sha256 "071b43792714b1cef71af5921fb3b4ffea71c7b6df69361762d1588ee255653d" => :mojave
     sha256 "b3169722c61bc6f10acef02e6b22533ffc9704598cb9923c3f2f58b6f91accd2" => :high_sierra
+    sha256 "659ed19152f6060cc6d5dbc438040a30a6f8cf3121f918d287afaa66c98f9225" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
