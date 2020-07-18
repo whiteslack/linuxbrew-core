66e78e5d08c7e3edeebe4d8d68e85d1b88f80a40
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-18 11:14:20 +0000

    fswatch: update 1.15.0 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -9,6 +9,7 @@ class Fswatch < Formula
     sha256 "77233b7d6c11644f14682862d613ed37a5eda86ba1ec5a6ea3c18b75ccafe906" => :catalina
     sha256 "2602ae4b5b43cb63ec6a249bdf8b1e81b124ae18392f01a2a8ebf0faec1ac5ff" => :mojave
     sha256 "057894d713ee24ea5e64c0db014e1593cee8eb253bbb962c68a6426a25f39c9e" => :high_sierra
+    sha256 "84bff95c09f1b363beb9708b675cb01d45dda875fe62e8907bd58839c50cabc7" => :x86_64_linux
   end
 
   def install
