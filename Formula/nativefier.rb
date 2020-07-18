f0bf0286b66cfe67f9dfbb9eaf398c7ccf8a200c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-18 21:15:01 +0000

    nativefier: update 9.1.0 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -12,6 +12,7 @@ class Nativefier < Formula
     sha256 "31fe1e9e28f7d558b7da6a49a8e43511dc13cd93dd062503ce62e85ae777e438" => :catalina
     sha256 "65079bebe1c8b2095b9c9e6274a24ad25dc90a891cd9d57c6201928035f8d7eb" => :mojave
     sha256 "63d3c91e07a6141d213f57f4b4143ed994a62b628999eeee5bce68615ec81718" => :high_sierra
+    sha256 "f6e287c59c14a4b79dc05944d482203e408f673b4647d2d79ef5549b49a5d392" => :x86_64_linux
   end
 
   depends_on "node"
