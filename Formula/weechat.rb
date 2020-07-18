66e17d7ebaa57a939c9807208e85e5b80a9a7feb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-18 21:27:14 +0000

    weechat: update 2.9 bottle.

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -10,6 +10,7 @@ class Weechat < Formula
     sha256 "27597527a70b72960498c972a98837be24b8b7e2751e6b9cdacaf436eacea671" => :catalina
     sha256 "cdd18f140fe07c6244beea285942e6e507dea191e55bda661dcff3b12ec5c46a" => :mojave
     sha256 "a6db2ccf286b42557ca86fed6927f87053ed9c083090bd8a25bfbc68e7fa63cd" => :high_sierra
+    sha256 "1e7a609307b53593a207ecad500cc2b8b2edba0d4e4ed4c08059161d9cf7ca1a" => :x86_64_linux
   end
 
   depends_on "asciidoctor" => :build
