bac7fefcbfe8131d3b35cc364ab54b51af351e79
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-18 21:20:21 +0000

    broot: update 0.19.1 bottle.

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -11,6 +11,7 @@ class Broot < Formula
     sha256 "35cacc4caebb27f9da5f647d7fe373ae8813314d1deb080a5aee18bad5741999" => :catalina
     sha256 "df06367fd7c2c650e5dccf1c67df86f46ffbe1eb6cc228ee3ed6edeebdcb1271" => :mojave
     sha256 "ea668e1b6e562711f42bbec31102ffdaae74c465d75cddf6e8fbc4aa03684b4e" => :high_sierra
+    sha256 "1f0298321dd81bc46596f542df305a941ac86b21068f717e5a0d352d1ba7faf5" => :x86_64_linux
   end
 
   depends_on "rust" => :build
