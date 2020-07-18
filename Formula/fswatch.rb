79b7718ed526af40837052ea318b9a693a4d3a89
Michka Popoff <michkapopoff@gmail.com>
2020-07-18 13:13:38 +0200

    fswatch: Fix bottling for Linuxbrew (#20757)

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -6,7 +6,6 @@ class Fswatch < Formula
   license "GPL-3.0"
 
   bottle do
-    cellar :any
     sha256 "77233b7d6c11644f14682862d613ed37a5eda86ba1ec5a6ea3c18b75ccafe906" => :catalina
     sha256 "2602ae4b5b43cb63ec6a249bdf8b1e81b124ae18392f01a2a8ebf0faec1ac5ff" => :mojave
     sha256 "057894d713ee24ea5e64c0db014e1593cee8eb253bbb962c68a6426a25f39c9e" => :high_sierra
