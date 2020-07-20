ce05e044e27d0af6322edcbeb8a1ce6c5d7f7e65
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-20 19:08:35 +0000

    gh: update 0.11.0 bottle.

--- Formula/gh.rb
+++ Formula/gh.rb
@@ -10,6 +10,7 @@ class Gh < Formula
     sha256 "ee4b879f06935d40035318d4189d5c057e53c38c53e5d4e28ec6b458bd98ce22" => :catalina
     sha256 "85cc073f44cbb43bd87e171fa505a770822e4655b5c92c2424d7aeac73fae727" => :mojave
     sha256 "34074c761bd6200fd5d5a61e23434836e487febc0bee3336c93e8a1c4864715a" => :high_sierra
+    sha256 "b717b917cb5f32a161f5dc1e23f8d759ef044293efdc23e5f8a60737c5a73a1d" => :x86_64_linux
   end
 
   depends_on "go" => :build
