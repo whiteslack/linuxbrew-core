4f219782b7886f8f9f0edfefe1016f159ad81baa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-21 21:14:39 +0000

    appium: update 1.18.0 bottle.

--- Formula/appium.rb
+++ Formula/appium.rb
@@ -13,6 +13,7 @@ class Appium < Formula
     sha256 "d6d0adf1f5b4380cf09ad9571e708f51e7d3f5c99307eafd5411dc7318ee03b9" => :catalina
     sha256 "6e1fc2e7528234ec093d77933030e796accdd927e58fed2869e9b91f9951590f" => :mojave
     sha256 "cc6f096c1464b401f52b6808eb8fe3d71975bb9c83594fcc7a0dc67551abb5e8" => :high_sierra
+    sha256 "fe7c733450d8cd694f23ada108efdb36d8ddf5161eb2b799a83534fccbf7f356" => :x86_64_linux
   end
 
   depends_on "node"
