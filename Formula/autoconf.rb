3f36becf5587f859f0372de4105297baa840bd92
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-21 20:56:37 +0000

    autoconf: update 2.69_2 bottle.

--- Formula/autoconf.rb
+++ Formula/autoconf.rb
@@ -16,8 +16,9 @@ class Autoconf < Formula
     sha256 "a76fca79a00f733c1c9f75600b906de4755dd3fbb595b1b55ded1347ac141607" => :sierra
     sha256 "ded69c7dac4bc8747e52dca37d6d561e55e3162649d3805572db0dc2f940a4b8" => :el_capitan
     sha256 "daf70656aa9ff8b2fb612324222aa6b5e900e2705c9f555198bcd8cd798d7dd0" => :yosemite
-    sha256 "d153b3318754731ff5e91b45b2518c75880993fa9d1f312a03696e2c1de0c9d5" => :mavericks
+    sha256 "a884e59c2b63ff27d2a76a7e343d7dda0464a72e88fcc8cc7b520c3e52e4d61f" => :x86_64_linux
     sha256 "37e77a2e7ca6d479f0a471d5f5d828efff621bd051c1884ff1363d77c5c4675e" => :mountain_lion
+    sha256 "d153b3318754731ff5e91b45b2518c75880993fa9d1f312a03696e2c1de0c9d5" => :mavericks
   end
 
   uses_from_macos "m4"
