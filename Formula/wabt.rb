01e06fb27afea00817275a103524a37d7d73f63a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-22 20:15:54 +0000

    wabt: update 1.0.19 bottle.

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -11,6 +11,7 @@ class Wabt < Formula
     sha256 "60d464680e3e70f9db8e079d1e6cb4d45f9c9994025cb3ba0f2b3a98c422f132" => :catalina
     sha256 "e19ec65d36ef68a8f5637dcc8d9728642fc0887e9f8655b912ec906c1e686d34" => :mojave
     sha256 "992e3b6ca47b54df5755d37e5968f15a2dda4070ec1f94d96c1e9a82828289d6" => :high_sierra
+    sha256 "00da57f8a20db1714b9426c4e5b7eaded413b38f87b6ee3370f8340984ad7bb4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
