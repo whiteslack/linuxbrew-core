c41a3626b5d070a43a28ce61eb31739eccdf0232
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-23 09:29:16 +0000

    libupnp: update 1.14.0 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -10,6 +10,7 @@ class Libupnp < Formula
     sha256 "f687168c37ba09bb0ee06b2d0d373c6de2f8d5511c563191788e45282fd104af" => :catalina
     sha256 "b7e3d1d7fc0a4011e093f040f71578f549e06b7702d9ec29c9595fb11fa206f6" => :mojave
     sha256 "70340bed620fd9c537881d58a6b0f720689fbf0ebf9dbfa9acfdb846190e9c69" => :high_sierra
+    sha256 "52f5c389900d34422b43d3c73c59f166d4fe84b8f441ab4552a84bdd2308f92c" => :x86_64_linux
   end
 
   def install
