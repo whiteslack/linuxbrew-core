4b1a7e09bf8d9e49a6169656742c41e90c187750
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-25 08:14:58 +0000

    nspr: update 4.27 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -10,6 +10,7 @@ class Nspr < Formula
     sha256 "a9ad990acb59b9578b81d9875d3bdc7633ce21552af014ef77e8b0d07ab94983" => :catalina
     sha256 "f5bd7b904233ba05c23bb33ca290ce14605999f8ae97a8eb12c2c06320a39772" => :mojave
     sha256 "5da91b9a45b0ff47d1ff9cf0c77295b68029a73b0cb71cde25d3f632443c9ee7" => :high_sierra
+    sha256 "712c98aa7cac0ff820896abbe5718123d3899e279901fe24513495c45533dace" => :x86_64_linux
   end
 
   def install
