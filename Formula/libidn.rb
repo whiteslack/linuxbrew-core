b33e67de2df1ec2b86c76ce0901eb67168a436f3
Michka Popoff <michkapopoff@gmail.com>
2020-07-26 08:32:12 +0200

    libidn: fix bottling for Linuxbrew (#20796)

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -6,7 +6,6 @@ class Libidn < Formula
   sha256 "14b67108344d81ba844631640df77c9071d9fb0659b080326ff5424e86b14038"
 
   bottle do
-    cellar :any
     sha256 "1c1767101241edbd4141dc100e1c715b021be85e3fcf3657ea3bbdcb1fa884ec" => :catalina
     sha256 "61e978964009ca11bb25bb366f47872b306a54bbec468f0eca4924a8155bc8a4" => :mojave
     sha256 "306d665a4722e8c32da822c5185eba3abfa2ca0f0624e67e28549c44fb83f9f4" => :high_sierra
