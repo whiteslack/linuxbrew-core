75350cdfd64da4aeac4e82cdeb77d1355cafa5d1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 11:26:47 +0100

    gtk-doc: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -44,7 +44,7 @@ class GtkDoc < Formula
     system "make"
     system "make", "install"
 
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
