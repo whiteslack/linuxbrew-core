37ed3a0773a0334fcbd04feb100d56f112bf022d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-27 17:20:23 +0000

    serf: update 0.9.3 bottle.

--- Formula/serf.rb
+++ Formula/serf.rb
@@ -12,6 +12,7 @@ class Serf < Formula
     sha256 "c716a0e85a67699c6c83e3cb178ece969d94ec7aec6f36ec60e4b65e6093258d" => :catalina
     sha256 "e56b01a8475e3c056233631945ad4d3710dc746c048c09679835f795cbf371f2" => :mojave
     sha256 "fd50cd7495843099f7895ad8f0ed25537cbc853a03555a4c2bf375b2c1b53df9" => :high_sierra
+    sha256 "b1bf71423e797f3f0e99a0a7a7cc35233391679d65d22778d0a37dc71bbc7c52" => :x86_64_linux
   end
 
   depends_on "go" => :build
