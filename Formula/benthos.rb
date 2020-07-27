b2b475b4585a9f8c39f81ff80894eb1cf3c13195
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-27 21:26:02 +0000

    benthos: update 3.23.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -10,6 +10,7 @@ class Benthos < Formula
     sha256 "9796a7003050bc4f2ebb07e4b10f84b3dac4cd2ab8b4a77c7f5b01d4821cb869" => :catalina
     sha256 "02cd1f129608bc4f608c9bee3316bbf7b4ca10b7431beda95a9f13843293b946" => :mojave
     sha256 "5aa55b68f0138270ad7679dce9392fb0d053c0275f729c91faee2daadcc79b37" => :high_sierra
+    sha256 "2c3b0244edfe16d32437f85e15813a7475b96520a1243680722d3d6e3e4bc277" => :x86_64_linux
   end
 
   depends_on "go" => :build
