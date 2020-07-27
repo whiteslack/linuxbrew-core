13b7130ca8f709a4f117317dad785608ca6ce28a
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 11:29:04 +0100

    serf: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/serf.rb
+++ Formula/serf.rb
@@ -2,8 +2,8 @@ class Serf < Formula
   desc "Service orchestration and management tool"
   homepage "https://serfdom.io/"
   url "https://github.com/hashicorp/serf.git",
-      :tag      => "v0.9.2",
-      :revision => "5642cc7572cebea332176ca3024bec4b3474a11a"
+      tag:      "v0.9.2",
+      revision: "5642cc7572cebea332176ca3024bec4b3474a11a"
   license "MPL-2.0"
   head "https://github.com/hashicorp/serf.git"
 
