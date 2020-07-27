629e138ba283e3f0cd1fc153228d77105e1781be
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 11:28:06 +0100

    nef: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/nef.rb
+++ Formula/nef.rb
@@ -9,7 +9,7 @@ class Nef < Formula
     sha256 "db1f3b419794b096f913f805f08b56e0f51cb01cfa20d4a8bfd77cc6ae1f236c" => :catalina
   end
 
-  depends_on :xcode => "11.4"
+  depends_on xcode: "11.4"
 
   def install
     system "make", "install", "prefix=#{prefix}", "version=#{version}"
