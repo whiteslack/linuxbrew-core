8c764e4d827c316d0d8b18896e67b7248a3b4af0
Mike McQuaid <mike@mikemcquaid.com>
2020-07-27 11:26:07 +0100

    drip: fix RuboCop style.
    See https://github.com/Homebrew/brew/pull/7867.

--- Formula/drip.rb
+++ Formula/drip.rb
@@ -17,7 +17,7 @@ class Drip < Formula
     sha256 "048944c8240d219b4777e4f35bb7d456062042385e6b2a375f2d00d1b9d517f9" => :x86_64_linux
   end
 
-  depends_on :java => "1.8"
+  depends_on java: "1.8"
 
   def install
     system "make"
