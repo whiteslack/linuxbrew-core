610b8883808a27b64279fba94cd9137577e5cabe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-28 21:11:11 +0000

    aws-google-auth: update 0.0.36_1 bottle.

--- Formula/aws-google-auth.rb
+++ Formula/aws-google-auth.rb
@@ -14,6 +14,7 @@ class AwsGoogleAuth < Formula
     sha256 "1d6890e0d3fd5e514d91db206c47b798ee5c5411a25cf2cd2eda8cf4abfc4806" => :catalina
     sha256 "a1da781b800a1a533e2b6afd3443f77b04a952da872320732c16b1bab858a4a5" => :mojave
     sha256 "371f6acaf6ce534b1a7d6569ea5fb02d05a0da5818b6f0e7097edb91ebdc5a66" => :high_sierra
+    sha256 "aefbd042882fc53d68311a39f0b4e1b6d1670e7d0f1c9e125dcba71516591d78" => :x86_64_linux
   end
 
   depends_on "freetype"
