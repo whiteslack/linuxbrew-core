2d5aec80f1c67b3d7108adbbdb8901dd476f3b68
Michka Popoff <michkapopoff@gmail.com>
2020-07-28 22:43:05 +0200

    xxhash: revision bump (#20830)
    Due to
    https://github.com/Homebrew/brew/pull/8112

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -3,6 +3,7 @@ class Xxhash < Formula
   homepage "https://github.com/Cyan4973/xxHash"
   url "https://github.com/Cyan4973/xxHash/archive/v0.8.0.tar.gz"
   sha256 "7054c3ebd169c97b64a92d7b994ab63c70dd53a06974f1f630ab782c28db0f4f"
+  revision 1 unless OS.mac?
   license "BSD-2-Clause"
 
   bottle do
@@ -10,7 +11,6 @@ class Xxhash < Formula
     sha256 "06ea145c49451bf37f2a73139100f436b4bf9f275b77b9dadcb5d36a7b07fae1" => :catalina
     sha256 "df75758d4b4756b23530ec54e2289148652e3f54d6f9a8e45c43f662bc69d7c2" => :mojave
     sha256 "821f8c8df3ada242236f2f231ae72cdcaf23412a5e22458c23df453631129300" => :high_sierra
-    sha256 "017065d6940389966074bbd3ce91361a43d8f681463cd3b3b5baf3be289cfd85" => :x86_64_linux
   end
 
   def install
