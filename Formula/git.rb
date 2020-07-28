e2f940e7c1a86f37b4616271708383da0de8d87d
Michka Popoff <michkapopoff@gmail.com>
2020-07-28 18:48:18 +0200

    git: revision bump for rebuild (#20828)

--- Formula/git.rb
+++ Formula/git.rb
@@ -5,6 +5,7 @@ class Git < Formula
   url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.28.0.tar.xz"
   sha256 "dfa5d1a253aa451465478fe45c6a40ab8605b340fdb4c4e80b16d7f87708439d"
   license "GPL-2.0"
+  revision 1 unless OS.mac?
   head "https://github.com/git/git.git", shallow: false
 
   bottle do
