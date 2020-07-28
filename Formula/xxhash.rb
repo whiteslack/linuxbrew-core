72c331d59a76c55ecab28dbdad96727acae10b94
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-28 20:45:19 +0000

    xxhash: update 0.8.0_1 bottle.

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -11,6 +11,7 @@ class Xxhash < Formula
     sha256 "06ea145c49451bf37f2a73139100f436b4bf9f275b77b9dadcb5d36a7b07fae1" => :catalina
     sha256 "df75758d4b4756b23530ec54e2289148652e3f54d6f9a8e45c43f662bc69d7c2" => :mojave
     sha256 "821f8c8df3ada242236f2f231ae72cdcaf23412a5e22458c23df453631129300" => :high_sierra
+    sha256 "c1bd2046c7055bc4d321b40336e00ba6d30cabce4bdfd84aea766f4758f4a4d3" => :x86_64_linux
   end
 
   def install
