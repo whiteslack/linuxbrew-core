16df35d5eb24246ba55b97817178502273ef8868
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-07-31 13:05:40 +0000

    kapacitor: update 1.5.6 bottle.

--- Formula/kapacitor.rb
+++ Formula/kapacitor.rb
@@ -12,6 +12,7 @@ class Kapacitor < Formula
     sha256 "45d4043ba983183baffd30a4316cc18f0da3c2b432be37e542c9d15de49dcfa4" => :catalina
     sha256 "41f6340ac74e2c46544fbad969ae1e098f66bfb565676e12a90b3e4746a4b877" => :mojave
     sha256 "ac353ad7fa8f5fcbc8b522858eee05fb2e193b4678e9b4d168df2d3e08e406e8" => :high_sierra
+    sha256 "4e9699b8b043b6e2d101705e1d610e8bbaa9ebceb4c2bcd1a0f0731d19705d1c" => :x86_64_linux
   end
 
   depends_on "go" => :build
