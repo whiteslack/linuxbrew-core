777b36bbe4b525badcb0256d70e63d0a356de622
Jonathan Chang <jonathan.chang@monash.edu>
2020-08-02 11:42:50 +0000

    cryptol: update 2.9.0 bottle.

--- Formula/cryptol.rb
+++ Formula/cryptol.rb
@@ -14,6 +14,7 @@ class Cryptol < Formula
     sha256 "0def1bdbc98588cd583352d774d31402ee01b1bfc980391ad035dc2c78625e5d" => :catalina
     sha256 "37883057748d5d1fa952bd76430956821a9cd92a254c3ac58d87ef72152edfdf" => :mojave
     sha256 "b2afda7c0df0359122b35280a3bd659d8f2697fd4b51677c3834e01dc67e4e30" => :high_sierra
+    sha256 "2a99b7966552b81df2d95455ad305df3768e086e418ada805a0f89aa2edd727a" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
