59d526c50fbf33b1a1c927d449030fa7baf52b02
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-04 20:06:35 +0000

    janet: update 1.11.3 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -11,6 +11,7 @@ class Janet < Formula
     sha256 "bd57a023531328a0c1f0d30f9a67e1b25280590f0549bf60afba51206ce2f30b" => :catalina
     sha256 "029ee62ab7e7168f484c872a6c1603d7ce1a64a16421d4ce1f49402e97f77edf" => :mojave
     sha256 "bffb15b4c6df0df9158cde841d05c9444e9d166fcc42391c089bd325fd0ce04f" => :high_sierra
+    sha256 "889c52df94ef53e3ff02bdef23b0b43969a3b69b549b3d35548e6dc76580e7a8" => :x86_64_linux
   end
 
   depends_on "meson" => :build
