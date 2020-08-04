959f1c28ff4328d65b4094dde4fd8aee9f1b61e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-04 05:35:30 +0000

    cobalt: update 0.16.3 bottle.

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -10,6 +10,7 @@ class Cobalt < Formula
     sha256 "05ca26b8e2ae6c7f10f6e52ad37027d8d3990092a37e78d3a2475bdda2fa51f7" => :catalina
     sha256 "52b2a35136119708ec6598dbf8b964ca5cdc71bd955a04bbeda4a9dc45741e71" => :mojave
     sha256 "5001ab4d12aed7498b3bd42cad67cc75c6e8aecdb6fcde205b0a7c989ce260eb" => :high_sierra
+    sha256 "45d47870eec516f1eb260ec5057cfc944bd9cdea18c07e518ac3ad3dca46c47d" => :x86_64_linux
   end
 
   depends_on "rust" => :build
