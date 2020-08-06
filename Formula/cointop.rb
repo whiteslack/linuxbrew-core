4fdac8a8ccb335811d42e01a180693dbaaf13465
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-06 20:56:27 +0000

    cointop: update 1.4.8 bottle.

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -10,6 +10,7 @@ class Cointop < Formula
     sha256 "b0febcd7ed6ca56d2e120c5ba225a293fd564cfe43e582ee8295ddd0240ad30e" => :catalina
     sha256 "e24e0fd427f8eab96fdeb1e90349a776ababcab6346b695495b0d2cb80d52311" => :mojave
     sha256 "1374a8fa5bce92d2483138833540661538d6d1f99e2fced94d7b12ad27b9b604" => :high_sierra
+    sha256 "6e858033052c97ff4c2ec27ff3fdea2f40d28581754131215b63f5b948ef2297" => :x86_64_linux
   end
 
   depends_on "go" => :build
