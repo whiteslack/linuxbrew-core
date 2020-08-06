328e6b2cd2129a46fe6a9b6232657672a97e3fdf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-06 20:57:20 +0000

    mercurial: update 5.5 bottle.

--- Formula/mercurial.rb
+++ Formula/mercurial.rb
@@ -11,6 +11,7 @@ class Mercurial < Formula
     sha256 "99cd39e3f4bd9bbfc94b191d4c73276da4e174f0bd4043de96737c2b91c1bfe8" => :catalina
     sha256 "a0e0763833b37b8731e2555c99c7a96825df7ab2c978f912b4bb17b87d0f8bcd" => :mojave
     sha256 "5477a3357f4e2345d626cb5dfe5f090f3765d24100aec70af9c958c5fd6338c0" => :high_sierra
+    sha256 "76f6ec4a0c1d9bf02be3d28c5471c573a2872d17350009ccdd8164c42cabc7a3" => :x86_64_linux
   end
 
   depends_on "python@3.8"
