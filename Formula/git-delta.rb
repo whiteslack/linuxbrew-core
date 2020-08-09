b56c02d6199bd76d7b4b985092227ec187111b20
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-09 17:17:31 +0000

    git-delta: update 0.4.1 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -11,6 +11,7 @@ class GitDelta < Formula
     sha256 "d963089b08030f60e813d1d42ba505ee0874a40050d8ca9fc0dd3e1772b464cf" => :catalina
     sha256 "c09ae0331cccd3007eb3b0420efbd78008a252b19048ea359c76a524c5207a3a" => :mojave
     sha256 "9e3514f8c1203ab8119c3a670ada98b08f30139c2308e920cdb131c26e7e66fb" => :high_sierra
+    sha256 "8fa6da29eec6618d4ca4f0e80f84dc0f2fd03c798c216b42e67f6f2f57af3b7c" => :x86_64_linux
   end
 
   depends_on "rust" => :build
