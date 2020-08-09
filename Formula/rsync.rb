c57b4e305d8acf674a8ba8e6d007813c13485bac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-09 13:00:29 +0000

    rsync: update 3.2.3 bottle.

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -12,6 +12,7 @@ class Rsync < Formula
     sha256 "fd92045e16b9b83ab6c281a1e658e27731ef9af53fcb6bb7ce6b9533851e728e" => :catalina
     sha256 "1be1e9754c4f4a4b043aece33299d90f50d01274682f63c29eca7d9bcb8a2090" => :mojave
     sha256 "5cf9c6e0014687c4abb4044e7f5e12d5d453b81c5dbadd472438b4131c655a1f" => :high_sierra
+    sha256 "7cb96b93ffe5ad5e3473032a4f869861fc2305738795e95a21f41a3fcfdc24c2" => :x86_64_linux
   end
 
   depends_on "lz4"
