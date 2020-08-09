1aa5650f47b12efe5cf3cb132e3d91d3997e5896
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-09 15:18:21 +0000

    mruby: update 2.1.2 bottle.

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -11,6 +11,7 @@ class Mruby < Formula
     sha256 "b6a638c04a991a249a737d0ad0d9f7fac31d35a7b2fd3c8507304e68f13bc983" => :catalina
     sha256 "1f31eadc8801f65d42e2cfec43cda336324daf86978529dfc76338e6b094b16c" => :mojave
     sha256 "5b5dca177d9fdd6a2b543c9aeb2117e0d112d1578fadbb709d8565d83b21d6a7" => :high_sierra
+    sha256 "e0a33a84718e9fcdf9478790564efb353120d8ea0e5d798766cee648c2f743b0" => :x86_64_linux
   end
 
   depends_on "bison" => :build
