a5e267c11f7dac6a7819970de83a255604d1497c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-09 13:02:37 +0000

    fmt: update 7.0.3 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -9,6 +9,7 @@ class Fmt < Formula
     sha256 "ecc628fabb67ff7f03a9fbe916ad629e30e6add398c84750c0af55ea5b261bd7" => :catalina
     sha256 "a037d74f6005e7df7b7f080b3a3f643c229e315dec0e948851f64cadaed4764d" => :mojave
     sha256 "6e5496f417d00981661d6d716125cde6b7e6502827735d3c0d726fc63407b283" => :high_sierra
+    sha256 "cec4a68de6832a1fbb86e87a35a20e87634511f8664ab8127382343e8aa2e4ee" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
