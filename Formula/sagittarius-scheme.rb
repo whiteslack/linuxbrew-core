8c0a100e951e59ec1733ca76c614da15c9b1d07c
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-08-10 19:12:05 +0000

    sagittarius-scheme: update 0.9.7 bottle.

--- Formula/sagittarius-scheme.rb
+++ Formula/sagittarius-scheme.rb
@@ -10,6 +10,7 @@ class SagittariusScheme < Formula
     sha256 "7199d76824cd0de62da70f2eff3db66f0944c2851e4ee868359b7bdbe4685994" => :catalina
     sha256 "d350853fda37321efe29a14f988b3039126559e489b2fb93a41ca538ca7a2f29" => :mojave
     sha256 "8824ccf2baa439cc953d8b373010e5f00fabb51dd6837f68323993b69549bb84" => :high_sierra
+    sha256 "ca2374ff9eb5e9225763f1fdbfa3512dcdbcf7bde79e238f8282b74a2e537fda" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
