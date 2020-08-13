0cdaa78b8c3b5dfde7a4f1a641e7603de4528927
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-13 20:13:56 +0000

    minikube: update 1.12.3 bottle.

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -12,6 +12,7 @@ class Minikube < Formula
     sha256 "eea174b37eb1d6a3f740d0d584f5f30a22895c178d2617a61edd37e77e1dace8" => :catalina
     sha256 "1620f78a138602df2ef42bffde3e9bef7c7df3de3884f2818864f45be8b651c0" => :mojave
     sha256 "00b35e0cd6042a3a794ef11c273a7ee4a4770e11c71ec01ca123793377721c45" => :high_sierra
+    sha256 "7df4b5518fa65b2e0f2b522aa6d9c13425c868db39baed24b492f718e59f0409" => :x86_64_linux
   end
 
   depends_on "go" => :build
