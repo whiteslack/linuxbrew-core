8f73c59d2f5bea127711f647634028bf02f2a0ba
Michka Popoff <michkapopoff@gmail.com>
2020-08-14 23:56:08 +0200

    fetch-crl: remove duplicated resources

--- Formula/fetch-crl.rb
+++ Formula/fetch-crl.rb
@@ -13,33 +13,6 @@ class FetchCrl < Formula
     sha256 "a78a25752c9674989dfb237cd291422a30d115e7f21f7945600d5807de0a6e87" => :x86_64_linux
   end
 
-  unless OS.mac?
-    resource "LWP" do
-      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/libwww-perl-6.43.tar.gz"
-      sha256 "e9849d7ee6fd0e89cc999e63d7612c951afd6aeea6bc721b767870d9df4ac40d"
-    end
-
-    resource "HTTP::Request" do
-      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Message-6.18.tar.gz"
-      sha256 "d060d170d388b694c58c14f4d13ed908a2807f0e581146cef45726641d809112"
-    end
-
-    resource "URI" do
-      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-1.76.tar.gz"
-      sha256 "b2c98e1d50d6f572483ee538a6f4ccc8d9185f91f0073fd8af7390898254413e"
-    end
-
-    resource "HTTP::Date" do
-      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Date-6.05.tar.gz"
-      sha256 "365d6294dfbd37ebc51def8b65b81eb79b3934ecbc95a2ec2d4d827efe6a922b"
-    end
-
-    resource "Try::Tiny" do
-      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Try-Tiny-0.28.tar.gz"
-      sha256 "f1d166be8aa19942c4504c9111dade7aacb981bc5b3a2a5c5f6019646db8c146"
-    end
-  end
-
   resource "LWP" do
     on_linux do
       url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/libwww-perl-6.43.tar.gz"
