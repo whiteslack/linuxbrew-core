0355f2913985315d722e67f6f3228ecda2d32b36
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-18 09:57:11 +0000

    virgil: update 5.2.9 bottle.

--- Formula/virgil.rb
+++ Formula/virgil.rb
@@ -12,6 +12,7 @@ class Virgil < Formula
     sha256 "841082fa11c796ba0045d4ced3cead342fba308b049f07db4a0bd3309acc08c7" => :catalina
     sha256 "d115016c280fbfe9381b56d0e08b9a69b4dc62042bb73424c243ea3f73280cd9" => :mojave
     sha256 "f7b6c179875ab30f849e3cbba53c8aeed7af4c569b69d4b112c2d749e5c38ea4" => :high_sierra
+    sha256 "818fae592fbf7584c0d9db6bb16daba6355ba6a966205147c1e8672dd31476d8" => :x86_64_linux
   end
 
   depends_on "go" => :build
