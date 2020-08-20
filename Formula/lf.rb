43432cff3300c8ac0d00e4ba3ef486d8ce353bf4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-20 15:19:28 +0000

    lf: update 16 bottle.

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -10,6 +10,7 @@ class Lf < Formula
     sha256 "a1bb783c227f7c75a701d786fd0ffadd577ef09f4fe1aef0125e1f03a81ba106" => :catalina
     sha256 "92bbce3dd69010267a5ff66cd3472494ae9e11e7661fb0089bb44fb97fa24d77" => :mojave
     sha256 "05f7e48eccdff3dd0d2fb35ef82d7e9596e4a57b5b37c672e2f2e5a06af080fb" => :high_sierra
+    sha256 "df5df760d3129ca6d757c3df85bb28df0884703be7105ea757e9ee13fc14de98" => :x86_64_linux
   end
 
   depends_on "go" => :build
