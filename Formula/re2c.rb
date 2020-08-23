6e1093f9faa652e484d2465015cb7344e9265653
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-23 13:52:24 +0000

    re2c: update 2.0.3 bottle.

--- Formula/re2c.rb
+++ Formula/re2c.rb
@@ -9,6 +9,7 @@ class Re2c < Formula
     sha256 "9c833a1f7a3c31b73a18bb63a76bfedcb6645242aaf6d567cd0d8603d8ac47a2" => :catalina
     sha256 "3ed464eb3353504f1a931912c498b3faa8289d1842a9e60055c1483cc2e76bd4" => :mojave
     sha256 "62a37784b94c5406ed7c4bcd20d76836dff43d3fb90798a144d9408678ddc71e" => :high_sierra
+    sha256 "263991bf98fe57b25b791855cf8629aff1f8759eaf506374c062bddbdb1d336d" => :x86_64_linux
   end
 
   def install
