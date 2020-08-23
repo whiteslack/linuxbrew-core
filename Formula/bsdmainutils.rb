5238e7c864d626e0c1b02c1cb50f5048e8e66214
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-23 16:07:26 +0200

    bsdmainutils 12.1.7
    Closes #21010.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bsdmainutils.rb
+++ Formula/bsdmainutils.rb
@@ -1,19 +1,16 @@
 class Bsdmainutils < Formula
   desc "Collection of utilities from FreeBSD"
   homepage "https://packages.debian.org/sid/bsdmainutils"
-  url "http://ftp.debian.org/debian/pool/main/b/bsdmainutils/bsdmainutils_11.1.2.tar.gz"
-  sha256 "101c0dede5f599921533da08a46b53a60936445e54aa5df1b31608f1407fee60"
+  url "http://ftp.debian.org/debian/pool/main/b/bsdmainutils/bsdmainutils_12.1.7.tar.xz"
+  sha256 "4d3d01e1793a35b1122d0dd00933002c383532d5708916d55594a3a1e9ea0723"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1d97c44ab6e2decc460d0950e77a3ff5ea6edff38a55aa4b8a5d8182d997461f" => :x86_64_linux
   end
 
+  depends_on "libbsd"
   depends_on :linux
-  unless OS.mac?
-    depends_on "ncurses"
-    depends_on "libbsd"
-  end
+  depends_on "ncurses"
 
   def install
     File.open("debian/patches/series") do |file|
@@ -21,8 +18,6 @@ class Bsdmainutils < Formula
     end
     inreplace "Makefile", "/usr/", "#{prefix}/"
     inreplace "config.mk", "/usr/", "#{prefix}/"
-    inreplace "config.mk", " -o root -g root", ""
-    inreplace "usr.bin/write/Makefile", "chown root:tty $(bindir)/$(PROG)", ""
     system "make", "install"
   end
 
