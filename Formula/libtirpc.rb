3c9cc3acf6858e49afc6fef594f5a2678390a609
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-23 15:56:28 +0200

    libtirpc 1.2.6
    Closes #21004.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -1,12 +1,11 @@
 class Libtirpc < Formula
   desc "Port of Sun's Transport-Independent RPC library to Linux"
   homepage "https://sourceforge.net/projects/libtirpc/"
-  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.1.4/libtirpc-1.1.4.tar.bz2"
-  sha256 "2ca529f02292e10c158562295a1ffd95d2ce8af97820e3534fe1b0e3aec7561d"
+  url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.2.6/libtirpc-1.2.6.tar.bz2"
+  sha256 "4278e9a5181d5af9cd7885322fdecebc444f9a3da87c526e7d47f7a12a37d1cc"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8877610a23ff7fc029060517988cd0d07ad253448840bc6927ab5e6b381c3562" => :x86_64_linux
   end
 
   depends_on "krb5"
