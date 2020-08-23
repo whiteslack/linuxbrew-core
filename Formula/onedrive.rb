a2fa0543ba4a1ca4977c9ab277946d1352d70b65
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-23 15:52:28 +0200

    onedrive 2.4.5
    Closes #21000.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/onedrive.rb
+++ Formula/onedrive.rb
@@ -1,12 +1,11 @@
 class Onedrive < Formula
   desc "Folder synchronization with OneDrive"
   homepage "https://github.com/abraunegg/onedrive"
-  url "https://github.com/abraunegg/onedrive/archive/v2.3.13.tar.gz"
-  sha256 "efa8f132a0dfe5faf1b24c4afaffe95954262bb4e48024198ac984df195f82ed"
+  url "https://github.com/abraunegg/onedrive/archive/v2.4.5.tar.gz"
+  sha256 "1f1f5e1f2f37376b6d96bda2426552a94a8b195f545b4fb7f3668c4fe2e8f6a0"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "23866244bcf417302e2602cfd74df7f161930cff8305a460131a4336412c5cd4" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
