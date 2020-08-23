c193d793a98cd981e3920d06c02cd045598d00c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-23 13:51:37 +0000

    inja: update 3.0.0 bottle.

--- Formula/inja.rb
+++ Formula/inja.rb
@@ -11,6 +11,7 @@ class Inja < Formula
     sha256 "2d2e56f3daecf8306ebb2df948024c6ce7115e26cc160496538ca05900c6d8a5" => :catalina
     sha256 "22ea5918d1033829ab77202e877ce6f79d0bcb965feee60081004654a3ea60b4" => :mojave
     sha256 "9a67ee6245a22e809dd4c7fc29227b42bbc443f58bb24fdc7258929a99c7d0f3" => :high_sierra
+    sha256 "23111bed81e782d541280f32215bffe72ab5bd5f4a868e2c9c411494f996bdc8" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
