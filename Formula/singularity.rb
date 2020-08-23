24e9ee63d7986116dbf4c36d23a269bfa08f34eb
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-23 15:51:45 +0200

    singularity 3.6.1
    Closes #20999.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -1,12 +1,11 @@
 class Singularity < Formula
   desc "Application containers for Linux"
   homepage "https://www.sylabs.io/singularity/"
-  url "https://github.com/sylabs/singularity/releases/download/v3.5.0/singularity-3.5.0.tar.gz"
-  sha256 "849c699eb3569c1b9e4e0824223ea6c0fea8b0805b33ddd3400b7c795d07809e"
+  url "https://github.com/sylabs/singularity/releases/download/v3.6.1/singularity-3.6.1.tar.gz"
+  sha256 "6cac56106ee7f209150aaee9f8788d03b58796af1b767245d343f0b8a691121c"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d736d58bcb753bc0859f43973af2d4952766257a1a23e84bcfb943a8f57027d4" => :x86_64_linux
   end
 
   depends_on "go" => :build
