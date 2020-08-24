eee8f3908c5f439f6da5e3b96c81e4039b87027b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-24 12:19:11 +0000

    topgrade: update 5.6.0 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -10,6 +10,7 @@ class Topgrade < Formula
     sha256 "9b8b9a08993d665ce701cc5c1925cdf0e0b6a7f6f5bc73a9a7b7e480feea4d82" => :catalina
     sha256 "28b73442f0fabcc4ecc2837c6e75fd664d9d961f99904eb06cff1c38cbd3677a" => :mojave
     sha256 "09cf0fd7d79cb026c183d6337a6ec69595e7e467c15875d97b3515585ec4a9b5" => :high_sierra
+    sha256 "502b3f85e7843af2ae9fde35154c46d3e7d0fa2a05f61d027529b66dd29002f6" => :x86_64_linux
   end
 
   depends_on "rust" => :build
