1a9c5158c03acdf74e92f7f4368315d39c5f49e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-28 18:42:31 +0000

    firebase-cli: update 8.9.2 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -13,6 +13,7 @@ class FirebaseCli < Formula
     sha256 "9fc0c1d3051805b2324c019316d4a387da1cc933bdcb91556da6d816b212873b" => :catalina
     sha256 "7a922ecefd3e3789b33fdcab099ae528a80fd11bdc1cdcd378962672ef87a312" => :mojave
     sha256 "f77d305a49cda9762b9df05c941871f9b2e9475fd1c4d7131cecf4d3df1cd24d" => :high_sierra
+    sha256 "9bcf823f10112fb82ac61bbde95d0c75d1295687028de50c9e48175a4c91479c" => :x86_64_linux
   end
 
   depends_on "node"
