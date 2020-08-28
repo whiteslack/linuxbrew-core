ea9247a3a58f3146ec4b1bc9021c466a162ad0a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-08-28 19:30:06 +0000

    qpid-proton: update 0.32.0 bottle.

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -16,6 +16,7 @@ class QpidProton < Formula
     sha256 "04bed9699fa8e399d7361dd79e2819780e43cc5a9d3d1d221f1085a644801e09" => :catalina
     sha256 "a6fb688d4b98625e77f1886284ab795374dcba771504301c4114825e79084ef0" => :mojave
     sha256 "754da3603d6c444bd392dcc94b28a1d8da76489dbf972638644889278b75932f" => :high_sierra
+    sha256 "dd61c76f0a3ebd8fec2ff5856a3843009ce6b7bfb3231bba4c84791251821e89" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
