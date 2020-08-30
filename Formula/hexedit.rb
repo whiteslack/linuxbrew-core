b49f2e7654ea1e4235fd5b2d8e1d2440750252f1
Dawid Dziurla <dawidd0811@gmail.com>
2020-08-30 10:25:58 +0000

    hexedit: update 1.5 bottle.

--- Formula/hexedit.rb
+++ Formula/hexedit.rb
@@ -11,6 +11,7 @@ class Hexedit < Formula
     sha256 "b292c19f7f562a6d062f834395f4add815855ae4c9bfb9192a967af7c135da06" => :catalina
     sha256 "17eb067bc169a802ebdfe9a4af927cb70f2b2230337a78ee4ef00dc5cef1c7ba" => :mojave
     sha256 "cd9b838e99eafc11ab15415f46073e549273b6f461e37adb9e6eaead2e93021d" => :high_sierra
+    sha256 "c81ec2d9c26d0e0c26b97d8560f27a737971c0cfed1e5ea0162d372b08eacf40" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
