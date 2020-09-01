20bb6b7b546818d2832c69a893f1439fdd4764ae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-01 21:02:04 +0000

    redis@4.0: update 4.0.14_1 bottle.

--- Formula/redis@4.0.rb
+++ Formula/redis@4.0.rb
@@ -11,6 +11,7 @@ class RedisAT40 < Formula
     sha256 "a3da3a3af97a3ca5c36c2399defd06e6cbbd0b105980b2d626f224e384adc017" => :catalina
     sha256 "3cbcec571f8c420f2f6daacbef87ed462229f31e4d189be36bee7795a25fc050" => :mojave
     sha256 "e2c6a35a448d8a5bc23182d07a499e7005681d522131253d9087de9c091ba8f5" => :high_sierra
+    sha256 "e303e3369879649bf99c0765097cb60a441ac07e75094bb1997e3f446f310ce2" => :x86_64_linux
   end
 
   keg_only :versioned_formula
