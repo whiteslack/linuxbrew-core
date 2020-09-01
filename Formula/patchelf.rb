ac20e913dc04e888fba6c20cd4d564c1691c3010
Michka Popoff <michkapopoff@gmail.com>
2020-09-01 21:46:42 +0000

    Revert "run bootstrap"
    This reverts commit 709cfefb38d9a3fcb1aac45d561a0cc75970c36a.

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -31,7 +31,6 @@ class Patchelf < Formula
       ENV["HOMEBREW_RPATH_PATHS"] = nil
     end
 
-    system "./bootstrap.sh"
     system "./configure", "--prefix=#{prefix}",
       "CXXFLAGS=-static-libgcc -static-libstdc++",
       "--disable-debug",
