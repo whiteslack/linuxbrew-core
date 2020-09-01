4771ba2546c5902db1c842361e6f10d3e8bcb0d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-01 21:10:01 +0000

    code-server: update 3.5.0 bottle.

--- Formula/code-server.rb
+++ Formula/code-server.rb
@@ -14,6 +14,7 @@ class CodeServer < Formula
     sha256 "79ec8cadf71c1b23a92b552888a971b8cc6055a6bd25b34b74e1ab90509fd08f" => :catalina
     sha256 "437c96b5ab795a11341025a3d0640b289636ae2ef6f382c7d91cb4112d6f168c" => :mojave
     sha256 "02d2d1c07b5650a4a7786d09f75a7c988a51dd20dd3bec49f0bb4a6ba6cfa9f7" => :high_sierra
+    sha256 "da52c88c53178cf4d2eef99dee49eddf99a4451c2e7db23e94981db7466b5c9d" => :x86_64_linux
   end
 
   depends_on "python@3.8" => :build
