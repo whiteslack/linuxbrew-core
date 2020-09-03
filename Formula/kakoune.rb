818618920f4ba96fab348feeaa855d86c4d7e362
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-03 19:43:31 +0000

    kakoune: update 2020.09.01 bottle.

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -16,6 +16,7 @@ class Kakoune < Formula
     sha256 "19ff009f6f44de0e54fc01736f8e145bc6a866307f18adf5a002c8053b7e2bd9" => :catalina
     sha256 "48b2c91f86c65517c8a83a0a0083bc7c0bf54a4e8fc93b22b5744f7c0ce4fc33" => :mojave
     sha256 "dbee14709bcbe746293b0a80852347cc53cb646c9013b6fc119ee37aab4ab859" => :high_sierra
+    sha256 "c2b565e53df96ef5b838475a85f0a904d9c7c2adb1e3f7331729585eed04546d" => :x86_64_linux
   end
 
   depends_on macos: :high_sierra # needs C++17
