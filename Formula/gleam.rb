d2de8334d5398cf0869fbda1590491d04e2e015f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-03 21:07:43 +0000

    gleam: update 0.11.2 bottle.

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -9,6 +9,7 @@ class Gleam < Formula
     sha256 "6341cb125abf33b142c33104b89d6ad680ae7e2fb9cc15e20984a19466d2de6b" => :catalina
     sha256 "ccfb7ad2cc98c358ce6f2ac4797e3d2cb60683c36420419d57a257d63f5fe907" => :mojave
     sha256 "f8a8090c533509c5449fe24ee49da9cb4da2f18cda095cd48a487c9861803e42" => :high_sierra
+    sha256 "611942818b67dc2ece0571a1874ca505f36825d2b586529911d71c277034a354" => :x86_64_linux
   end
 
   depends_on "rust" => :build
