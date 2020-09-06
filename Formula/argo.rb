8fea765b4cf44d7909ba125351edf6ac487dbcc6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-06 20:37:27 +0000

    argo: update 2.10.1_1 bottle.

--- Formula/argo.rb
+++ Formula/argo.rb
@@ -12,6 +12,7 @@ class Argo < Formula
     sha256 "a2992d27e2888f34d7c035112508106f437e04bbbe7178d34f8a9908adbd4106" => :catalina
     sha256 "1ff5a9cc742fe9e657e8e2e67337ffcf3d8f083af8a2d45b5fde459859cea281" => :mojave
     sha256 "d7febd066562584d6ccb69971a5071adce894c5df10f331212ee84ae682b5fda" => :high_sierra
+    sha256 "1b27154f274d151a54e999ff0ad1a0110279165fea6bb07758b236c4d50ce63d" => :x86_64_linux
   end
 
   depends_on "go" => :build
