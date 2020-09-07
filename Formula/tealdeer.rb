3360305ddf0823a7653e9bfd70fabac4b7c7c232
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-07 21:52:33 +0000

    tealdeer: update 1.4.1 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -10,6 +10,7 @@ class Tealdeer < Formula
     sha256 "db4a2fc7dceef4f4f914aaf7b655741e960664b46c61ab2c163f015a74949533" => :catalina
     sha256 "9ad3e3ea878b05fd2764cc8e534888b8bf810f88cc4d986de158ed6b33633b42" => :mojave
     sha256 "80a29641c9b29a3cda69adf5afd2c36b27f84fb8f89a555dbf2a676dddf03b70" => :high_sierra
+    sha256 "c655031e2d3305ad18448f62790f90afa5e770b7d1aaa7fcc7ba1bb9c5471a35" => :x86_64_linux
   end
 
   depends_on "rust" => :build
