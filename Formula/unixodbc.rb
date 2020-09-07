bd665ccda9da1ba2e0d6de1e2bf56d95fbbe21d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-07 21:49:38 +0000

    unixodbc: update 2.3.8 bottle.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -14,6 +14,7 @@ class Unixodbc < Formula
     sha256 "b0df0122140e5b40cf874b0334c6a7bfc1cd5acc79ceffde21f9b95c56c27cc6" => :catalina
     sha256 "7cd0f37108f83ec9079cd22592e4afc528d726bba5dafb7a93cc8a62ffcd7961" => :mojave
     sha256 "6d0f38787107264f9e55563625b18d8c5a744da061925c37ee013c538001f875" => :high_sierra
+    sha256 "36874e5ff573c8030dedecfdbbb02a74ab9689ffc037b8f0acafe397857e9535" => :x86_64_linux
   end
 
   depends_on "libtool"
