ed827f5dd9052e8e3dbaf81d98c4637082719c74
Seeker <meaningseeking@protonmail.com>
2020-09-09 11:49:35 -0700

    device-mapper: add livecheck

--- Formula/device-mapper.rb
+++ Formula/device-mapper.rb
@@ -5,6 +5,12 @@ class DeviceMapper < Formula
     tag:      "v2_03_10",
     revision: "4d9f0606beb0acb329794909560433c08b50875d"
 
+  livecheck do
+    url :stable
+    strategy :page_match
+    regex(/href=.*?;a=tag;.*?>Release (\d+(?:\.\d+)+)</i)
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "152056e9f33c4d22f82a84c85d21a920d9f9a645be10f67644b8830a05fcff7e" => :x86_64_linux
