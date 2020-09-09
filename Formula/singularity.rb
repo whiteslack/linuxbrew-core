a0845d68f73c1ce60990f3a0dd7071c0029755d9
Seeker <meaningseeking@protonmail.com>
2020-09-09 18:20:37 +0000

    singularity 3.6.2
    Closes #21084.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -1,12 +1,11 @@
 class Singularity < Formula
   desc "Application containers for Linux"
   homepage "https://www.sylabs.io/singularity/"
-  url "https://github.com/sylabs/singularity/releases/download/v3.6.1/singularity-3.6.1.tar.gz"
-  sha256 "6cac56106ee7f209150aaee9f8788d03b58796af1b767245d343f0b8a691121c"
+  url "https://github.com/sylabs/singularity/releases/download/v3.6.2/singularity-3.6.2.tar.gz"
+  sha256 "dfd7ec7376ca0321c47787388fb3e781034edf99068f66efc36109e516024d9b"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9f4f80a6becf3037b3ec46b41d2af1b4760dc522273a87a61f851968abacdb3e" => :x86_64_linux
   end
 
   depends_on "go" => :build
