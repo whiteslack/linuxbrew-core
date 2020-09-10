eb4d6f8707490090f153232e4b36910bb644dad6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-10 06:02:29 +0000

    memcached: update 1.6.7 bottle.

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -16,6 +16,7 @@ class Memcached < Formula
     sha256 "7a6ad9f44ec7a42aab0a0b36294f60f66212b70ac8e8fde27f6a697f777f0529" => :catalina
     sha256 "ac32c89c5efb51c33f7918a1e148bfde8cb007d74df4b4d1460941bc2c96e7f9" => :mojave
     sha256 "b943eb42d8f00ae09e8eae09aecff9df1280a81647c909bb81554ad2188070ad" => :high_sierra
+    sha256 "116389be6ac345bc653188998d1325e8ac3b026a0bd8a0db46129d73293bc859" => :x86_64_linux
   end
 
   depends_on "libevent"
