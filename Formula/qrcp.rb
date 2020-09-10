a96525c95e5990fc4752fa10b72dd3ef089332ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-10 05:52:41 +0000

    qrcp: update 0.6.4 bottle.

--- Formula/qrcp.rb
+++ Formula/qrcp.rb
@@ -10,6 +10,7 @@ class Qrcp < Formula
     sha256 "35c4fc732d2e19d78aff13129954130331498d2450f3320fe0a0046623c9adf2" => :catalina
     sha256 "c68d4a7fd6225ddaada0355ed374902f64875d6dda923ad460d2ada1a268bde5" => :mojave
     sha256 "0a05c9f53a4ef108b2716699f3c6f7586221db2b9385f7755c4cf79844c0869c" => :high_sierra
+    sha256 "c574f6157e99a5e1cc13ef037f7ac61c20b979cffd5335eda1a6256f2251fff9" => :x86_64_linux
   end
 
   depends_on "go" => :build
