acaf5df247a569d48c9d4697caa8a1a7f77881e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-10 17:11:45 +0000

    rav1e: update 0.3.4 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -15,6 +15,7 @@ class Rav1e < Formula
     sha256 "ee813b17cb949b695b9a199598b87370decbd7b9b2bfa1400aacd5542c2f73de" => :catalina
     sha256 "047aa3d9eb50622a2f8ebf34559393d8d7b276989ef0329ffad94933eaca2060" => :mojave
     sha256 "042c41714f694ce482a1405b0346f14e69607c0cea8285a6563dfbf2f899aba0" => :high_sierra
+    sha256 "b8f887180d71ec8a81d87d3e45ad082075a700110f79a1c7f20fe72d16f30bd9" => :x86_64_linux
   end
 
   depends_on "cargo-c" => :build
