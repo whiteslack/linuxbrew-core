fd504c3604a9edeafae303a5a42edafa6a7b9637
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-12 11:36:46 +0000

    zint: update 2.9.1 bottle.

--- Formula/zint.rb
+++ Formula/zint.rb
@@ -16,6 +16,7 @@ class Zint < Formula
     sha256 "27a13b9022616484c612860ec1ac80146f765de23c32a52cf7f6f7a516727672" => :catalina
     sha256 "398f6493010f6b4778fe5ce80b559b745f53de2dcbd0c331f844431274a1d1ac" => :mojave
     sha256 "7142283083b90b3d185672f98fc987292337b8cb50cfb4e76cb61394df05781a" => :high_sierra
+    sha256 "521eb64c1a8847575ce8bd1b00c081e9baafff939e8459559ab7339c6d12fb36" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
