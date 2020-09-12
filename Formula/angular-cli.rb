f1e58e3f6e5b1290d63674d9674f751ca9675dc5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-12 07:17:04 +0000

    angular-cli: update 10.1.1 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -16,6 +16,7 @@ class AngularCli < Formula
     sha256 "54910ad81accbcba059e3b8a567690dffaaee7322e765a7e77a57cb35475a8bb" => :catalina
     sha256 "f259877647d8e838b155ca659a65682a3347208986c1313c958832c8bbcd187a" => :mojave
     sha256 "34c2767e2d9bb2da0c9ae403bb86e29ee718f193bb73e80e4ee767d5b0d562f2" => :high_sierra
+    sha256 "d7ead716b4e590e9022b8a2a7f5b78b3b407b2d157f04c3df533901f43f45fea" => :x86_64_linux
   end
 
   depends_on "node"
