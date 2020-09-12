a2c289c9a489e0410cd4e05216be81ee94775cfa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-12 19:37:11 +0000

    pumba: update 0.7.5 bottle.

--- Formula/pumba.rb
+++ Formula/pumba.rb
@@ -11,6 +11,7 @@ class Pumba < Formula
     sha256 "9466bf26ee389122ea63bc3ddd87619052436b81960f321b2ed7eb12ae30dd97" => :catalina
     sha256 "4d6b6d75185c7c1caa95a6e5cd79513ee579f54c5a2d0bb220f2e9f279ebab2d" => :mojave
     sha256 "567508d3166f37c6ec5875a9079d8ac7168ce6c17e1274de9a31f3ee58eede94" => :high_sierra
+    sha256 "b0a027b3d04c4dd21cae671ab41e658206b903f8a247236ebe486c1d65cae4b8" => :x86_64_linux
   end
 
   depends_on "go" => :build
