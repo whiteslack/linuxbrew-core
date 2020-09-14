4098bcc2cd0c74d0e513bf88cb5a40236dc4cc1c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-14 20:38:25 +0000

    usb.ids: update 2020.08.26 bottle.

--- Formula/usb.ids.rb
+++ Formula/usb.ids.rb
@@ -10,6 +10,7 @@ class UsbIds < Formula
     sha256 "2994769226c7815ef5eee9ba27f729005fd993341dfbca50f413139ef411ac5c" => :catalina
     sha256 "8b29c5873a395b8bdff9219dcfafb13d05d7428c8f4d050cb776d332dd7aef1f" => :mojave
     sha256 "18a048550eae20c48c7af4cc0b93f1da748cae52417e364b1aedc154c27613d5" => :high_sierra
+    sha256 "e6d5f6c9f12df8f008e42a4fd05ce9b78e130c17463fdb062b77a56f4da58d28" => :x86_64_linux
   end
 
   def install
