5d273b05ca63fa070d6c7402590a5b6b67090264
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-15 11:16:37 +0000

    hugo: update 0.75.0 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -11,6 +11,7 @@ class Hugo < Formula
     sha256 "fcff812c6b91c4c879c00bea6849bc0eaf1da408cfe3476ae8606e219d49ba4a" => :catalina
     sha256 "ab6e90f361d53e01516ff0282b199447acbe7291921ce6acee9275b00136a73d" => :mojave
     sha256 "0ef2cb85d4c6195dae8b3c9859c2853ea08a14e0c5061b4b5f0b69bcfaee0766" => :high_sierra
+    sha256 "95abd7d820a9aa439e1818ee3297d806cdbab61d06896f9fda37483ddf8b23f1" => :x86_64_linux
   end
 
   depends_on "go" => :build
