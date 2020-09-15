c95eaa6e729978a1cea219de1f52899b5eb5ed39
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-15 11:13:36 +0000

    xtensor: update 0.21.6 bottle.

--- Formula/xtensor.rb
+++ Formula/xtensor.rb
@@ -10,6 +10,7 @@ class Xtensor < Formula
     sha256 "8568b35a4b75c51e28887a0f68e658e2a3257fda91070de601f2c2b77b576778" => :catalina
     sha256 "2c7baddc17bdd3e0521a0c2244498e7c8f2860bf5b00426615095d46cfa88278" => :mojave
     sha256 "869e75b04b0e9246b6e1a862af79b5c1ae2e2a1b1ad60c27150e292cf4754135" => :high_sierra
+    sha256 "790870544568dda35338b00a9c552695eef2ad4191651b45ac62d06da43bf6fe" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
