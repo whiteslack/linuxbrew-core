8cc30319258f5b45793f1964329ed104a138e3ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-15 11:17:25 +0000

    libdnet: update 1.14 bottle.

--- Formula/libdnet.rb
+++ Formula/libdnet.rb
@@ -15,6 +15,7 @@ class Libdnet < Formula
     sha256 "f8c9ace5eb112c484f50da7624df13c551b14114ece91a155ce2394b30e264b7" => :catalina
     sha256 "1e967ac6c5b9c70f72efba9082844c755dba2d62054a4c4dfbd5629da3cb0b76" => :mojave
     sha256 "fd53de5c1830dcdb52ecba97cf0c9c6afccf44037e6df6f64ef1d163d6c6adff" => :high_sierra
+    sha256 "5c5af547c3581d357f028605acd9f3d09e999a34e0d07a38844d073f235f1ef7" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
