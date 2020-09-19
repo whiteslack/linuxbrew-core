71433c98ee5011fa861f40a8f40d07a9196dadcc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-19 12:55:59 +0000

    seal: update 3.5.9 bottle.

--- Formula/seal.rb
+++ Formula/seal.rb
@@ -9,6 +9,7 @@ class Seal < Formula
     sha256 "e13c03ab99faa6ad88a86622e3646eac5c3db1c6b57542bad702612f714eedfb" => :catalina
     sha256 "fd36a3416c5bc506c8a56c1e5c3267aec2f28e4720f8cf31db31b7ac2f9cab63" => :mojave
     sha256 "576df58ef112717404d4c9ced08289fd3f58d7eb275e01175f16552673a26ac8" => :high_sierra
+    sha256 "1ba91bab71f7b98bc70ab7b45503d3e996c0cfab13bf96716d4d733be6dd7969" => :x86_64_linux
   end
 
   depends_on "cmake" => [:build, :test]
