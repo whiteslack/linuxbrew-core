f1d82c5253a0ec01784f545389d69c2701a9cfd8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-20 16:34:30 +0000

    tth: update 4.15 bottle.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -14,6 +14,7 @@ class Tth < Formula
     sha256 "eba438c9ec2902578cd3a043fdfbc1fa6bf22bedbde36e868c4fe02740448b06" => :catalina
     sha256 "a0fcd3029a8a43a59a530537c5fef4df45c0155924f41b07ac14ec2362d73ba7" => :mojave
     sha256 "dc8224b651a9bbb3ab8a5f5dd2e213bfd42518962bd7132cb3d8a2db12ddd5bb" => :high_sierra
+    sha256 "9ea9f32bcde75812b033b2a8e65eb94fd1ac1e044e9028f4eb9d37078652ebf7" => :x86_64_linux
   end
 
   def install
