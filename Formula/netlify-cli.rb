5112242cc4cccb819c188177a3255a696ec1c905
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-21 18:27:30 +0000

    netlify-cli: update 2.63.3 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -17,6 +17,7 @@ class NetlifyCli < Formula
     sha256 "5421effde400c98e4d76a00386b03c7db617ce0c4e30de93709452e92e428f27" => :catalina
     sha256 "17e542f6b6ef91a55eab64e48eca2c0b7d12b18b5f2b7cc41f36ef5d35d8156a" => :mojave
     sha256 "111228131dbdc2f824123404a56b26e5df92cf5d24fd38b442f175c72feaadc3" => :high_sierra
+    sha256 "ba5e773e52854060c36cae46a5a3581688b8e8010792e85b6ecce021212d7311" => :x86_64_linux
   end
 
   depends_on "node"
