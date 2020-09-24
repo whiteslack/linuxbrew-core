639ca7ea4679ec3352d6a76ce8c5624effaaf2a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-24 06:14:08 +0000

    helm: update 3.3.4 bottle.

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -12,6 +12,7 @@ class Helm < Formula
     sha256 "8ea831439125a0d31e243876dde7e30b6273e97adc6e45fd3f14cf8d097acb63" => :catalina
     sha256 "e5af6006990de1e44bdb0fa789cd92a4ad78a5f6726545a7ddae3a6f5da401b8" => :mojave
     sha256 "bb3bc2cdc2fa93491565d549579780248e84448a7b2a25bd13ac8d8f491bdde6" => :high_sierra
+    sha256 "7b727d6ef2e4f3109e29b0d7bc4a95e1115f279da5a153d3549f5c9dbbcc5717" => :x86_64_linux
   end
 
   depends_on "go" => :build
