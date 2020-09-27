d20c9c9ef8e771fdf09c4981b5936a8f5e26d2fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-27 15:23:20 +0000

    balena-cli: update 12.23.0 bottle.

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -16,6 +16,7 @@ class BalenaCli < Formula
     sha256 "64c18bc02693145fdf339a86091b2c8081e489ad695444b13b4ded459abcfc28" => :catalina
     sha256 "c9132974c680709d6abc72748daff8e8a43c6975d8d926f7fc8a605a5b5ae0c6" => :mojave
     sha256 "ccf55cbdb92b95e72b7e4d4d843736d71a59be0c08e48660d704c44e0c26f504" => :high_sierra
+    sha256 "17824375614a4171cbca82e443e5d7bc5876507355dffd9f32bcc7f1839c24da" => :x86_64_linux
   end
 
   depends_on "node"
