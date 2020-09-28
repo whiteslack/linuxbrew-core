07ed754b809598bdbe7bfe5eaa78f2c3b3235381
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-09-28 16:47:22 +0000

    insect: update 5.5.0 bottle.

--- Formula/insect.rb
+++ Formula/insect.rb
@@ -16,6 +16,7 @@ class Insect < Formula
     sha256 "003e2a5e62cee61b92252eaa82c086cdf70137f4bbaf081f8112f1d9ed7b780b" => :catalina
     sha256 "8beb06f0b8e5a2f576dd4b13cf70d71c6009ffd8530ea7e9a0690872c1197189" => :mojave
     sha256 "56137febe41808c75c8a8827f50c67c03ee472e3e8ab7c53554e262c8750fec2" => :high_sierra
+    sha256 "5734ae109e9326ee4bcdf735d6ff4cb746546ff144a94ab63c5a1feacd486f3e" => :x86_64_linux
   end
 
   depends_on "psc-package" => :build
