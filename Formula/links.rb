2dc76c9de8dccc5c3ae805e90c31b50c895f8946
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-01 18:43:55 +0000

    links: update 2.21_1 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -16,6 +16,7 @@ class Links < Formula
     sha256 "c43cf180508872a2e5461af4c3c80a58d2d8824d1bb8b0c1a7d0f890aa12ef49" => :catalina
     sha256 "aef8fa60ec4bdc8adca1cab129a2a8fd0a44888d867e7b468a1ed7672f7c7550" => :mojave
     sha256 "6660756b8d9ac4a433212c44067fd1627e96c3bcf7996f4a51da1affe39e29e5" => :high_sierra
+    sha256 "2d4792eecf3c48126c9673330977dc997583e4364acba811c95a373050b386a8" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
