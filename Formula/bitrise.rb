de857dfe979b559e73fa82a8af484a92e71a288e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-01 17:43:17 +0000

    bitrise: update 1.43.0 bottle.

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -10,6 +10,7 @@ class Bitrise < Formula
     sha256 "a1140b9568455794ceb38150b65154ba25b9a769c66f639f2b9055b74bf92b47" => :catalina
     sha256 "5b4dbb41ad4ed225807356dd794ef854752f177ba20866f73985f36c037806f6" => :mojave
     sha256 "b814ca00dbbd74aa5ac726bd4914d55faa0d0bc929744b677d7884fec6783455" => :high_sierra
+    sha256 "c1eee16ea056ad5e12d85adcd41166ded274fe255cd650d320448009d8aa1d34" => :x86_64_linux
   end
 
   depends_on "go" => :build
