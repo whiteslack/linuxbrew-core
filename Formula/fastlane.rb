a1ad14baa44126fb30b57f574c84dde6876a824c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-01 17:45:16 +0000

    fastlane: update 2.161.0 bottle.

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -16,6 +16,7 @@ class Fastlane < Formula
     sha256 "fa9092e388bca3b32807eddcc1c6514e92f3e5e20da966f82bd7670c3e0d42d3" => :catalina
     sha256 "431e169597585884f93781f00c17c64fd779d6ea311602891567236146473c5f" => :mojave
     sha256 "39bdfc80e896775b45617f1b7bb9f209cad91b91a5bb0a3cbeae0ed0080f45cc" => :high_sierra
+    sha256 "c6b377bf457a28d357ab15479da5a844c4595581b0950a38a80917c462635218" => :x86_64_linux
   end
 
   depends_on "ruby"
