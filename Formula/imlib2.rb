7f4a1a5dba1f4fcd698e034f558449b7bf7bf31e
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-01 19:22:11 +0000

    imlib2: update 1.7.0_3 bottle.

--- Formula/imlib2.rb
+++ Formula/imlib2.rb
@@ -14,6 +14,7 @@ class Imlib2 < Formula
     sha256 "3b036ab0c372688bdd116674d463f85b7a1fc900dcb71deddbfa5aa28fca0be1" => :catalina
     sha256 "ab1f7ef9cd4c1b7eced2f47f20bed71d724d09c48907675e4144ae039a10d297" => :mojave
     sha256 "153ebc859ca8a20b20b9d24db87667b8d338fd0e571f01f8400262e3d5700f86" => :high_sierra
+    sha256 "71ef1999cdd01182f407f87becc4a14164bc9fd3022ce05a0562ff61aa41d7aa" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
