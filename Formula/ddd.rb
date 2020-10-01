ebc6339d1832683cb3d9e14ae0f7e2b049102299
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-01 11:27:55 +0000

    ddd: update 3.3.12_2 bottle.

--- Formula/ddd.rb
+++ Formula/ddd.rb
@@ -17,6 +17,7 @@ class Ddd < Formula
     sha256 "381ae07c96a67534b05a03ca72741d99aa3437a01c0fef603336ea218c470df9" => :high_sierra
     sha256 "af12e95b5b4326906236559a40f6715e896d164d5c18d9448384e0e22d089abf" => :sierra
     sha256 "68864faf1967b400bc5df5809ab9ee03a0d632f3736071131dd5469be715c58f" => :el_capitan
+    sha256 "7a36c28b4c2ac721bd6785e43953ae81b871f6695232f6e0ebf86fb2666aac32" => :x86_64_linux
   end
 
   depends_on "openmotif"
