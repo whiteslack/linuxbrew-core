46486efa69384e78cf7e8d4f1eec0f0d57cd76fa
Michka Popoff <michkapopoff@gmail.com>
2020-10-01 19:39:20 +0200

    xdotool: replace xorg by specific xorg packages
    Closes #21164.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/xdotool.rb
+++ Formula/xdotool.rb
@@ -3,20 +3,24 @@ class Xdotool < Formula
   homepage "https://www.semicomplete.com/projects/xdotool/"
   url "https://github.com/jordansissel/xdotool/archive/v3.20160805.1.tar.gz"
   sha256 "ddafca1239075c203769c17a5a184587731e56fbe0438c09d08f8af1704e117a"
-  revision OS.mac? ? 1 : 2
+  revision OS.mac? ? 1 : 3
 
   bottle do
     sha256 "8dbfb2b1c32176c7cba00aaa2365f3cd438619dc0286e668e5d87412c3717d53" => :catalina
     sha256 "860e5e7f2ca2ae88c86e8a979eba543f544960894bb4d8ec59d98cbba9805614" => :mojave
     sha256 "9de15325d8ed42b629a94e34ff710672e96c1570dc51a6544aff0d0445de5e9c" => :high_sierra
-    sha256 "844136b01ebd2c12060c9c5c7937c4dc72cea7630998e38fc8eb73bca603fbc6" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "libxkbcommon"
-
   depends_on :x11 if OS.mac?
-  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
+
+  unless OS.mac?
+    depends_on "linuxbrew/xorg/libxtst"
+    depends_on "linuxbrew/xorg/libx11"
+    depends_on "linuxbrew/xorg/libxi"
+    depends_on "linuxbrew/xorg/libxinerama"
+  end
 
   def install
     # Work around an issue with Xcode 8 on El Capitan, which
