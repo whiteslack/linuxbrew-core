254fe2fedb2e0f4b875e4755abea9f46cfd968d3
Michka Popoff <michkapopoff@gmail.com>
2020-10-01 23:24:07 +0200

    gatsby-cli: remove xorg dependency (#21160)
    The linkage check for libx11 is broken anyway for this formula and
    we have no bottle.

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -21,10 +21,6 @@ class GatsbyCli < Formula
 
   depends_on "node"
 
-  on_linux do
-    depends_on "linuxbrew/xorg/xorg"
-  end
-
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
