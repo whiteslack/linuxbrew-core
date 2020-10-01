730db01a03092700fe9af0b126262d07c82b5ad3
Michka Popoff <michkapopoff@gmail.com>
2020-10-01 21:03:18 +0200

    xclip: replace xorg by specific xorg dependencies
    Closes #21178.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -4,7 +4,7 @@ class Xclip < Formula
   url "https://github.com/astrand/xclip/archive/0.13.tar.gz"
   sha256 "ca5b8804e3c910a66423a882d79bf3c9450b875ac8528791fb60ec9de667f758"
   license "GPL-2.0"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -13,13 +13,16 @@ class Xclip < Formula
     sha256 "0963015158b7d4ae2981503edc18427737a0586b7155da5cd2ddaa93fb3b92bd" => :high_sierra
     sha256 "bb26c2bb6d7ce8f15ab50144f38d11ddde113bb400326ccea990ca9a5d0a9c69" => :sierra
     sha256 "9e17790e9a94ae1e29317f013a65f2d639ae9063db48ed7fa0aed7449f221abb" => :el_capitan
-    sha256 "9de13067a4d4ab379e82ab78d97c559695551fd51ee99c23879e2503de6c4d7e" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on :x11 if OS.mac?
-  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
+
+  unless OS.mac?
+    depends_on "linuxbrew/xorg/libx11"
+    depends_on "linuxbrew/xorg/libxmu"
+  end
 
   def install
     system "autoreconf", "-fiv"
