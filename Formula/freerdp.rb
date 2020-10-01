1be89b3412df8e6fbdbc2a44a974129334b62036
Michka Popoff <michkapopoff@gmail.com>
2020-10-01 07:44:29 +0200

    freerdp: replace for specific xorg packages
    Closes #21159.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/freerdp.rb
+++ Formula/freerdp.rb
@@ -4,12 +4,12 @@ class Freerdp < Formula
   url "https://github.com/FreeRDP/FreeRDP/archive/2.2.0.tar.gz"
   sha256 "883bc0396c6be9aba6bc07ebc8ff08457125868ada0f06554e62ef072f90cf59"
   license "Apache-2.0"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "a2ca3e1a307c549ab620d98fc5e96870017d63dc3d279da5ca56dda76fc38075" => :catalina
     sha256 "67732bc5f1195cd4c959243b02f3f7a3f81c516f97b6f5eda7376cdf2ee73edd" => :mojave
     sha256 "a08c1b8bb532c6c9b9ada2b5ff9f6c2dbfef696abd4ac6e3de4d38e58bd9592a" => :high_sierra
-    sha256 "8ccae8b3e5bace66baa9312c89bab9856489b504a031c07efe7ce734db6510e6" => :x86_64_linux
   end
 
   head do
@@ -22,19 +22,21 @@ class Freerdp < Formula
   depends_on "libusb"
   depends_on "openssl@1.1"
   depends_on :x11 if OS.mac?
-  unless OS.mac?
-    depends_on "alsa-lib"
-    depends_on "ffmpeg"
-    depends_on "glib"
-    depends_on "systemd"
-    depends_on "linuxbrew/xorg/xorg"
-    depends_on "linuxbrew/xorg/wayland"
-  end
 
   on_linux do
     depends_on "alsa-lib"
     depends_on "ffmpeg"
     depends_on "glib"
+  end
+
+  unless OS.mac?
+    depends_on "systemd"
+    depends_on "linuxbrew/xorg/libx11"
+    depends_on "linuxbrew/xorg/libxcursor"
+    depends_on "linuxbrew/xorg/libxext"
+    depends_on "linuxbrew/xorg/libxinerama"
+    depends_on "linuxbrew/xorg/libxv"
+    depends_on "linuxbrew/xorg/wayland"
   end
 
   def install
