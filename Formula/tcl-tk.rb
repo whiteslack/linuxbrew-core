0415f54e5eb60775025c48d6ece8dc8b0d3b9ddc
Michka Popoff <michkapopoff@gmail.com>
2020-10-01 21:06:37 +0200

    tcl-tk: replace xorg by specific xorg dependencies
    Closes #21179.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/tcl-tk.rb
+++ Formula/tcl-tk.rb
@@ -5,7 +5,7 @@ class TclTk < Formula
   mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcl/tcl8.6.10-src.tar.gz"
   sha256 "5196dbf6638e3df8d5c87b5815c8c2b758496eb6f0e41446596c9a4e638d87ed"
   license "TCL"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   livecheck do
     url :stable
@@ -17,19 +17,20 @@ class TclTk < Formula
     sha256 "4740b30b97f0308ecc59c1308945c38ddca5d3da528d779f38199a2dad905fa1" => :catalina
     sha256 "1851fee12a3ee44648845d8663a192712ce6827ef8fe167301d2638ac9ddb96c" => :mojave
     sha256 "d1d689cc3e9cf08b2a42d487db3c4142e7ee4ff322bef22d6187fc67a5b776b7" => :high_sierra
-    sha256 "0de2a7527b765b2cc4de33d8c9ee8676b33116bbda67eeb40bbcfce9d96e35c1" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
 
   depends_on "openssl@1.1"
-  unless OS.mac?
-    depends_on "linuxbrew/xorg/xorg"
-    depends_on "pkg-config" => :build
-  end
 
   on_linux do
     depends_on "pkg-config" => :build
+  end
+
+  unless OS.mac?
+    depends_on "zlib"
+    depends_on "linuxbrew/xorg/libx11"
+    depends_on "linuxbrew/xorg/libxext"
   end
 
   resource "critcl" do
