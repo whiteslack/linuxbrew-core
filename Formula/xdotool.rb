c7398eea0433ca9e0a28778adb94124479313068
Michka Popoff <michkapopoff@gmail.com>
2020-10-03 19:08:07 +0200

    xdottool: use core xorg dependencies

--- Formula/xdotool.rb
+++ Formula/xdotool.rb
@@ -3,13 +3,12 @@ class Xdotool < Formula
   homepage "https://www.semicomplete.com/projects/xdotool/"
   url "https://github.com/jordansissel/xdotool/archive/v3.20160805.1.tar.gz"
   sha256 "ddafca1239075c203769c17a5a184587731e56fbe0438c09d08f8af1704e117a"
-  revision OS.mac? ? 1 : 3
+  revision OS.mac? ? 1 : 4
 
   bottle do
     sha256 "8dbfb2b1c32176c7cba00aaa2365f3cd438619dc0286e668e5d87412c3717d53" => :catalina
     sha256 "860e5e7f2ca2ae88c86e8a979eba543f544960894bb4d8ec59d98cbba9805614" => :mojave
     sha256 "9de15325d8ed42b629a94e34ff710672e96c1570dc51a6544aff0d0445de5e9c" => :high_sierra
-    sha256 "eb2c19573448569504c6dbb38eaaee168d5ef115270d7d696118f809f789d229" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
@@ -17,10 +16,10 @@ class Xdotool < Formula
   depends_on :x11 if OS.mac?
 
   unless OS.mac?
-    depends_on "linuxbrew/xorg/libxtst"
-    depends_on "linuxbrew/xorg/libx11"
-    depends_on "linuxbrew/xorg/libxi"
-    depends_on "linuxbrew/xorg/libxinerama"
+    depends_on "libxtst"
+    depends_on "libx11"
+    depends_on "libxi"
+    depends_on "libxinerama"
   end
 
   def install
