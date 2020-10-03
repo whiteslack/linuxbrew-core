d445c4049903ac1df8c649e9ea4bdb4cb86a2a2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-03 07:48:42 +0000

    terrahub: update 0.4.32 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -16,6 +16,7 @@ class Terrahub < Formula
     sha256 "0e6a87ad033e2c2664d8a358dbfcb9a5625385a575a5407011edf45497948c74" => :catalina
     sha256 "cd330be5ec1274e46eb696fbd080e4e1709e275bd772c4161608e9e039f9b57e" => :mojave
     sha256 "d94c6a19692e49720bde3dae78dea34e3c27a1e8245aadd1da1849e19944142e" => :high_sierra
+    sha256 "84e159030acdde6e5fed02dca9fc1886d3a9943b5c02aece927bbcecf348f2c7" => :x86_64_linux
   end
 
   depends_on "node"
