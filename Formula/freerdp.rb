774a57aab1f7919d7ce2eb6030dbb4c064950148
Michka Popoff <michkapopoff@gmail.com>
2020-10-03 18:43:19 +0200

    freerdp: use core xorg dependencies

--- Formula/freerdp.rb
+++ Formula/freerdp.rb
@@ -4,13 +4,12 @@ class Freerdp < Formula
   url "https://github.com/FreeRDP/FreeRDP/archive/2.2.0.tar.gz"
   sha256 "883bc0396c6be9aba6bc07ebc8ff08457125868ada0f06554e62ef072f90cf59"
   license "Apache-2.0"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
     sha256 "a2ca3e1a307c549ab620d98fc5e96870017d63dc3d279da5ca56dda76fc38075" => :catalina
     sha256 "67732bc5f1195cd4c959243b02f3f7a3f81c516f97b6f5eda7376cdf2ee73edd" => :mojave
     sha256 "a08c1b8bb532c6c9b9ada2b5ff9f6c2dbfef696abd4ac6e3de4d38e58bd9592a" => :high_sierra
-    sha256 "efe3de752e7cde4b862a8e2ab362e5b2835701ef034d74717c872f56320d9169" => :x86_64_linux
   end
 
   head do
@@ -32,11 +31,11 @@ class Freerdp < Formula
 
   unless OS.mac?
     depends_on "systemd"
-    depends_on "linuxbrew/xorg/libx11"
-    depends_on "linuxbrew/xorg/libxcursor"
-    depends_on "linuxbrew/xorg/libxext"
-    depends_on "linuxbrew/xorg/libxinerama"
-    depends_on "linuxbrew/xorg/libxv"
+    depends_on "libx11"
+    depends_on "libxcursor"
+    depends_on "libxext"
+    depends_on "libxinerama"
+    depends_on "libxv"
     depends_on "linuxbrew/xorg/wayland"
   end
 
