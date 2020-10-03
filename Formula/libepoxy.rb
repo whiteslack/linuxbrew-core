4f84b46c378d753a147342ef4e73ba1448207312
Michka Popoff <michkapopoff@gmail.com>
2020-10-03 18:52:24 +0200

    libepoxy: use core xorg dependencies

--- Formula/libepoxy.rb
+++ Formula/libepoxy.rb
@@ -4,7 +4,7 @@ class Libepoxy < Formula
   url "https://download.gnome.org/sources/libepoxy/1.5/libepoxy-1.5.4.tar.xz"
   sha256 "0bd2cc681dfeffdef739cb29913f8c3caa47a88a451fd2bc6e606c02997289d2"
   license "MIT"
-  revision OS.mac? ? 1 : 3
+  revision OS.mac? ? 1 : 4
 
   # We use a common regex because libepoxy doesn't use GNOME's "even-numbered
   # minor is stable" version scheme.
@@ -17,7 +17,6 @@ class Libepoxy < Formula
     sha256 "9f58a2eab6aafcc95ade6893bde8d878ab422284353e22c11d04c3a6f3a1e7cb" => :catalina
     sha256 "e42a0410e6f94fa419f785c5b0901eea1506242b1729f97b672f25b463ce3d4e" => :mojave
     sha256 "95cbc3ce1fc94931e0259f9e55a25d9dcacacd70713ae3e59cba28f3d7ff2a3a" => :high_sierra
-    sha256 "234d5a715ed2154a92c0917b52b78ca53c85addff9ef9bddd186ad72e0ccb2b9" => :x86_64_linux
   end
 
   depends_on "meson" => :build
@@ -26,8 +25,8 @@ class Libepoxy < Formula
 
   unless OS.mac?
     depends_on "freeglut"
-    depends_on "linuxbrew/xorg/xorgproto"
     depends_on "mesa"
+    depends_on "xorgproto"
   end
 
   def install
