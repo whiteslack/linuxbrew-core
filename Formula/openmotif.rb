c2731f8fb30439d4e0444158177751bc93bad9f9
Michka Popoff <michkapopoff@gmail.com>
2020-10-03 19:00:36 +0200

    openmotif: use core xorg dependencies

--- Formula/openmotif.rb
+++ Formula/openmotif.rb
@@ -4,7 +4,7 @@ class Openmotif < Formula
   url "https://downloads.sourceforge.net/project/motif/Motif%202.3.8%20Source%20Code/motif-2.3.8.tar.gz"
   sha256 "859b723666eeac7df018209d66045c9853b50b4218cecadb794e2359619ebce7"
   license "LGPL-2.1"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   livecheck do
     url :stable
@@ -16,7 +16,6 @@ class Openmotif < Formula
     sha256 "ca0c7a96b098ed5efc2dace2cb1b9bc2447c8f1cf0780e882bfee691160466e0" => :high_sierra
     sha256 "21120a7b3aab57d5660c480ab5f1924cbfb31e8625674bf02704971f103616f9" => :sierra
     sha256 "bef02966fb2d72ac23235c8038cdf864cefe47d1cb905fac08a9194d7c9ed554" => :el_capitan
-    sha256 "dcdc89a25236ac65de5dc6f40572fff24f5feccf65326be73203eba95b6e58da" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
@@ -28,10 +27,10 @@ class Openmotif < Formula
 
   unless OS.mac?
     depends_on "flex" => :build
-    depends_on "linuxbrew/xorg/libsm"
-    depends_on "linuxbrew/xorg/libxext"
-    depends_on "linuxbrew/xorg/libxft"
-    depends_on "linuxbrew/xorg/libxt"
+    depends_on "libsm"
+    depends_on "libxext"
+    depends_on "libxft"
+    depends_on "libxt"
     depends_on "linuxbrew/xorg/xbitmaps"
   end
 
