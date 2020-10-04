34aa0f2b04efafd93ee944a4941cf31f592081d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-04 19:52:02 +0000

    libxxf86vm: update 1.1.4 bottle.

--- Formula/libxxf86vm.rb
+++ Formula/libxxf86vm.rb
@@ -10,6 +10,7 @@ class Libxxf86vm < Formula
     sha256 "0a4d6a9d0b98bd8b8cd2aa2025b6ee80a19ffca34744fed599ef0e754d9e810b" => :catalina
     sha256 "c89b765023eb0b6820cbf776d6af714363dc6ebd1ea000e1e53a99fa79be4a0d" => :mojave
     sha256 "37cf2b440d8d4e7cda6aa70d070cd45427cebc61781b6c1b3aec153bf82d638d" => :high_sierra
+    sha256 "3f2182663cf20729f2236026e21edbde1f34918b1c267d742ad6ffc818d5aa76" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
