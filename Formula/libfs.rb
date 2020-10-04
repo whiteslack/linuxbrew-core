64c1bcbb45869ff0e5f2ed7ab112cbae39b61c15
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-04 19:47:20 +0000

    libfs: update 1.0.8 bottle.

--- Formula/libfs.rb
+++ Formula/libfs.rb
@@ -10,6 +10,7 @@ class Libfs < Formula
     sha256 "6ccad0f27601c96ffffac5229d5a25c0ece882b0faf626ae115dfd57a1ac09cb" => :catalina
     sha256 "b8edb6c54600c14a791a33e7997927d394254b1212ea646ac786a667db263921" => :mojave
     sha256 "f62ce9b989c58747a5b5764478cab8acdf021b60d2a6d306547605305ad41f04" => :high_sierra
+    sha256 "5ce12b29cb3ed2cff925ab4a77e6cab1176ddeba78e9296887817d0fdeeceac7" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
