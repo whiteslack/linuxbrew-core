7912b97cdef2ac44d5d55c7142acca2c44c2a739
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-04 19:47:04 +0000

    libxau: update 1.0.9 bottle.

--- Formula/libxau.rb
+++ Formula/libxau.rb
@@ -10,6 +10,7 @@ class Libxau < Formula
     sha256 "d10771f476b47134c9c3f18a33fb4d4d86c37e2a4d6dbbc87c13b7ffd06c7248" => :catalina
     sha256 "3a34b529a2092bf1aaffc6603056871c2b0c4b8bd1fe728a14ae6b35e8cf3f77" => :mojave
     sha256 "15522122382cdc3e364167c71835e4885a0241189be938853cc4744f38e82aa0" => :high_sierra
+    sha256 "6be192d4f2b68b34f86d0a1a2e8e5324a2124bca531972464942e56cc4339e8d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
