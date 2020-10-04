14972dbb4f6e076810949dbaf48f0eba0022cce3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-04 19:54:01 +0000

    libxaw3d: update 1.6.3 bottle.

--- Formula/libxaw3d.rb
+++ Formula/libxaw3d.rb
@@ -10,6 +10,7 @@ class Libxaw3d < Formula
     sha256 "e089705cc442b4076b7d3b9bc86a1379eb193b73cb57387d04809411876eb755" => :catalina
     sha256 "b5510c021114d1579116242eb88f4fbd6a9fd3d3a61292a17d6158c02ac71194" => :mojave
     sha256 "ccf2fd19db504f82ab9f280525a879bd0669934a51cdc69003238d0626735b52" => :high_sierra
+    sha256 "96949d8666646ad07cb16ef2b0fa8943141a6475e5ad21d4f2c6948d6eaad9c1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
