ce02c032c97045727367ffcf733008006f4a914a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-04 20:38:55 +0000

    glfw: update 3.3.2_2 bottle.

--- Formula/glfw.rb
+++ Formula/glfw.rb
@@ -12,6 +12,7 @@ class Glfw < Formula
     sha256 "deaf1b20e9fc336d5f0c9a927bc07f2c509fc63538c39e4ab3a024ca7c6170d8" => :catalina
     sha256 "0c0de277c23273346d703004279d92d17a8962f4d62bf01f76021beea3c3f20a" => :mojave
     sha256 "c6a198383ef979823c1e0071e65771ed9059626071390f2dc5b84b218dc565c3" => :high_sierra
+    sha256 "cac7fec8f28963def819c8b5211cef64d57d8198280a3b4e9ff5530305913ea5" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
