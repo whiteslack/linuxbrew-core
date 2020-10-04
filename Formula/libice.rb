14d755371243f54664792cea9a55905237a328f0
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-04 20:56:35 +0000

    libice: update 1.0.10 bottle.

--- Formula/libice.rb
+++ Formula/libice.rb
@@ -10,6 +10,7 @@ class Libice < Formula
     sha256 "4c5c97814304360fdaeec959107e79e9fdb62ba151159ca55342944efec4bd82" => :catalina
     sha256 "d7249247483e6ee2787e66c7f887a7df52aedd5abd2558ae377b5d16e3b6275e" => :mojave
     sha256 "b5f1f14bc4fd8d18fd19b2552ddc898f53f573015de0706289de54c177b16eb4" => :high_sierra
+    sha256 "935f190724128149ac5c9d38202080532a071a4e8ae1248a794824a9dee61f12" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
