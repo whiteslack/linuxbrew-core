22994c99f221631153da39e77dd5c4f32b2f93e5
Dawid Dziurla <dawidd0811@gmail.com>
2020-10-05 14:49:12 +0000

    trunk: update 0.6.0 bottle.

--- Formula/trunk.rb
+++ Formula/trunk.rb
@@ -11,6 +11,7 @@ class Trunk < Formula
     sha256 "e30a89f19b89e892613e044bc11b7297c4a4e319cacb5f9738dc26a0f4f6c9a7" => :catalina
     sha256 "4d60c3565e55e1b72914f32a9a7fd546da25e852ba9fe1fadbcecd9c1b58c313" => :mojave
     sha256 "1951420ea7503d12523b1497deea5655737c7b1b95ca0b70696fec6457b715df" => :high_sierra
+    sha256 "caed041b63f3e513ad6e2205996e3ae5eabb9bc887747140bc70d3ff81db7b23" => :x86_64_linux
   end
 
   depends_on "rust" => :build
