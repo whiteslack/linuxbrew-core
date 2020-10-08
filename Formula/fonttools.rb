2923853b3b0cdeac06766d4fd78139debac9fc58
Michka Popoff <michkapopoff@gmail.com>
2020-10-08 22:36:32 +0200

    fonttools: rebuild for virtualenv fix
    Closes #21226.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -6,7 +6,7 @@ class Fonttools < Formula
   url "https://github.com/fonttools/fonttools/releases/download/4.16.1/fonttools-4.16.1.zip"
   sha256 "991eb05e0366af5a6e620551f950a4f49433c5a8de70770a7066bcbe78bb86cc"
   license "MIT"
-  revision 1
+  revision OS.mac? ? 1 : 2
   head "https://github.com/fonttools/fonttools.git"
 
   bottle do
@@ -14,7 +14,6 @@ class Fonttools < Formula
     sha256 "c0fc8efd6caaabf9b3340e2eb6d84ebba1f4065e7f82869262f4fb69dfbe115b" => :catalina
     sha256 "ec3b0c20e2f9517ff3037e07b07881e4d8d4e0a765735bb6a136635ae6d13112" => :mojave
     sha256 "ee8e0c4b45dcbeb31f147907e257a1478972dca701556755a8149510245f571c" => :high_sierra
-    sha256 "082c534f88d75b6dc3cd8aca3380f4a9fe9ba6a76cf435a23128d1e85c8ad4b5" => :x86_64_linux
   end
 
   depends_on "python@3.9"
