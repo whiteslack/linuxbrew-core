53b391abef0ecae4081da0f53126d5d2218130e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-08 21:15:08 +0000

    codemod: update 1.0.0_5 bottle.

--- Formula/codemod.rb
+++ Formula/codemod.rb
@@ -19,6 +19,7 @@ class Codemod < Formula
     sha256 "e0a2e42e92636a4b0ccb54fdfa45ca5e73870315357d57c5c673d7710e3ffb7a" => :catalina
     sha256 "07b7c3807d776ca2991a321f32846a9613d0af356f69482f2653a5c30b7304df" => :mojave
     sha256 "b7b6b35729c1e0e990f4dc2d09c197d6c07cd8fbdacaa3d81decfe16e8856cb3" => :high_sierra
+    sha256 "76f441b50d78411faa44ddfaa082e3d04b96bbcc20dc9f44ca6510227b5c1fde" => :x86_64_linux
   end
 
   depends_on "python@3.9"
