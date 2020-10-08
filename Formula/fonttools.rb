8fb73458d9d17e14f6e939f3ae850c3320740d90
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-08 20:45:53 +0000

    fonttools: update 4.16.1_2 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -14,6 +14,7 @@ class Fonttools < Formula
     sha256 "c0fc8efd6caaabf9b3340e2eb6d84ebba1f4065e7f82869262f4fb69dfbe115b" => :catalina
     sha256 "ec3b0c20e2f9517ff3037e07b07881e4d8d4e0a765735bb6a136635ae6d13112" => :mojave
     sha256 "ee8e0c4b45dcbeb31f147907e257a1478972dca701556755a8149510245f571c" => :high_sierra
+    sha256 "baea3e221d17b5c2576d00b9176f672946bcad0a7eda6ee7940f4256e41cb555" => :x86_64_linux
   end
 
   depends_on "python@3.9"
