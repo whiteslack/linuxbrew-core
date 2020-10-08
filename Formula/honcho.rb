50de2d35c7707aed1ce53cf1aeabf67c10204d91
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-08 21:15:52 +0000

    honcho: update 1.0.1_3 bottle.

--- Formula/honcho.rb
+++ Formula/honcho.rb
@@ -11,6 +11,7 @@ class Honcho < Formula
     sha256 "042378b35c51528de85dc5c1999605bfaf561063da18b50bc3665331b9193019" => :catalina
     sha256 "65e4f0673086e6cf37184b82faa1c78c8cf32dc642c9a70caeb8deb16e455f95" => :mojave
     sha256 "39a8cdb134c590156a7ca18d4342933aa19a5163d38ab1caf6f2079d0348b506" => :high_sierra
+    sha256 "3590a8c7314433dd0ce7e0e4646a64c5e86a54a0cfe2266abef6ce2cf95176ed" => :x86_64_linux
   end
 
   depends_on "python@3.9"
