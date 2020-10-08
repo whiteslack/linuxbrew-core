19e7c5398cbf4a6414dd7247ed4359cf663b4678
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-08 21:16:04 +0000

    hashpump: update 1.2.0_5 bottle.

--- Formula/hashpump.rb
+++ Formula/hashpump.rb
@@ -16,6 +16,7 @@ class Hashpump < Formula
     sha256 "96dc135554b1dfa6b432120e716ab925ed28f9ea570ee2741816bb3309fbc9bb" => :catalina
     sha256 "0f9dc011b37341b4b0c6817738811d4825910aab7f25c6a34fe62e85e679281a" => :mojave
     sha256 "9ca69bd8f3c736e915db2f5b80de0b804170f6f2a71876fa4656c788187db6e7" => :high_sierra
+    sha256 "aa480a552bb911194c144299082490371a216e7c27659b01c30dde23c9240c21" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
