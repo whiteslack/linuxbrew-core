f62f722138862e2d895affe184f5e145707d9f94
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-10 08:00:00 +0000

    awscli: update 2.0.56 bottle.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -12,6 +12,7 @@ class Awscli < Formula
     sha256 "2208ab5d95330760932cb33031a442f323f3af82265d365b88a7667bad0166c9" => :catalina
     sha256 "c2934f8ae4c6f9e6fc2dfb9ba8908a25fa755e87bf0b768072571d3eadcc9fae" => :mojave
     sha256 "c194c1cd3583b059771c2e18fe4581b60c28ff6eedabf2dfb1ff47e57eb52b39" => :high_sierra
+    sha256 "d930d656518d640ead0785c83f1f04714e43287267e85e48a20ff747e23140a7" => :x86_64_linux
   end
 
   depends_on "python@3.8"
