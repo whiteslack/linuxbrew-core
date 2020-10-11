5c1c0563c63d678dc844a4186a402e69b490fed5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-11 13:23:31 +0000

    glow: update 1.0.1 bottle.

--- Formula/glow.rb
+++ Formula/glow.rb
@@ -10,6 +10,7 @@ class Glow < Formula
     sha256 "e74da6ca3d25f57d2dfd56799d8ca109320086eb40fd42d3c904120b4d871bf3" => :catalina
     sha256 "f48a89f443dc2f9649f52dceef74d9bddfb758fa09457e515e1c4b0cfa3fb8d4" => :mojave
     sha256 "876a145c0a6d83547f43c317d05545a02c8781fc10460f7e5c660c33033bc69b" => :high_sierra
+    sha256 "75ff3cda2276edae082b89b1788527ef86e9bca19261beb921b0db4f6409e16d" => :x86_64_linux
   end
 
   depends_on "go" => :build
