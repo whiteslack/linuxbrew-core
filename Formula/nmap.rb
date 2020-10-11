acff13404f8b0574a78f6ab99b721bcd9fe9d371
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-11 14:21:29 +0000

    nmap: update 7.91 bottle.

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -14,6 +14,7 @@ class Nmap < Formula
     sha256 "5592fb8c2fe633a6339ee61901122c075a4b44c002e2887bddfb2c4b3aa2885f" => :catalina
     sha256 "ba808d31033d996488fdf56664de1cf424fc942db794ab7030d40a1caad93aa8" => :mojave
     sha256 "b9a5b9d54fb0af76b1ce343e94f142b3421309fbeb81078d73e41bc2a9d862ea" => :high_sierra
+    sha256 "d05a8f02cea4a640932703a473f15f4f46bf44cc81da41c26fa369306894bbe3" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
