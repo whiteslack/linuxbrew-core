3faaa49301aa77949c31e37f9869321de7ecdb5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-11 11:09:33 +0000

    xcb-proto: update 1.13_1 bottle.

--- Formula/xcb-proto.rb
+++ Formula/xcb-proto.rb
@@ -11,6 +11,7 @@ class XcbProto < Formula
     sha256 "f1784c79ed426c069f6a63bc93cfa81bdbf2927c88f44f46bdbe7721095a545a" => :catalina
     sha256 "632746e279433e8ec37e692bd9f90475d7a8cb16dbc743641677937e74974027" => :mojave
     sha256 "428b789f3406ebfc2c4b1857cb8ca900853e2cc75d314588002484a2a8648d87" => :high_sierra
+    sha256 "9790373fa0581b1c8b3b221a4ee71bb4c16b2f9ef4a16d5b4f0c158e3ffa68e5" => :x86_64_linux
   end
 
   depends_on "pkg-config" => [:build, :test]
