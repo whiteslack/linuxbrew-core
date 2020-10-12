d6a5d4b12ef73aa04c9cf4014bbb1e9e97583f47
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-12 19:14:30 +0000

    kubespy: update 0.6.0 bottle.

--- Formula/kubespy.rb
+++ Formula/kubespy.rb
@@ -10,6 +10,7 @@ class Kubespy < Formula
     sha256 "c803662722beea17aa638cfab61ca1b47326ca4e9c6fc8522d3e8776c43cb7bb" => :catalina
     sha256 "02dd7561ce07c576d9ab3de63dbcdf0c43ccc75a00260f44b56d036076059662" => :mojave
     sha256 "c573da0ba80217ac5ce529fa070b5ecfb398e1554c93f1d824b1df9bcc16c406" => :high_sierra
+    sha256 "7b408874289813f151e3f384f27789768436bd32c781db325a5ac715e0df1861" => :x86_64_linux
   end
 
   depends_on "go" => :build
