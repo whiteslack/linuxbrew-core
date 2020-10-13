5b4418c09958b0fe32a801d31251cf0521d9318b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-13 10:15:48 +0000

    dbdeployer: update 1.55.0 bottle.

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -11,6 +11,7 @@ class Dbdeployer < Formula
     sha256 "fb64fa2235307cde79cbdb62eb0b6bf0ed97da264f791fdb7b8a844194c9d1af" => :catalina
     sha256 "6cea16d3066446b68ba57f5cc8c5505b7dfc637b462a4a151bbab5077dd8f1f3" => :mojave
     sha256 "fb35d80d90b883f8c100bca714d37b7579e296ba5f27d00473cabf5c7c2ab84e" => :high_sierra
+    sha256 "915707dbdf3e1584a6a84098afdaa96da635a2d8cb028b4cf122389980414813" => :x86_64_linux
   end
 
   depends_on "go" => :build
