afa7c8fffa02608f9d857e501dbfb08d6b4abe5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-13 10:19:07 +0000

    libmtp: update 1.1.18 bottle.

--- Formula/libmtp.rb
+++ Formula/libmtp.rb
@@ -14,6 +14,7 @@ class Libmtp < Formula
     sha256 "9b305e731b8d8608b688bb5c8bb98192d4879eb944fd4b08c09daadf367b68fc" => :catalina
     sha256 "e4c497e80277170743a4ff8ddde06687a01f3afb053088b921b8399796f630ae" => :mojave
     sha256 "704cd1e718e42dc9284ca020a11c1788d8a222cb8a4ca939d6b289cd17cf86ad" => :high_sierra
+    sha256 "f6d670474aaeeefde53e8068fdd26eae9a5fb265813959f53f042c11740fa7b1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
