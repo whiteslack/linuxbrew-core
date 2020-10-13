d50b249174b5fc95f75927a7a229ee811111380e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-13 06:09:37 +0000

    vulkan-headers: update 1.2.157 bottle.

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -10,6 +10,7 @@ class VulkanHeaders < Formula
     sha256 "fdb764b9af98dfa1451e186f2923340db863b0e7e36ada87b72e38a9e7972009" => :catalina
     sha256 "271699f601bc77de5846f9280f7b7519eb55c1661ce34ebf010cbae2d217ab1e" => :mojave
     sha256 "4715d40056b8e43b916c1e09dc4f35e870232918546709923f515913c869f829" => :high_sierra
+    sha256 "cc0726c8afb9f65211085b3b0f5451db984e4593d281f05d0cf0cbea97253ad9" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
