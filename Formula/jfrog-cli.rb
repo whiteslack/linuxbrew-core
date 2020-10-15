3a38bdad90bd5969efacd8faa82084a42cae43ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-15 06:36:18 +0000

    jfrog-cli: update 1.39.7 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -10,6 +10,7 @@ class JfrogCli < Formula
     sha256 "0aa8616db8004b62e3d9de3b8b1dceb790a8c51832f4a4417d6280d4c47f0f16" => :catalina
     sha256 "7b4f709ec992f4e2c6d9458e1c02fcbd9c58138a5c8bf4be07d80340c53342f5" => :mojave
     sha256 "a1feaf504ae8bcb9eb3b530fa4d019fe4dcdebd5abb508f97fda0008d59f27a8" => :high_sierra
+    sha256 "721ae3402b4f211ce9446421a91673f1a1909cf734f1904a257c03dbc5917407" => :x86_64_linux
   end
 
   depends_on "go" => :build
