51b82996bf4bf5aa23c3ec7c6a0e5dee5fea5a6b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-15 08:32:19 +0000

    sslscan: update 2.0.4 bottle.

--- Formula/sslscan.rb
+++ Formula/sslscan.rb
@@ -16,6 +16,7 @@ class Sslscan < Formula
     sha256 "adc0501ff362f3f71de2074ff00e4160d2df6f1659028a3377c229572813f593" => :catalina
     sha256 "30875bd698e136fe81c9161ed5e5ee75817325828bf9c2766fc95e07caba4d73" => :mojave
     sha256 "71fc4b9ecbe7f67fca0a5d5ae231f5689bdfac4ac7d7ea45aefd6f8096026ebb" => :high_sierra
+    sha256 "436e5d02c7a78abff6fd32e511e80baee38a167b251b908e1fa6d505c76c181e" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
