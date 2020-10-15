fd1455ec53b6ce5966fe08d930342b6778f8ce36
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-15 17:37:44 +0000

    vgmstream: update 405509 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -12,6 +12,7 @@ class Vgmstream < Formula
     sha256 "74b726aca9e8a085ff1d5f4afe6f892f3620d2cb229c86276451fcb25e993a11" => :catalina
     sha256 "b233a6c088526b989be281b44ff467aec792ff8cfc69afed7329f60f82b51eb5" => :mojave
     sha256 "ffa46eab45d850eaa18f0f0e280a9e387d0332b89935e4a6cdc5f1e5455fd228" => :high_sierra
+    sha256 "754a6c6eb673af76acac8b20d621e23651dbbd34d9bc8b34a27edbf0674c1c9c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
