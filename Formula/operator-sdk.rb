a272d97009e3001b39d9bc206a383bc375b598c4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-16 09:59:25 +0000

    operator-sdk: update 1.1.0 bottle.

--- Formula/operator-sdk.rb
+++ Formula/operator-sdk.rb
@@ -11,6 +11,7 @@ class OperatorSdk < Formula
     sha256 "69874324c44d8731c7a0c572a1dbcbea2b27811ffd5d29d60f3c32ae46434287" => :catalina
     sha256 "88d91481811b2fba02c348d54a6f46b6303fccdd60b6a36ee4f56412a773698e" => :mojave
     sha256 "e2181db5b4f393784088f66377fe1fb410002e1faac0802e115209805ee9b064" => :high_sierra
+    sha256 "fe16704f7789a03bb67769884eda8ae8ff29843b402fd8846b8e5ee8ca5fd066" => :x86_64_linux
   end
 
   depends_on "go"
