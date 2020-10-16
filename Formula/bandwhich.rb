21f3a1d0c778db4d0feb7981fa201830dbd90901
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-16 10:03:26 +0000

    bandwhich: update 0.20.0 bottle.

--- Formula/bandwhich.rb
+++ Formula/bandwhich.rb
@@ -10,6 +10,7 @@ class Bandwhich < Formula
     sha256 "424d3eff3b11609ad4645c028b3806babf18d9457749486fceff2522e2dd703d" => :catalina
     sha256 "99d4980e850a91edc9e12749150151a0803aa0f2591a790f7236bc7031d1f8da" => :mojave
     sha256 "5ca8f58d406af543ec3833c190472cbefaa8fb614cd5f42cfc42392e3139283c" => :high_sierra
+    sha256 "f7b9fd2d47afe3b4f6f12bc76678b0785e2c4e90f7fb29105b1a7c17e1f331e0" => :x86_64_linux
   end
 
   depends_on "rust" => :build
