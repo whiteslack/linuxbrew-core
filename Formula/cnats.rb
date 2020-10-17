8475056448990557a4895347a02c531e51803cee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-17 15:31:21 +0000

    cnats: update 2.3.0 bottle.

--- Formula/cnats.rb
+++ Formula/cnats.rb
@@ -10,6 +10,7 @@ class Cnats < Formula
     sha256 "874dae9b5751b247f322df936983238c37fb25e4907b57258a8cbdbec7863802" => :catalina
     sha256 "cdce55913e70d44003278835ead106710f3221593ac4d47ff525c5f29c510d1c" => :mojave
     sha256 "885c923403383be453e7d200eb085b1238636c9b96213064d36c2fa71b709a55" => :high_sierra
+    sha256 "13f9dcf16a6b44a29c707ce05d13376cdb82c935c574b80955f8d9f1407c0336" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
