39e94878cb8e9ae5bf1c214984f0363723bc9d0a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:40:00 +0000

    ask-cli: update 2.18.0 bottle.

--- Formula/ask-cli.rb
+++ Formula/ask-cli.rb
@@ -16,6 +16,7 @@ class AskCli < Formula
     sha256 "29283147431e7711c038de81747082bf220336a90be6582864f2f24f0494340f" => :catalina
     sha256 "eccb3aeced7d78bc3c3d2cd7c33fc03ab964e33001e2a6f846b1d976398122ad" => :mojave
     sha256 "fff38a4bf3d2f70e958d47fee3e0fddd3a5e2cfa43a31214c80be07331f2eade" => :high_sierra
+    sha256 "e3acbb3b690f2847cb57c11d56e448bcbd878adc77ec72fed3868442c2fdeff1" => :x86_64_linux
   end
 
   depends_on "node"
