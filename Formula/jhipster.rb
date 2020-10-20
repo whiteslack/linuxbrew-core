84a6112dba2d814325c9059a149a167135c87765
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:44:08 +0000

    jhipster: update 6.10.4 bottle.

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -16,6 +16,7 @@ class Jhipster < Formula
     sha256 "2b1e8ae95a7926b1e78790f7735c4cacd6c631a806d6c1585db36d36f706d3cb" => :catalina
     sha256 "d7bfdb83872bb3422a5fdd11ad9f4046dc84d6764a09687893246176d55b4381" => :mojave
     sha256 "a4dcacb4539ff11137b39071eff111b0965e10fde63c3d8f9f9280475934c309" => :high_sierra
+    sha256 "12d68e9fdd5478a7d33768210fc36729c8d19b9b826eb3ee519955c3235db949" => :x86_64_linux
   end
 
   depends_on "node"
