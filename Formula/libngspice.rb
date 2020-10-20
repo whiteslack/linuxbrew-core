436f1111df7f9ad4c3609e1cf97f9b009e1e8d63
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:42:14 +0000

    libngspice: update 33 bottle.

--- Formula/libngspice.rb
+++ Formula/libngspice.rb
@@ -13,6 +13,7 @@ class Libngspice < Formula
     sha256 "0e5f16893b51a7c07f9f5aebd2d0e0f40404e380d1be9b81a179533baaaef218" => :catalina
     sha256 "f366e430c2ad73979c3d1869e6619db951e88aaf0a94fd3e9ea9a819c6fa4c58" => :mojave
     sha256 "15639064eb1fd7cdb856cfed521d266c42afe4ac752723aa87a8dcbaeeee9fa3" => :high_sierra
+    sha256 "694955a7d3cbdf52dc86b45351bf79f9020374dc6c29b592c361fc18ca31f101" => :x86_64_linux
   end
 
   head do
