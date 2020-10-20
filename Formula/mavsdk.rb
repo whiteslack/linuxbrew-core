292873502aa1a8d3d890b813440a246e606fb126
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:57:59 +0000

    mavsdk: update 0.33.0 bottle.

--- Formula/mavsdk.rb
+++ Formula/mavsdk.rb
@@ -16,6 +16,7 @@ class Mavsdk < Formula
     sha256 "f917ee8d44cea83ef72582be192b964287bf6e56c2c5b07f90dfb94454ef9f01" => :catalina
     sha256 "7d050565f30e504e61148af660d96b0e5b4cbd328ef36404ae3b5f4f8e9a7da4" => :mojave
     sha256 "5b8dbf7c16cd80f86283d24be140b579686231e9b77a344e7a87c35c795ff67e" => :high_sierra
+    sha256 "3bdd3ea464f671426597911f6d389a0c39514ec4db9f9e9cf34b09a7f2b00a7c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
