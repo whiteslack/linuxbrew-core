96fca6cbee9d2e8fe4a60ff6c0e02da241f806bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:39:16 +0000

    kepubify: update 3.1.6 bottle.

--- Formula/kepubify.rb
+++ Formula/kepubify.rb
@@ -11,6 +11,7 @@ class Kepubify < Formula
     sha256 "02f990a949cebef432e4355e90c5e4685d85f556519392ecb425a0d7e0730add" => :catalina
     sha256 "6f5f0a9dff4919fbdd57fece754775014f2528fdd86e0f6a3ed5b30333d14435" => :mojave
     sha256 "ea8a1abda1b013780b9475fdcd9ee1332fbb33d22e8e41a43bfd4ad7b99bbfd1" => :high_sierra
+    sha256 "e6f2c71c2f4911e3c95cb516ebf7f91f9bd0500147a86a51e08977bf854bb46e" => :x86_64_linux
   end
 
   depends_on "go" => :build
