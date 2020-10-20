7c7dd8663989869cae55420cf839c182c35a57f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 10:32:50 +0000

    ccls: update 0.20190823.6_1 bottle.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -11,6 +11,7 @@ class Ccls < Formula
     sha256 "f1215fa22a47c52ea83666f2ebc1c416a39a0b7b80d7f7767e4d049b97e3b8ac" => :catalina
     sha256 "0acf04329f231cd825d4abf3366a7bf5fa26717c4e1421c06b0fef262de1f7ba" => :mojave
     sha256 "b8c9a39e3fc523a5b70f2d25682365ce921761923bd35ac1b41ca4a7f43dce66" => :high_sierra
+    sha256 "0d20c9367bc7fa92a8069b94a3a503b1a1bb3444472afed6cc975e4e9db33b10" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
