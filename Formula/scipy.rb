9ad19c40fa39ac088f47728da047da82865356fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-20 12:53:36 +0000

    scipy: update 1.5.2 bottle.

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -15,6 +15,7 @@ class Scipy < Formula
     sha256 "be1e59d00561966180e718918c7d6caf1ea01a38020574dbb0d23021d11d19d2" => :catalina
     sha256 "cc394fd2371a6ed3eec03e72829c74b25d2a5e99d4cd42ddc181589c3c14530b" => :mojave
     sha256 "1479546b40f3ab749beb7ab6e07349b9fdd9e1ed1b0a2b64facf4b554a2c01eb" => :high_sierra
+    sha256 "a7f1a9a385fe01158d1e01a10f98e703b43e0ff06f2b32c4cf1001661f09c16b" => :x86_64_linux
   end
 
   depends_on "swig" => :build
