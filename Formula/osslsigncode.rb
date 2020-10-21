aa064a9df89dcaa3697973345f74cd02c613a52f
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-10-21 20:23:27 +0000

    osslsigncode: update 2.1 bottle.

--- Formula/osslsigncode.rb
+++ Formula/osslsigncode.rb
@@ -9,6 +9,7 @@ class Osslsigncode < Formula
     sha256 "964162e471801ec6335e1cb88fa7d71145a09acd7507f71d049af1edc6375f9e" => :catalina
     sha256 "6ce5ae481bea9b92e4baaf795dfbdaf6cb29d574189978012f641857ffe39113" => :mojave
     sha256 "2a70933b296047d0042df4e1c1361cab8d588ff70c36ef44f63ac01105ce32f6" => :high_sierra
+    sha256 "98541a57f790363fbd5efa26dbeb7a7b485a0109da5aa4683788c5a40479fd44" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
