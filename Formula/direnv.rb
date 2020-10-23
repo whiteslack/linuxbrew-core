9089f9d17da7f7d38e8a79953faa730fb56d8201
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-23 18:04:35 +0000

    direnv: update 2.23.1 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -11,6 +11,7 @@ class Direnv < Formula
     sha256 "4cc10d8fad64eb6d5f9a0eee257c77c7d812a0a6ea6d53439e001bf023ac1bf1" => :catalina
     sha256 "1307aa1a9a14aa8e4b04abc444d2d36c199c4ff8466d2ef3fba115b61e12ef21" => :mojave
     sha256 "d7cc71241d1e4656598051536dfb30f0d2ef18bf9134e3e899ff3ada3888cd6a" => :high_sierra
+    sha256 "0c3fbd6a853891b7db94a250e1998138a761067b09bde78266eb7035f113ac9f" => :x86_64_linux
   end
 
   depends_on "go" => :build
