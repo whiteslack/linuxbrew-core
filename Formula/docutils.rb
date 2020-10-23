63d60a7bf5800e85250c462266657543ae4084ab
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-23 18:54:18 +0000

    docutils: update 0.16_3 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -16,6 +16,7 @@ class Docutils < Formula
     sha256 "b29e292e287170881f5e88d0e05fa41420515081126e486332d9036c8024f9ef" => :catalina
     sha256 "8fe38011ae417e9f2476fdadc5a3c96558fa4f6ce7c86ac3961a71b9b35ecfbc" => :mojave
     sha256 "771eee4e347d10e5e9722e2c74f99c31e4c712aa06d422bd1c03903b2bba7b97" => :high_sierra
+    sha256 "78b8062ec4dd23c06784dddaf4e752ed20cb97d5b5fedc75496cb54ce21a8b5a" => :x86_64_linux
   end
 
   depends_on "python@3.9"
