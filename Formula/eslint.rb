6ca60a943187be2178518f7373f3cb023d4d5e55
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-27 12:16:49 +0000

    eslint: update 7.12.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -16,6 +16,7 @@ class Eslint < Formula
     sha256 "4c6a518a7b2955015bfa32bea6a98a5dc4661a128895e13e25f5d9d8b0d6f634" => :catalina
     sha256 "b7f7ea8376c9b7d3514923c225ffe89d4950c130551e611745745950995c788c" => :mojave
     sha256 "55c3a89a7f556e706b9b0f2dda2d24cb7309e9c21546f8f0967587fad9647666" => :high_sierra
+    sha256 "9dedcdeb3a81e7741ab6005d7f044cbd8d63aeb5873c032af7d6daaf5ca33393" => :x86_64_linux
   end
 
   depends_on "node"
