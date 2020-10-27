0e182c269817058b2c84f86935e6b2cf784844c9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-27 18:58:01 +0000

    sentencepiece: update 0.1.94 bottle.

--- Formula/sentencepiece.rb
+++ Formula/sentencepiece.rb
@@ -15,6 +15,7 @@ class Sentencepiece < Formula
     sha256 "34eaf54eed2f033c154575c04b517742dea72d9fdba2d80515348acb9afb8793" => :catalina
     sha256 "367c727302ab46993a8604b21f4276d051bb06e17dd91b27252639abee05d84b" => :mojave
     sha256 "81a320ad4d6d5c92493f471735bc50fc38ca580d97dd4c710928e743f48d3841" => :high_sierra
+    sha256 "ed294aeb6f0eda9ede6ee61c8ffd8d66641819581c30d75585c009d2e9ef7992" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
