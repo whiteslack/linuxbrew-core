9b964b0f09fee5cc7ee68fb453398f4cbc3784f7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-27 18:56:06 +0000

    tundra: update 2.15 bottle.

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -15,6 +15,7 @@ class Tundra < Formula
     sha256 "782e5cffac2df8f04a2f86a994a0e6f3dbc5d6c0627119fa2e7e8e9c048b58aa" => :catalina
     sha256 "0fb994652330be64e5375a8a21924c2b6f65505685dfaac7d1dcdf4f26f54016" => :mojave
     sha256 "065e6fc51e912585a278de152b5b149c1d86fc6dd3843d5add5b0a0230c6b118" => :high_sierra
+    sha256 "1c6e1c7ca50d187c7673b3a9b4b51c5aa5d4bf7be96ce77dd4b79a6e783f1c12" => :x86_64_linux
   end
 
   resource "gtest" do
