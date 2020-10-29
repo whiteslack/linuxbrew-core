cff1ab2a64b5a931e38b3023135f72189f4653de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-29 07:16:28 +0000

    ccls: update 0.20201025 bottle.

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -10,6 +10,7 @@ class Ccls < Formula
     sha256 "9eeac4bcb801299d73e1d8a838b9b75701cef86506928082c5b63603949fad4e" => :catalina
     sha256 "eb2e73b77b5c3221fe9bc2b3330b9a2456b95558fc3b326fc29d35739467847d" => :mojave
     sha256 "578e24f878766fdcc3e024ddfcd6d124fbeaa79c79e32b89f6286f58d1213894" => :high_sierra
+    sha256 "053cd63cec45fd12c4f8a4648ef41cf08a211c52da6ac6b68048bd0df5207464" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
