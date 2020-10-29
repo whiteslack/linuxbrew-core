ec0699dbff396511fdd4bae5b9c2c9accaaf413a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-29 07:53:55 +0000

    quantlib: update 1.20 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -10,6 +10,7 @@ class Quantlib < Formula
     sha256 "61d144264132e25c31f347dbdf3c44595be790e851d671159ffaa3d6027b0f04" => :catalina
     sha256 "0fb3b38c1c385bdfd8bbe930c3eb64b8b9d3cf4451715325e0f3717c9ade0b69" => :mojave
     sha256 "d6b3bda389b8d94685f47611d4abfa578d9b2166a2e9603ff07a6961a99f70c4" => :high_sierra
+    sha256 "7c677291a95ecf35d0acd8f43716873975416ce318b8632ae89b7473ba1f0853" => :x86_64_linux
   end
 
   head do
