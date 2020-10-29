c40fc7201a68077e9b519a0c3dfac6743c864b28
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-29 22:51:42 +0000

    okteto: update 1.9.4 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -10,6 +10,7 @@ class Okteto < Formula
     sha256 "30a4c5387930bcdf4c16091d3ed8e714c0223e0566501390564236e44c50ed2a" => :catalina
     sha256 "4f64ed6133e285e29919ca26ae309f2a58966ddb4784e12295b41512b91d494b" => :mojave
     sha256 "330d4fc0e555543b4b45bfce0394520d981d03a8c6a920a510409a9e51793155" => :high_sierra
+    sha256 "dfb847c2cbfa730032dfaeb31dfba58b6e4fbaaaf07f504a35e68a9352dc542b" => :x86_64_linux
   end
 
   depends_on "go" => :build
