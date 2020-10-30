884cb0e8ecfc104bb4f1ae603fc43f9ac3777504
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-30 09:01:08 +0000

    libass: update 0.15.0 bottle.

--- Formula/libass.rb
+++ Formula/libass.rb
@@ -10,6 +10,7 @@ class Libass < Formula
     sha256 "427b18a8c9c8c5331553c0e814bf4e4c6f965cc53715d89a0ad3ba66b8e231c4" => :catalina
     sha256 "64f2a67f35510fe088f3e6e18075d5e08e93081d958fcee6b65ee29ab3b730ad" => :mojave
     sha256 "881db49f437027abdae60f4c849097b720216bcfa197589aea373b5f3451f9ef" => :high_sierra
+    sha256 "6761a8d9bd4879982b6233241f74990addac23b6adf847041fa4ca515f983f0e" => :x86_64_linux
   end
 
   head do
