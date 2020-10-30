a95cc469feb554636e780e75c846652ab81613e4
Michka Popoff <michkapopoff@gmail.com>
2020-10-30 21:46:55 +0100

    openjdk: fix sha256 for linux

--- Formula/openjdk.rb
+++ Formula/openjdk.rb
@@ -42,7 +42,7 @@ class Openjdk < Formula
       sha256 "386a96eeef63bf94b450809d69ceaa1c9e32a97230e0a120c1b41786b743ae84"
     else
       url "https://download.java.net/java/GA/jdk14.0.2/205943a0976c4ed48cb16f1043c5c647/12/GPL/openjdk-14.0.2_linux-x64_bin.tar.gz"
-      sha256 "bb67cadee687d7b486583d03c9850342afea4593be4f436044d785fba9508fb7"
+      sha256 "91310200f072045dc6cef2c8c23e7e6387b37c46e9de49623ce0fa461a24623d"
     end
   end
 
