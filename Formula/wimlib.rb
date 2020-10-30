9a484f431267c105a67b462290cf3c2202c78365
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-10-30 14:11:12 +0000

    wimlib: update 1.13.3 bottle.

--- Formula/wimlib.rb
+++ Formula/wimlib.rb
@@ -14,6 +14,7 @@ class Wimlib < Formula
     sha256 "cca83d6932d6acad1449a9e55509868e7e403b1b360e1da34ad5d4e1900def87" => :catalina
     sha256 "edeeaa5d20b4612eb24252ef825cec5c4a9c6e36d31f93d8901f6575bffeaeeb" => :mojave
     sha256 "cc317323e8e2a95cf69518cb6257ae2de1bd548b8e76c3c404e057cfc27bdcf6" => :high_sierra
+    sha256 "af3416aa610e9c169575cb6e89d1a42bc19c6cf2f0e7a794d70064e43962e4c2" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
