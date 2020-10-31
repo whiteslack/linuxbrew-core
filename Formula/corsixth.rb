411fcdc1e0397453dafca7624f4aded274c22ba7
Michka Popoff <michkapopoff@gmail.com>
2020-10-31 10:40:58 +0100

    corsixth: xcode dep is a no-op on linux now

--- Formula/corsixth.rb
+++ Formula/corsixth.rb
@@ -14,7 +14,7 @@ class Corsixth < Formula
 
   depends_on "cmake" => :build
   depends_on "luarocks" => :build
-  depends_on xcode: :build if OS.mac?
+  depends_on xcode: :build
   depends_on "ffmpeg"
   depends_on "freetype"
   depends_on "lua"
