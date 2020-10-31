38452001ba7f5d762a41ffb021a214c53efbf817
Michka Popoff <michkapopoff@gmail.com>
2020-10-31 10:44:06 +0100

    llvm@7: xcode dep is a no-op on linux now

--- Formula/llvm@7.rb
+++ Formula/llvm@7.rb
@@ -24,7 +24,7 @@ class LlvmAT7 < Formula
 
   # https://llvm.org/docs/GettingStarted.html#requirement
   depends_on "cmake" => :build
-  depends_on xcode: :build if OS.mac?
+  depends_on xcode: :build
   depends_on "libffi"
 
   unless OS.mac?
