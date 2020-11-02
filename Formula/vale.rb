42952e1b2915bec5a7d80b7d4ed94f8ffdcd7419
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-02 18:18:25 +0000

    vale: update 2.5.1 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -10,6 +10,7 @@ class Vale < Formula
     sha256 "20fc18d4ed90e76ad8c5d3c7c681a9ae2dc14f584901628b25d30faa027655e7" => :catalina
     sha256 "c27b0b54f6f1bc2563c76a2b58c3369a6b5dc8de5350f4cf1bf08694e73e44dc" => :mojave
     sha256 "bd28283c640fd8e1ab5eb84dd6086bd1a6bd316f78b6dedd29f78b2720c79bc8" => :high_sierra
+    sha256 "2fef1851f2889f3192e86bb46100a80afa66daa0fa682def8a63e671048792d8" => :x86_64_linux
   end
 
   depends_on "go" => :build
