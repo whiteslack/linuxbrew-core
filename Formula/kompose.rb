98f26cd5aef8f0621f16cdd2bd166145578d57da
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-02 22:12:38 +0000

    kompose: update 1.22.0 bottle.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -10,6 +10,7 @@ class Kompose < Formula
     sha256 "34da28575e40dd6c1bb1fcb36e073aa7d8236f4d8c16a33876cdaa2bcd4f7af2" => :catalina
     sha256 "2f6bf388c3aa7d51a9151f39378911b7d1a6cd16505ada04eba05b7b65e7ec78" => :mojave
     sha256 "8f727cb8dce4e8f5090c856ef6725f000d3618d6129868a0057293e449f1c79a" => :high_sierra
+    sha256 "8cd49588d70aac2e3e42bf53c3ce802fe47850a4d83bcd058f983a9c8e821247" => :x86_64_linux
   end
 
   depends_on "go" => :build
