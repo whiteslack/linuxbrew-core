54deb923c3f5257caaff9698a019ca1ca3f51afe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-03 12:02:16 +0000

    taskwarrior-tui: update 0.9.0 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -16,6 +16,7 @@ class TaskwarriorTui < Formula
     sha256 "99d5dc70534dc556b5ee04f65325c1fe1e6edf8e06b050403c77dc12bf24acd3" => :catalina
     sha256 "1ed0f1608949bb581da1ecfdd6dec512b70206e3a4593cddff72542bee66d1ef" => :mojave
     sha256 "2de2c50dffa8368ecda125f685559f685dc10765ca9054083dba732e22bdfe46" => :high_sierra
+    sha256 "14d2d1da5b052631b54a66174e51eafe3f864385f00e29d560e79989e51efc3c" => :x86_64_linux
   end
 
   depends_on "rust" => :build
