071501bd4fe2bfb1e1850ccff67983a5896e8e08
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-03 12:23:54 +0000

    keydb: update 6.0.16 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -10,6 +10,7 @@ class Keydb < Formula
     sha256 "399a17386d452bac3d05d7997af196954b4dddb4dd3c2dd753e112405c8eefe6" => :catalina
     sha256 "19e86e52f647fb1557683cd6f4b682a5e150095f0f99e13c19b7816c81d3bd17" => :mojave
     sha256 "ab28323dbffb9fcc0f11eeb18d9490808592b5caab0f8168e310937abb83b212" => :high_sierra
+    sha256 "97f837f813ba24794577daac35977c7b661fa28afab7fe022e63fd784f47e4e8" => :x86_64_linux
   end
 
   on_linux do
