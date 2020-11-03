19fc3a97ac98a67025cddc26d229d87cb25b8970
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-03 12:08:09 +0100

    keydb: reorder

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -5,15 +5,15 @@ class Keydb < Formula
   sha256 "809369321d1a98a57337447cce0fd84197dd3c9b493ec1ea2e29268d8534ee5d"
   license "BSD-3-Clause"
 
-  on_linux do
-    depends_on "util-linux"
-  end
-
   bottle do
     cellar :any_skip_relocation
     sha256 "399a17386d452bac3d05d7997af196954b4dddb4dd3c2dd753e112405c8eefe6" => :catalina
     sha256 "19e86e52f647fb1557683cd6f4b682a5e150095f0f99e13c19b7816c81d3bd17" => :mojave
     sha256 "ab28323dbffb9fcc0f11eeb18d9490808592b5caab0f8168e310937abb83b212" => :high_sierra
+  end
+
+  on_linux do
+    depends_on "util-linux"
   end
 
   def install
