97c28d651ba178522bd25717791fd46c1204ba89
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-04 10:56:29 +0000

    yh: update 0.4.0 bottle.

--- Formula/yh.rb
+++ Formula/yh.rb
@@ -11,6 +11,7 @@ class Yh < Formula
     sha256 "1a2425d399a63df18758dfabf9d50da2559fb489c32bfb4462d7437f64fc0817" => :catalina
     sha256 "69f1ab9c740906f04924c780cb512ea26fa0c51bdf66be85c71c4cbaa9dc6ca1" => :mojave
     sha256 "184eb9a41954f7a3d11f3065dfab42085a724c617ec635681e05784eeebe6329" => :high_sierra
+    sha256 "bb931efbbdf0b82803769d855550c979a6a7406813c5da5035de3fe0f3e42489" => :x86_64_linux
   end
 
   depends_on "go" => :build
