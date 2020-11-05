4566928eeeb62190e0f50f9e3204d663b861c6cd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-05 16:47:28 +0000

    cheat: update 4.1.1 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -10,6 +10,7 @@ class Cheat < Formula
     sha256 "c8157571d901d5b1a92214a29e70202829f1a336523782a1f63fc8ccce520a96" => :catalina
     sha256 "aa58b40807dbee5f5127a66f5fa0b244da42344d88ceb67dd0ee8d03e1915618" => :mojave
     sha256 "d0f7dbb233a37fffa2c76fd3bded84af3f6a95d5a56bc1bd0e36551dbbe8c9b8" => :high_sierra
+    sha256 "79a383ae9148252d7d66b00d056c91e6bf3d1391042f8dede0def1ce5ccd8c05" => :x86_64_linux
   end
 
   depends_on "go" => :build
