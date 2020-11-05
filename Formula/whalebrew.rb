6e84a5ee9ce7b606a242b2dea76e2526a0e6dbf0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-05 06:55:29 +0000

    whalebrew: update 0.2.5 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -12,6 +12,7 @@ class Whalebrew < Formula
     sha256 "13a6c59ba4c19570fcccef4b187755a4dfcc26038faba5168a3eb33644ade588" => :catalina
     sha256 "0aeae5e296d00a1b00a53b6a5b9c9aea39731826ea882e3adb51e51bd3fed653" => :mojave
     sha256 "14b39018ed3d1fa076250354d86e3f9296de24bd5a57144cd265666cbe29a6ee" => :high_sierra
+    sha256 "9c52faa573edce1a1831d112c24daa6bcfd3cde7a2d3bc25c74bcda84c9dc9fd" => :x86_64_linux
   end
 
   depends_on "go" => :build
