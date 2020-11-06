b8cbb3aed073f62b01148718e9ccbc59cc8a3f5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-06 18:13:20 +0000

    frps: update 0.34.2 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -11,6 +11,7 @@ class Frps < Formula
     sha256 "91891a9763996630549e03123860518ef5297468837478d8c39936722c7af3c2" => :catalina
     sha256 "19c3257eb15eae43f3ce057ffe0eaefa21c642959b8705e1d522292df9d3f818" => :mojave
     sha256 "924d515b418239e8e51e10fa8145ca667ef4a8730241be1f0f02d79cce527e08" => :high_sierra
+    sha256 "e148efd12aacc30cbf4afc31013a058a9fc714272b4694cbab8cdc04bb103685" => :x86_64_linux
   end
 
   depends_on "go" => :build
