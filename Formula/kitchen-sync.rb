83c34b5e4edb7c2b329f39e752e3562940dddaf3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-06 18:14:32 +0000

    kitchen-sync: update 2.10 bottle.

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -16,6 +16,7 @@ class KitchenSync < Formula
     sha256 "6b5366aa8413f25ff8985cb7d195723a78c9c42bc4af1eb50b9980e1694e605e" => :catalina
     sha256 "900f81a4a99ad8b1aef571d8c1d20de1edf102dbd9d202832c6f96a09b6f4454" => :mojave
     sha256 "80e91aab20f98adb5265fe24d39cbc4b8a10d66d2ec6663ad7ec294206554eb2" => :high_sierra
+    sha256 "cdeb9c975d62f436a25f3b68bda324e42b28eb10035c27b5ada4a9bf03aeb5f1" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
