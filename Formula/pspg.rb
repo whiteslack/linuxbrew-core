14071fdc87c12f06c6b6df608b780d237930b7ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-07 15:30:55 +0000

    pspg: update 3.1.5 bottle.

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -11,6 +11,7 @@ class Pspg < Formula
     sha256 "21e2efbe6cfaabe7e6b14f1457a61ffd2749b641ed3d3d252f849537ec4cae10" => :catalina
     sha256 "cb76982d14b5490e86d2664b02f127eb7848ed792a2eb61624fde9467411f6fd" => :mojave
     sha256 "51dba789c8fb09d7317ef41383405187188ac1b68099df0a9358bc75fbf2df38" => :high_sierra
+    sha256 "2e473a9b905962c527ccc6621254e08f9f4f7b58020d0217512ded3d1dcb222d" => :x86_64_linux
   end
 
   depends_on "libpq"
