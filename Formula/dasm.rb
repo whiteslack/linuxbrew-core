e167d379a65515456e0968e4895e9977c4432791
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-10 22:54:45 +0000

    dasm: update 2.20.14.1 bottle.

--- Formula/dasm.rb
+++ Formula/dasm.rb
@@ -11,6 +11,7 @@ class Dasm < Formula
     sha256 "354cf4953e70e7518fc7ee0b0861a0be21fa80770a60d18a2c0ea0d31deb979d" => :catalina
     sha256 "43a9c82d0ed5d8466cdf1bd749c3a94710f76c5a1f1599a5a4538a58616bc95f" => :mojave
     sha256 "145c79491ba96ba7d21f4085ff3cedf482555e46c9c334fe6c9b2458202bfb8c" => :high_sierra
+    sha256 "1e9e797aef1b736f37a23cb6d45dfe609b0db63e8738c804c06d02629fcefddb" => :x86_64_linux
   end
 
   def install
