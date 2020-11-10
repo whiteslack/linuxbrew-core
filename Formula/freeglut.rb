12eb8209be202d2b06d6591d1874a3503bf5fce2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-10 23:16:28 +0000

    freeglut: update 3.2.1_3 bottle.

--- Formula/freeglut.rb
+++ Formula/freeglut.rb
@@ -15,6 +15,7 @@ class Freeglut < Formula
     sha256 "21e92d3aa8a1615937c6776292dd823912220d272a4a437f66917d1e6dd0b655" => :catalina
     sha256 "8d71afe59334afe060d513d68e8c76b3fc0927cf05d61b146dd1444c66d5db35" => :mojave
     sha256 "0a30955c90e594481f1ebf4dd218065768386704e2fdcdc0aae45055171dfd2d" => :high_sierra
+    sha256 "55bfe3f83784fc2758d4ff0c69feffc6283a1a31951dd44d005de9eefe686cfa" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
