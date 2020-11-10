8fb1e2f53c14311c7199bff941d4e675aa6eeb74
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-10 09:29:34 +0000

    dotenv-linter: update 2.2.1 bottle.

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -11,6 +11,7 @@ class DotenvLinter < Formula
     sha256 "ea3cd8fbb12d6c5aaa962d20a07f6a3aabb418cf6142a9fff057fecbf63c02cd" => :catalina
     sha256 "6dc5965ef3f36811e133c28636dec3b06eed99782fbfc980e83d775722a979e0" => :mojave
     sha256 "8bdecac347a74490c4972a31a4c713cfb438e5531b5dc626fea42d495ad932c3" => :high_sierra
+    sha256 "471f14875f0aea1d681cb3125bbbf409392018c2edcca7f70ac4f2159f43dd5b" => :x86_64_linux
   end
 
   depends_on "rust" => :build
