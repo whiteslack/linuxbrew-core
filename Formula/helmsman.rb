650c23f96e0a2e34122cea573cf12351c6c5f777
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-11 11:48:14 +0000

    helmsman: update 3.5.1 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -11,6 +11,7 @@ class Helmsman < Formula
     sha256 "2db1845a0356b6b97f3b59b0fd21264c42b3cc3a2b1a4a9bcd792d26f5594141" => :catalina
     sha256 "b10d423fe179460c1a20da0af629869d848295c8c8d4eb73fbaa12ac389daeaa" => :mojave
     sha256 "20ff81067cd2d40abeb0356f2da97acb75dfec00326416ce99600efe58296369" => :high_sierra
+    sha256 "e6b727562114272ff168b647527a2e25470384cad02ba0dab1465adb209a28ed" => :x86_64_linux
   end
 
   depends_on "go" => :build
