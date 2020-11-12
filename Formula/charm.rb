e0ffae014ae67c7f3cae0481e4bcd6fc5ad45c46
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-12 10:36:24 +0000

    charm: update 2.5.1 bottle.

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -10,6 +10,7 @@ class Charm < Formula
     sha256 "b2a4c9b953f6a54c51a171d819cfddc0655caf857ec72c3ec5136493f9296b24" => :catalina
     sha256 "58e3a054f6b6ff73184e1f2909ee58117bd023bc28e448af2d6fe2e82b192735" => :mojave
     sha256 "389d0723e4bcc34bc7747e66f7f721a36cf1ab63623ec23982d68ca2623e3f29" => :high_sierra
+    sha256 "e149e78ec5ae240183c8f5c497f7eb5312c0e75a3fbba953b9a227b951eff34e" => :x86_64_linux
   end
 
   depends_on "go" => :build
