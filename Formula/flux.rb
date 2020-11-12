7ceec4a84855849c3730ce1549bba2499bbfb9f4
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-12 15:24:28 +0000

    flux: update 0.94.0 bottle.

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -17,6 +17,7 @@ class Flux < Formula
     sha256 "b1b981c9c6d0189d3c4fc93795077dfb04ee8349b33b19929f76c79993f373c0" => :catalina
     sha256 "bad9cd01d1a588e2a8de1ddbdb85b152d98645232781f3bfcf718c6b2282f48d" => :mojave
     sha256 "94d271d3a87ae8086197e5945488be7b8684a3fefc57a3ead34e26253b05fede" => :high_sierra
+    sha256 "a9713b830c66f194a5eba838fa3d34651cf163cd2e0a286ec4101a16a8ba9202" => :x86_64_linux
   end
 
   depends_on "go" => :build
