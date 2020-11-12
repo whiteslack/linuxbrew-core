493907be58d5488f5f5396422b012cb9ff5e4cc0
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-11-12 14:26:26 +0000

    glyr: update 1.0.10_1 bottle.

--- Formula/glyr.rb
+++ Formula/glyr.rb
@@ -12,6 +12,7 @@ class Glyr < Formula
     sha256 "45d36208e031f97c1202824c6a6a0a9e97d777fae91ce7cddc3ca17c3168d31c" => :mojave
     sha256 "fb3ef9186aae754a62a466aae16471049bdaefcc168106fc6f0097e937115524" => :high_sierra
     sha256 "04cbfc6d3294d068b3a97bfb5235aed84b3e95478f8f3e873be17127142b07f6" => :sierra
+    sha256 "77a732b0e3b1d2d05a83f72982300ec510f54e220473947ab26bf309cde4bde1" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
