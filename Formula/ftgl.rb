6275705b26ebb9bee3a7da18578f7ab6f3f9948f
Michka Popoff <michkapopoff@gmail.com>
2020-11-12 07:49:39 +0100

    ftgl: use glu from core

--- Formula/ftgl.rb
+++ Formula/ftgl.rb
@@ -3,6 +3,7 @@ class Ftgl < Formula
   homepage "https://sourceforge.net/projects/ftgl/"
   url "https://downloads.sourceforge.net/project/ftgl/FTGL%20Source/2.1.3~rc5/ftgl-2.1.3-rc5.tar.gz"
   sha256 "5458d62122454869572d39f8aa85745fc05d5518001bcefa63bd6cbb8d26565b"
+  revision 1 unless OS.mac?
 
   livecheck do
     url :stable
@@ -18,11 +19,10 @@ class Ftgl < Formula
     sha256 "6462eb0b97ab120639f1a191f6e3a39419bbb813abd71f5c741303dbf0aed7fb" => :el_capitan
     sha256 "26db05485600adfb7ead23d04fae9b1ee1d1a4b7ac304e1453ad83b4b2c39f64" => :yosemite
     sha256 "50a41f3c95a363b52bc367abf4b5b9dc272d71c8b35fe8e63f058c7cf7162225" => :mavericks
-    sha256 "6eea160488d9acf34d539e8efd45276df9171351c9b5a380eafa43919d3ed578" => :x86_64_linux # glibc 2.19
   end
 
   depends_on "freetype"
-  depends_on "linuxbrew/xorg/glu" unless OS.mac?
+  depends_on "mesa-glu" unless OS.mac?
 
   def install
     # If doxygen is installed, the docs may still fail to build.
@@ -35,7 +35,7 @@ class Ftgl < Formula
                           # Skip building the example program by failing to find GLUT (MacPorts)
                           "--with-glut-inc=/dev/null",
                           "--with-glut-lib=/dev/null",
-                          OS.mac? ? "" : "--with-gl-inc=#{Formula["linuxbrew/xorg/glu"].opt_include}"
+                          OS.mac? ? "" : "--with-gl-inc=#{Formula["mesa-glu"].opt_include}"
 
     system "make", "install"
   end
