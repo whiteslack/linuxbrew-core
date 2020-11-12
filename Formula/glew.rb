2c5f39b20420332071134dec6843f67e9b3442a3
Michka Popoff <michkapopoff@gmail.com>
2020-11-12 07:50:32 +0100

    glew: use glu from core

--- Formula/glew.rb
+++ Formula/glew.rb
@@ -4,6 +4,7 @@ class Glew < Formula
   url "https://downloads.sourceforge.net/project/glew/glew/2.2.0/glew-2.2.0.tgz"
   sha256 "d4fc82893cfb00109578d0a1a2337fb8ca335b3ceccf97b97e5cc7f08e4353e1"
   license "BSD-3-Clause"
+  revision 1 unless OS.mac?
   head "https://github.com/nigels-com/glew.git"
 
   livecheck do
@@ -15,14 +16,13 @@ class Glew < Formula
     sha256 "5f130b7557c1753c3880fc2eb16363de05a9d5a7d032294e8f8e744583df467f" => :catalina
     sha256 "dc1e74289200e3c1db6792f085f1216529b491fc463bc6205bcd40807a4dba31" => :mojave
     sha256 "1e2d9d489808104dfa3a4dab5662e200e1020b40b869bac45b6b84b8490cd936" => :high_sierra
-    sha256 "04846956d8601fa59d12ce4ad92627505f9b7cc84a356fbc83ec854958d1cac9" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   unless OS.mac?
     depends_on "freeglut" => :test
-    depends_on "linuxbrew/xorg/glu"
     depends_on "mesa"
+    depends_on "mesa-glu"
   end
 
   conflicts_with "root", because: "root ships its own copy of glew"
