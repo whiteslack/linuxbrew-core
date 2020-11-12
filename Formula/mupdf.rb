ec8f8661beede7900cdfe0346f2b6ceb146f0661
Michka Popoff <michkapopoff@gmail.com>
2020-11-12 07:51:48 +0100

    mupdf: use glu from core

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -4,7 +4,7 @@ class Mupdf < Formula
   url "https://mupdf.com/downloads/archive/mupdf-1.18.0-source.tar.xz"
   sha256 "592d4f6c0fba41bb954eb1a41616661b62b134d5b383e33bd45a081af5d4a59a"
   license "AGPL-3.0-or-later"
-  revision OS.mac? ? 1 : 2
+  revision OS.mac? ? 1 : 3
   head "https://git.ghostscript.com/mupdf.git"
 
   livecheck do
@@ -17,17 +17,15 @@ class Mupdf < Formula
     sha256 "b656ec4a7c2cbb3b55b52678e5129bbeb27215c793cd6e3876d40a51d293bd84" => :catalina
     sha256 "5b06c1203b68608f64d082b83db659a46d98a849d79530bfa83f28adb970e17e" => :mojave
     sha256 "32dc7277f5dce0762c695ecf15f3ec745ec7767afec09f6acefc4aea86386873" => :high_sierra
-    sha256 "82504bf74fd1ffaa71854309547a2fec3ac5ad428d57579c0548a53efc76a106" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "freeglut"
   depends_on "mesa"
   depends_on "openssl@1.1"
-  depends_on "openssl@1.1"
 
   unless OS.mac?
-    depends_on "linuxbrew/xorg/glu"
+    depends_on "mesa-glu"
     depends_on "linuxbrew/xorg/xinput"
   end
 
