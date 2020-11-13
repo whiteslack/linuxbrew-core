980285cc35eb1b15642965ab9e64181e7ee42fd3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-13 08:24:03 +0000

    lazydocker: update 0.10 bottle.

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -11,6 +11,7 @@ class Lazydocker < Formula
     sha256 "beb117f1a6967c87d619fb493ba6a4ec03b528ff55e568b8f740245e0679d1aa" => :catalina
     sha256 "0caabdbda1ce740c6a76952d40c8b8dc28acf936860d5f117113a9c3370ba84c" => :mojave
     sha256 "129c5552e5b09b98e170878c7713e52310daa8c3ab507200935415e4621ee1ce" => :high_sierra
+    sha256 "8b90e220bae9ee3ca6bc79e68c354b7ff848c5d001749a7853a659d84e6990d8" => :x86_64_linux
   end
 
   depends_on "go" => :build
