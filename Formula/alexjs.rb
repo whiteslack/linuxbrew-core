b844ea081fb501cd48eb982ff24ae0843d4ac5d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-14 14:32:48 +0000

    alexjs: update 9.1.0 bottle.

--- Formula/alexjs.rb
+++ Formula/alexjs.rb
@@ -12,6 +12,7 @@ class Alexjs < Formula
     sha256 "e37814ad3315475e5484cc5c334089213a023ea443ccb2207702b8667f3e1a0e" => :catalina
     sha256 "7c2d9fedf79264bd59e73b7f062512e03afdd1e6a66152c233996471636549f1" => :mojave
     sha256 "462578faccc1fd6ba5df072a50c90c28522067e6cbba84a3b6a6dfa29c7a2eaf" => :high_sierra
+    sha256 "c6f12901758a22ce864540822fba0c1ddac2e602b4cc3697397d51a4192dc4b3" => :x86_64_linux
   end
 
   depends_on "node"
