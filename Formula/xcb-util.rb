e5310836ba3c705b38d47486380761dea2dff5b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-14 15:03:24 +0000

    xcb-util: update 0.4.0 bottle.

--- Formula/xcb-util.rb
+++ Formula/xcb-util.rb
@@ -10,6 +10,7 @@ class XcbUtil < Formula
     sha256 "c161b6f0372d40ace1238507365c18a52581b798262c856099cd86eabc38c625" => :catalina
     sha256 "0979f730b01775f3dcb33c093132ec25a49912b99e679e774bae0e995fc3f73c" => :mojave
     sha256 "16578b76b505e33f0ccb428a947e475520d78f4dd7a56504ff9e0af9870793cc" => :high_sierra
+    sha256 "9e84cd3328e3f856fb039c9635de379863d43fce6a5e33a24a356f63cf5e28fd" => :x86_64_linux
   end
 
   depends_on "pkg-config" => [:build, :test]
