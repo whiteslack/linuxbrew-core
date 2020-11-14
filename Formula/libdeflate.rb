5221a5f33300d7e755aefcaa27fbc71c2dddc0b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-14 16:14:38 +0000

    libdeflate: update 1.7 bottle.

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -10,6 +10,7 @@ class Libdeflate < Formula
     sha256 "e270c74afc8fbd5171ba5b5e9cecd2c429e4934a6db9b0da1830b0a2a4298b8f" => :catalina
     sha256 "02fb406299f342554d4c236955b6c0cf0e917afb76ed7c8bfe622b03fd6a2b55" => :mojave
     sha256 "1c16f650b82c5e7b910b98249e635353d9beee3a3a0cd4f0c841b0d8d19e19c6" => :high_sierra
+    sha256 "a95dd72a6636853ef9a343ffc5f0c268b871be19309b7b2f278ddff3ec70bc77" => :x86_64_linux
   end
 
   def install
