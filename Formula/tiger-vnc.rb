b7503ad00f3a79d1c12047e3149e4701410e845a
Dawid Dziurla <dawidd0811@gmail.com>
2020-11-15 15:40:35 +0000

    tiger-vnc: update 1.11.0 bottle.

--- Formula/tiger-vnc.rb
+++ Formula/tiger-vnc.rb
@@ -9,6 +9,7 @@ class TigerVnc < Formula
     sha256 "b9a09483c45610c81dd29fc20a41b4fa8120e1353f736bb637732d4788e4bb28" => :catalina
     sha256 "c90bdf1ac012129c5d4caecd3e5acf2d110ca8cd68a8bcff6de07373149424db" => :mojave
     sha256 "2370d829c67ca1df886e47aca162c68034e138a10a93b846c31f1c927d84c435" => :high_sierra
+    sha256 "67db0f47bd15bc93d07689db2a89358f95332278c6a344997b01cb19a865f476" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
