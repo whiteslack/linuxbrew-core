700bd9a0f61c7bdf6f1a72299b612827038fdd92
Michka Popoff <michkapopoff@gmail.com>
2020-11-16 17:19:07 +0100

    patchelf: add linux bottle back

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -16,6 +16,7 @@ class Patchelf < Formula
     sha256 "bca91d5be894ea5ebc1c7b0af93027e7669c460b7f792455470e78e73fc16d52" => :catalina
     sha256 "d4d4b739c36108e8f794b19a76a44efeed42baeeb4f5dcd61002c7ba29105dfd" => :mojave
     sha256 "d7c841a08ca1f9e4cc24fa6378e14f82f46dac6124d860777fb53161ac82a426" => :high_sierra
+    sha256 "7396e6cf5dbe62fae0e7fcba57f88586359561824df9cfe1127b4d88dd6f2a5a" => :x86_64_linux
   end
 
   resource "helloworld" do
