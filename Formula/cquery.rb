e15a0aafcbf2a7f5bed1330a76da131cde44acc1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-16 11:05:36 +0000

    cquery: update 20180718_2 bottle.

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -15,6 +15,7 @@ class Cquery < Formula
     sha256 "fc7c73b7d9132c879399fc65a3556d9382d9617dd5e59bc10a51eaed228be5f8" => :catalina
     sha256 "89a896183bd8e6635146263dc09c0f26464845f91637e5ca9557558aaa034139" => :mojave
     sha256 "555804325cb45d5450c0d8b47096b71b8445af5370a9fc1e71acb1a6e86d3398" => :high_sierra
+    sha256 "733705b1b8a693c30238c3d1f893274afc426e6b697e12cf04e5cf0ac6fb23d9" => :x86_64_linux
   end
 
   deprecate! because: :repo_archived
