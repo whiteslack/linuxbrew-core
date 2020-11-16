31aaccf2a479a5d88d64c67f22f794f2bbb708c9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-16 19:59:56 +0000

    zsh: update 5.8_1 bottle.

--- Formula/zsh.rb
+++ Formula/zsh.rb
@@ -15,6 +15,7 @@ class Zsh < Formula
     sha256 "aaf19f69f79ac2ef80ff31d3b2f0017f400bf40022f8626d5ae046990961a5f5" => :catalina
     sha256 "a40a54e4b686eb75f04e7dcc57391245a4f6b08a39825f7f6ebc9f76ebcbff46" => :mojave
     sha256 "edfbc09a9571fadf351e0f94e545a88aa33763518a3330c0bae497a6a259d63f" => :high_sierra
+    sha256 "dd9af50837c589551654ab4677b054978dcdf332b6a17c23caab7070ca58a6ae" => :x86_64_linux
   end
 
   head do
