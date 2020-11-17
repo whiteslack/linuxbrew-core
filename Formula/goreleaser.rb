3f13f8d019691d7e23106d6d0512e6975772e8ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-17 06:49:17 +0000

    goreleaser: update 0.147.2 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -11,6 +11,7 @@ class Goreleaser < Formula
     sha256 "315d7f4c9743c64e6dc7f6cb4d6cc2989a3a4151dd9e8c32a48ea1689645d32b" => :big_sur
     sha256 "603f55f4f93607d2b47e72e3d2a5282628cffe9c997b85eb02e6389f2798dca4" => :catalina
     sha256 "99ac34034eda1992657110082d2c33eb93757a3e1ca27800193234a48773f2c6" => :mojave
+    sha256 "95d685ffbb5f7b56443912b2344a62d59cd10f426af1af1f1424cd551dbf6efe" => :x86_64_linux
   end
 
   depends_on "go" => :build
