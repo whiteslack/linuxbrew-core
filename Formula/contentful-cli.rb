c8f3cc4cb5e243d29dd112018b94000ee30e8589
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-18 20:08:56 +0000

    contentful-cli: update 1.4.54 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -17,6 +17,7 @@ class ContentfulCli < Formula
     sha256 "5b23eeacb772b093b241f8ec677add5fd0861310be4fc78f579ba8617fea9cd6" => :big_sur
     sha256 "f5c145c4ec1678f210a915074e38eb5d1c7a327d14a1b12ca1ca7933fa32cd5a" => :catalina
     sha256 "bc4bb76c0f90e575342e75ca0d840c69eb8f53eb7a72c630756c517f1e35ffb5" => :mojave
+    sha256 "5ab4d1a42e2a0b189402bac0baea6c3356bed7140b21eb3b8d73837b71bb8ea8" => :x86_64_linux
   end
 
   depends_on "node"
