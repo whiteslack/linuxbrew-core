7afcd6a943c55fe1ca97574628691cda0a7d4de5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-23 10:13:45 +0000

    parallel: update 20201122 bottle.

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -18,6 +18,7 @@ class Parallel < Formula
     sha256 "144e116a689c4fea40febb62cf7ed46dba36eee17e3ead766f63ecd55392bd8b" => :big_sur
     sha256 "dc76d7573944f64ce047c3025320a12aef3c744d019449d62040d7121a476d23" => :catalina
     sha256 "32f6187f65e0b9b6c706a5eb6c6e24c8097a3701f5d8f7cc885bf0be82478cf0" => :mojave
+    sha256 "7e24471e4cc77c1d865413664e28db8a052e0155fa9996e596c4a7257e521855" => :x86_64_linux
   end
 
   conflicts_with "moreutils", because: "both install a `parallel` executable"
