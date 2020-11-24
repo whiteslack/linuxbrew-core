81eb01fd8610d4196502d15a69da122cd314be22
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-11-24 18:38:45 +0000

    patchelf: update 0.12_3 bottle.

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -16,6 +16,7 @@ class Patchelf < Formula
     sha256 "d83931e807f58c62f0b321b9523d16de6602415f0e19b3702d072b4dec382cb6" => :big_sur
     sha256 "344c4459a5b03099308520eb7ef906242bca77f08ac1660ac61b74ccd7871b1c" => :catalina
     sha256 "906cd9171c62947d8133b990bbc15ad7803bb5623f5b72332fa792a01c9634ac" => :mojave
+    sha256 "d0271d785f75d67fe542cf51791cf6cd75e66ffbc738e6fbd61fe77c0041e9ab" => :x86_64_linux
   end
 
   resource "helloworld" do
