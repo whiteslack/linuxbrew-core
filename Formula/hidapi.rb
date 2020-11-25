68981a249855ed378d6209afe1890529670abbf9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-25 18:18:14 +0000

    hidapi: update 0.10.1 bottle.

--- Formula/hidapi.rb
+++ Formula/hidapi.rb
@@ -11,6 +11,7 @@ class Hidapi < Formula
     sha256 "98f2859ea147e9c92e4925f0887062c8b6f5177eb98a1012b95d3b788cb58ea5" => :big_sur
     sha256 "9287809ecfeaeb3c89b1f9bf8babb31a8971b41c4a9795922ab774bfcc66559d" => :catalina
     sha256 "e9c2bec30d5d1e9e0f9f91c43510071ba17234cd968b33f161c56cbee23a4d8d" => :mojave
+    sha256 "76255d24660fc5a7d70c363b7bfd5c3896a166276dbd6620e61d13105baf54b9" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
