355c141d3d9bb519b5cffb7681654824d6b3ace9
danielnachun <daniel.nachun@gmail.com>
2020-11-25 23:44:09 -0800

    clozure-cl: fix build for Linux
    Closes #21805.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -20,37 +20,67 @@ class ClozureCl < Formula
 
   depends_on xcode: :build
 
+  on_linux do
+    depends_on "m4"
+  end
+
   resource "bootstrap" do
-    url "https://github.com/Clozure/ccl/releases/download/v1.12/darwinx86.tar.gz"
-    sha256 "9434fb5ebc01fc923625ad56726fdd217009e2d3c107cfa3c5435cb7692ba7ca"
+    on_macos do
+      url "https://github.com/Clozure/ccl/releases/download/v1.12/darwinx86.tar.gz"
+      sha256 "9434fb5ebc01fc923625ad56726fdd217009e2d3c107cfa3c5435cb7692ba7ca"
+    end
+
+    on_linux do
+      url "https://github.com/Clozure/ccl/releases/download/v1.12/linuxx86.tar.gz"
+      sha256 "7fbdb04fb1b19f0307c517aa5ee329cb4a21ecc0a43afd1b77531e4594638796"
+    end
   end
 
   def install
     tmpdir = Pathname.new(Dir.mktmpdir)
     tmpdir.install resource("bootstrap")
-    buildpath.install tmpdir/"dx86cl64.image"
-    buildpath.install tmpdir/"darwin-x86-headers64"
-    cd "lisp-kernel/darwinx8664" do
-      args = []
 
-      if DevelopmentTools.clang_build_version == 1100 && MacOS::CLT.installed?
-        # Xcode 11.0-11.3 assembler is broken. Try the CLT in case it is older.
-        # https://github.com/Clozure/ccl/issues/271
-        # NOTE: ccl NEEDS the system assembler - it is not compatible with Clang's
-        args << "AS=/Library/Developer/CommandLineTools/usr/bin/as"
+    on_macos do
+      buildpath.install tmpdir/"dx86cl64.image"
+      buildpath.install tmpdir/"darwin-x86-headers64"
+      cd "lisp-kernel/darwinx8664" do
+        args = []
+
+        if DevelopmentTools.clang_build_version == 1100 && MacOS::CLT.installed?
+          # Xcode 11.0-11.3 assembler is broken. Try the CLT in case it is older.
+          # https://github.com/Clozure/ccl/issues/271
+          # NOTE: ccl NEEDS the system assembler - it is not compatible with Clang's
+          args << "AS=/Library/Developer/CommandLineTools/usr/bin/as"
+        end
+
+        system "make", *args
       end
+    end
 
-      system "make", *args
+    on_linux do
+      buildpath.install tmpdir/"lx86cl64"
+      buildpath.install tmpdir/"lx86cl64.image"
+      buildpath.install tmpdir/"x86-headers64"
     end
 
     ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
 
-    system "./dx86cl64", "-n", "-l", "lib/x8664env.lisp",
-                         "-e", "(ccl:xload-level-0)",
-                         "-e", "(ccl:compile-ccl)",
-                         "-e", "(quit)"
-    (buildpath/"image").write('(ccl:save-application "dx86cl64.image")\n(quit)\n')
-    system "cat image | ./dx86cl64 -n --image-name x86-boot64.image"
+    on_macos do
+      system "./dx86cl64", "-n", "-l", "lib/x8664env.lisp",
+            "-e", "(ccl:xload-level-0)",
+            "-e", "(ccl:compile-ccl)",
+            "-e", "(quit)"
+      (buildpath/"image").write('(ccl:save-application "dx86cl64.image")\n(quit)\n')
+      system "cat image | ./dx86cl64 -n --image-name x86-boot64.image"
+    end
+
+    on_linux do
+      system "./lx86cl64", "-n", "-l", "lib/x8664env.lisp",
+            "-e", "(ccl:rebuild-ccl :full t)",
+            "-e", "(quit)"
+      (buildpath/"image").write('(ccl:save-application "lx86cl64.image")\n(quit)\n')
+      system "cat image | ./lx86cl64 -n --image-name x86-boot64"
+    end
 
     prefix.install "doc/README"
     doc.install Dir["doc/*"]
