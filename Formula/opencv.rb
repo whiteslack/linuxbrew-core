24ed7ccf98b0f13964b480f99daa9914af9009fc
danielnachun <daniel.nachun@gmail.com>
2020-11-25 23:44:09 -0800

    opencv: use brewed libraries and fix dependencies

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -35,7 +35,8 @@ class Opencv < Formula
   depends_on "tbb"
   depends_on "vtk"
   depends_on "webp"
-  depends_on "openblas" unless OS.mac?
+
+  uses_from_macos "zlib"
 
   resource "contrib" do
     url "https://github.com/opencv/opencv_contrib/archive/4.5.0.tar.gz"
@@ -88,7 +89,20 @@ class Opencv < Formula
       -DBUILD_opencv_python3=ON
       -DPYTHON3_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3
     ]
-    args << "-DENABLE_PRECOMPILED_HEADERS=OFF" unless OS.mac?
+
+    # Disable precompiled headers and force opencv to use brewed libraries on Linux
+    unless OS.mac?
+      args << "-DENABLE_PRECOMPILED_HEADERS=OFF"
+      args << "-DJPEG_LIBRARY=#{Formula["libjpeg"].opt_lib}/libjpeg.so"
+      args << "-DOpenBLAS_LIB=#{Formula["openblas"].opt_lib}/libopenblas.so"
+      args << "-DOPENEXR_ILMIMF_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmImf.so"
+      args << "-DOPENEXR_ILMTHREAD_LIBRARY=#{Formula["ilmbase"].opt_lib}/libIlmThread.so"
+      args << "-DPNG_LIBRARY=#{Formula["libpng"].opt_lib}/libpng.so"
+      args << "-DPROTOBUF_LIBRARY=#{Formula["protobuf"].opt_lib}/libprotobuf.so"
+      args << "-DTIFF_LIBRARY=#{Formula["libtiff"].opt_lib}/libtiff.so"
+      args << "-DWITH_V4L=OFF"
+      args << "-DZLIB_LIBRARY=#{Formula["zlib"].opt_lib}/libz.so"
+    end
 
     args << "-DENABLE_AVX=OFF" << "-DENABLE_AVX2=OFF"
     args << "-DENABLE_SSE41=OFF" << "-DENABLE_SSE42=OFF" unless MacOS.version.requires_sse42?
