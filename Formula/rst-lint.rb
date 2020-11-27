26f6f73b4a4d5b21b0e4ca88ba17aaead1b3c942
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-27 10:28:14 +0000

    rst-lint: update 1.3.2 bottle.

--- Formula/rst-lint.rb
+++ Formula/rst-lint.rb
@@ -12,6 +12,7 @@ class RstLint < Formula
     sha256 "46d51dedf610ea9518195fb7e2f1d8a3298b4640b3ae490cfbb212ca665132b2" => :big_sur
     sha256 "7dced462fec105930e7c757faf9065d8e82cf73299440ee7a67834ba7a1d813f" => :catalina
     sha256 "3f9bcd92d9df3e9f7c6a6aeb1bc395f9bcfeb963efd597c832c438b62040b3c3" => :mojave
+    sha256 "d42c813f63119e88010e5114d7ed120a4c4b5eaa3508dec2c3f5a71da011413c" => :x86_64_linux
   end
 
   depends_on "python@3.9"
