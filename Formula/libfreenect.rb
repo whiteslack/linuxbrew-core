aec54d28320d3fe3c3343935d6836441be9774c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-29 22:26:38 +0000

    libfreenect: update 0.6.1_1 bottle.

--- Formula/libfreenect.rb
+++ Formula/libfreenect.rb
@@ -12,6 +12,7 @@ class Libfreenect < Formula
     sha256 "bc7f17d9146bf0bd83edae5ff498b2b24e4715dfe6d944d2fba9a699375aa299" => :big_sur
     sha256 "9b149ef4c0f3d06cc95e1dcf77d87d88302241173cd9ce80c49655bb08b1a21d" => :catalina
     sha256 "d4d3989b02368cc8b0b81bff675e2d8bd15a308c7087aec7d94782ab399437ae" => :mojave
+    sha256 "c49d3d455678f7b9854b55782863fb25f3653021b6e6809126cf022b589ce92c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
