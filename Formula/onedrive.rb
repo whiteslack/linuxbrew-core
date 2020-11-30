ff9c0efd5a92e0d0c6117e23f9ef4a50a00a1145
Michka Popoff <michkapopoff@gmail.com>
2020-11-30 23:09:15 +0100

    onedrive: use curl
    Closes #21683.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/onedrive.rb
+++ Formula/onedrive.rb
@@ -3,15 +3,15 @@ class Onedrive < Formula
   homepage "https://github.com/abraunegg/onedrive"
   url "https://github.com/abraunegg/onedrive/archive/v2.4.5.tar.gz"
   sha256 "1f1f5e1f2f37376b6d96bda2426552a94a8b195f545b4fb7f3668c4fe2e8f6a0"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dc05073c35937eeb6275a21587bc38b9f5030200b7231ad979ee626920e410bb" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
   depends_on "pkg-config" => :build
-  depends_on "curl-openssl"
+  depends_on "curl"
   depends_on :linux
   depends_on "sqlite"
 
