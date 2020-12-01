ae07043350afbf3a9c5bad2420cb93c19913426e
Shaun Jackman <sjackman@gmail.com>
2020-11-30 22:35:20 -0800

    hello: update 2.10 bottle for Linux. (#21684)

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -19,6 +19,7 @@ class Hello < Formula
     sha256 "54ac46b692fcca2abe77aeeca41976172a2dc24a6762f84a47fabcaba336df89" => :el_capitan
     sha256 "f81a305402e8f8b6cf11a17dac81f604b6f48d940909886a6733cf4f6a64c05f" => :yosemite
     sha256 "c80495cb6d1ad8f2c3a64c22c9dcee9d0117ca25fa6426f20a6acca275cd6c56" => :mavericks
+    sha256 "f81d7c0a3eee9fd62ebdecf685fbc5284e40d4a61c372d554fd45719e52a43cc" => :x86_64_linux
   end
 
   conflicts_with "perkeep", because: "both install `hello` binaries"
