bb35291716884cf370c927983cacc9d98d0fe8f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-03 06:41:19 +0000

    diamond: update 2.0.5 bottle.

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -10,6 +10,7 @@ class Diamond < Formula
     sha256 "b13ad0b54b2e17928c3d9a14680a8f575914b824bb2d74f1266a034276576a13" => :big_sur
     sha256 "d50080c145189391f41dbd0e8bc1ff0787825f98bea7c85e62b27424c3b25a08" => :catalina
     sha256 "5918ebadbedb3cd7809f4251488ba20ba01b65ba879052a3eac3730f0fbbefd5" => :mojave
+    sha256 "ff3b468e1b59dca44bf4a3b2ff5bfacbdf9f54295ae543950d43bb54d425f571" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
