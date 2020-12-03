dc95555e4551330e14dce190f744c56e24cd2d9c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-03 06:40:20 +0000

    bitwarden-cli: update 1.13.3 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -16,6 +16,7 @@ class BitwardenCli < Formula
     sha256 "d8d85b4f14d7b98d7ca5844741fc5009c21500724e9ce034b1ebc5298c54f667" => :big_sur
     sha256 "ef80f95534d1e0731599620dbbd06d8297a1b4c83a486b14a9c2f507313422f9" => :catalina
     sha256 "32f5ccd2ac760504711915d3291c0787a4b6b477ad44670b9c351d909a455167" => :mojave
+    sha256 "d88a522c8b4de4e8dba32896ce66a9b9d12465c0f8951a990a792a413e5757c8" => :x86_64_linux
   end
 
   depends_on "node"
