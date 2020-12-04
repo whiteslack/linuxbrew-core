31fa6ff2c5959cb4daac13f9487c1ffc212994a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-04 11:11:28 +0000

    shairport-sync: update 3.3.7 bottle.

--- Formula/shairport-sync.rb
+++ Formula/shairport-sync.rb
@@ -15,6 +15,7 @@ class ShairportSync < Formula
     sha256 "3db1d86ee0deaecd65c024f9918b60fcf9d14b19c147f186baa3ae8c97181f15" => :big_sur
     sha256 "65a5e9fb739fac564e2e4f70523fe6467cafd344db63394aea7faaf21d51dde2" => :catalina
     sha256 "b3c0527359c314d59d68533c1c158952818867ccb8a42a0a8a8c7afaabcc012e" => :mojave
+    sha256 "0f9a03c96a43e2403592b3ff5febf5f6654f789c41fa3b45ed402de39abca793" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
