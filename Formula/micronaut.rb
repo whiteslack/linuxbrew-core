06f64d53ac47b83e65ca60cde2531d5f67fd958a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-06 12:17:48 +0000

    micronaut: update 2.2.1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -15,6 +15,7 @@ class Micronaut < Formula
     sha256 "1434c2fca1c9972d2f37a2a0fa8c991a32a597ba01633d2c8c5a018f7d2790ee" => :big_sur
     sha256 "1c856d0b6ee5c23a910ddd5b149a6bb742232f35fb3d6973f2e9270c65cc9026" => :catalina
     sha256 "c99928829b56cb4277f6c3dd9aefe5b0006e815626aadd03cb41ce73789a3815" => :mojave
+    sha256 "e70b82446eb106f5c3eeca1d24a131590f9140fd60fd6acb152fd6247b4d1706" => :x86_64_linux
   end
 
   depends_on "gradle" => :build
