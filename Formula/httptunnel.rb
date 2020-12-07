6cd660a63d71019fdf3ce0e4a7f044f8a5c2d3e7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-07 18:42:12 +0000

    httptunnel: update 3.3 bottle.

--- Formula/httptunnel.rb
+++ Formula/httptunnel.rb
@@ -16,6 +16,7 @@ class Httptunnel < Formula
     sha256 "ba3194a2dd8fc577851681cbb058cd2f37b2568697c0bd8117c73e66841d6aa8" => :big_sur
     sha256 "20312eef433095612b34d5e73a3ae27d3198d3a559dddddc6a316844da8cf2c2" => :catalina
     sha256 "cd8ea90f49b98d3cbe213bfd750eb0a095d06a19a4705a9e7e08b153571c27a9" => :mojave
+    sha256 "5a8bb47734a2de17116531f6b2504d9ced1527aeccdc60d7d269195c739f3e54" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
