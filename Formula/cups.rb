1b71500070ac169ec260c5e97e8f3eb41a0b4169
Michka Popoff <michkapopoff@gmail.com>
2020-12-07 20:04:32 +0100

    cups: rebuild with gssapi (#21718)

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -4,17 +4,18 @@ class Cups < Formula
   url "https://github.com/apple/cups/releases/download/v2.3.3/cups-2.3.3-source.tar.gz"
   sha256 "261fd948bce8647b6d5cb2a1784f0c24cc52b5c4e827b71d726020bcc502f3ee"
   license "Apache-2.0"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "00bdcf0cb9e681d4e10bc7ec87eb57ccc9c63a5b76816a8545b77e454a8a0021" => :big_sur
     sha256 "1bed38f7aa9ee2c76fed5393e308cf7de5dd6d443debb0455ee7739e010b0e20" => :catalina
     sha256 "19ea09cef986e1c66f1736409f4e7917914100bb68525d8118e705500fbf28e0" => :mojave
     sha256 "0d3328bddece686cfa54e63b61f68c68fed1b1f8553cd45ed194d8a80e56f9c9" => :high_sierra
-    sha256 "0a7ab2887648d19d46f47447bb7e1545fb2ecc5438dc7df4deaa79b7fbb4f510" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
 
+  uses_from_macos "krb5"
   uses_from_macos "zlib"
 
   def install
@@ -22,8 +23,7 @@ class Cups < Formula
                           "--with-components=core",
                           "--without-bundledir",
                           "--prefix=#{prefix}",
-                          "--libdir=#{lib}",
-                          *("--disable-gssapi" unless OS.mac?)
+                          "--libdir=#{lib}"
     system "make", "install"
   end
 
