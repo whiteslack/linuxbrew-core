dc23d032a9dcebd27be134b716e3af565843dce7
Jonathan Chang <me@jonathanchang.org>
2020-12-08 20:38:37 +1100

    cups: update test (#21739)

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -11,7 +11,6 @@ class Cups < Formula
     sha256 "1bed38f7aa9ee2c76fed5393e308cf7de5dd6d443debb0455ee7739e010b0e20" => :catalina
     sha256 "19ea09cef986e1c66f1736409f4e7917914100bb68525d8118e705500fbf28e0" => :mojave
     sha256 "0d3328bddece686cfa54e63b61f68c68fed1b1f8553cd45ed194d8a80e56f9c9" => :high_sierra
-    sha256 "7d28af747e7d9a12fb409b6cdcf87878f817d39e8d715c7f8e45a0b55bc8bb19" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
@@ -29,13 +28,14 @@ class Cups < Formula
   end
 
   test do
+    port = free_port.to_s
     pid = fork do
-      exec "#{bin}/ippeveprinter", "Homebrew Test Printer"
+      exec "#{bin}/ippeveprinter", "-p", port, "Homebrew Test Printer"
     end
 
     begin
       sleep 2
-      system "#{bin}/ippfind"
+      assert_match("Homebrew Test Printer", shell_output("curl localhost:#{port}"))
     ensure
       Process.kill("TERM", pid)
       Process.wait(pid)
