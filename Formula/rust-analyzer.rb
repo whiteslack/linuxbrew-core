3a833d279c4afb47c2e9f42c64e4ad3b7b83b34b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-08 12:50:22 +0000

    rust-analyzer: update 2020-12-07 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -12,6 +12,7 @@ class RustAnalyzer < Formula
     sha256 "ea1e9c2cbcfadef88d194af359b8d426524f855eb7974212eac237ae2efdebc6" => :big_sur
     sha256 "81ec764be770e9d3781e9f27c2227766723811682304f34c5fcd43603d0e7ef7" => :catalina
     sha256 "72cb730b0b3842b199cfb65dabb0c4023e3f159a0606a6989179edfd3fe1d4e0" => :mojave
+    sha256 "36e63f86ca713686d3ca2c2b4eb7ac7fe6b63166d82803d1c9729de55ee24a63" => :x86_64_linux
   end
 
   depends_on "rust" => :build
