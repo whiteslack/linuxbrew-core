e54db958bec7f1cb4451845ec55faa86f543fb1a
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2020-12-10 17:17:32 +0000

    dopewars: update 1.6.0 bottle.

--- Formula/dopewars.rb
+++ Formula/dopewars.rb
@@ -13,6 +13,7 @@ class Dopewars < Formula
     sha256 "8d9aa3ab6faa85c24e9fa3046191b611295785a68f08de5e2d84aa59cdd55713" => :big_sur
     sha256 "0e20a6168f15c6c1245818efc5d5986b77d90d51f184a8e6b06538579d2b8461" => :catalina
     sha256 "85744f87e4866796bf4b06ad280f57542d951ec9498ebf814251448f93e24881" => :mojave
+    sha256 "ab39063c757603a74c505c7d5adade8cafa568a74f60e04c215b39985b7b1927" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
