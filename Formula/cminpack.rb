409a127699f185fe856671390d8b517743b76362
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-11 20:21:28 +0000

    cminpack: update 1.3.7 bottle.

--- Formula/cminpack.rb
+++ Formula/cminpack.rb
@@ -11,6 +11,7 @@ class Cminpack < Formula
     sha256 "86d43532f7780d59fa03678ef2c292bfd1008cb93d0a218e6f910260aa41a2b2" => :big_sur
     sha256 "5ca34e952085713f2afe3a21c8bf814bbd769aef29c4ade57d0d0545c8479b6f" => :catalina
     sha256 "ee0d22eff273b198c9392c33c14a57a53d9a7cf7332ae5019055332addd4c235" => :mojave
+    sha256 "c7b02d70a9e27bf35538013ece1a5316359e94398fab5edaacdf2e4daca248eb" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
