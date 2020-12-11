18ce0243243f8ce142ac35ab6176a7387e6af227
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-11 20:20:22 +0000

    marked: update 1.2.6 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -16,6 +16,7 @@ class Marked < Formula
     sha256 "739a746cb470f65d9f448c4dfc9c1d6b49de180c283e2095f356c54256083d02" => :big_sur
     sha256 "faefa20e213980936e1238dc1f46ffa4a6ce2559c9802c554a1e4f22dd388b9b" => :catalina
     sha256 "8181b402523e398951b90018efb048d314b3b62cf2671bae8e319d0f10ff9a3e" => :mojave
+    sha256 "8b06844cd85f56fc3a2b998fe8498c85f3ef21a174543f4fdcd7bf90265671de" => :x86_64_linux
   end
 
   depends_on "node"
