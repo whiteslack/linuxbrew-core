aa3a8ddc5f0049810a3badb7578ec6405cee413f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-12 13:40:59 +0000

    ruby-install: update 0.8.0 bottle.

--- Formula/ruby-install.rb
+++ Formula/ruby-install.rb
@@ -11,6 +11,7 @@ class RubyInstall < Formula
     sha256 "fd8726c3703f3132ef342e98d197a5d46d3adb6dc7db88a25b2e01ea2eafaeb8" => :big_sur
     sha256 "5d20763bbc1b77c06ab3615cdfb9d7df20ea8a933371d8b086146640bf806526" => :catalina
     sha256 "89972ce4292a039d6cd446756f0f64cd979081a79dcaba795e9b1b880027c37c" => :mojave
+    sha256 "20a532a3650cda8d3cf58c66c50351baaf0cafd1409d7628e98061e3394009df" => :x86_64_linux
   end
 
   def install
