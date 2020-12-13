3182797340d83fba7e34545c6960a0790fe0aa7a
Michka Popoff <michkapopoff@gmail.com>
2020-12-13 17:03:12 +0100

    at-spi2-core: add fake mac bottle block for bottle build

--- Formula/at-spi2-core.rb
+++ Formula/at-spi2-core.rb
@@ -5,6 +5,13 @@ class AtSpi2Core < Formula
   sha256 "97417b909dbbf000e7b21062a13b2f1fd52a336f5a53925bb26d27b65ace6c54"
   revision 2
 
+  bottle do
+    cellar :any
+    sha256 "5d336bd939f678b48dc1ced97ed0def383999638d80caa8cb2da780594556524" => :big_sur
+    sha256 "ce746662b98d93511b86920011b5cafcd2eecbce4c9c40d8c52a143cdf708456" => :catalina
+    sha256 "1d0c8e266acddcebeef3d9f6162d6f7fa0b193f5f71837174fb2ef0b39d324f3" => :mojave
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
   depends_on "meson" => :build
