14804386441e5670103bbf3f0be00aa2818eee92
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-14 12:40:45 +0000

    youtube-dl: update 2020.12.14 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -12,6 +12,7 @@ class YoutubeDl < Formula
     sha256 "5f2a2e7aa3c19963af8dc2339dab662a19c8d94f2a83450f18cc78e2ee0cb516" => :big_sur
     sha256 "ab8a02e2db35d53df6361de57520aa463b55563fe990f0c59e477d1e4e539ec2" => :catalina
     sha256 "55dc22a1db012a2e89fe4e82e3edfebda0693fc31cd1dc3f7891283bd74e678a" => :mojave
+    sha256 "2a863f45460abb369f230729f9fd4bbe86e38d7608bfbd953dc580371d79d69f" => :x86_64_linux
   end
 
   depends_on "python@3.9"
