79f30a12b499efab89119f95808f9e967abf4cc8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-14 17:41:08 +0000

    zig: update 0.7.1 bottle.

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -10,6 +10,7 @@ class Zig < Formula
     sha256 "f088607533abf8e77c38ad57b5c068b7c975d31d3558eda5ff9ef23221278650" => :big_sur
     sha256 "f1168c13a73d6677a8d6eb04ecd0d67e93038bd33218bafadd9aac9a23045e7d" => :catalina
     sha256 "eb2e0de16f666b740fb67529910712c517fed8ae3fd6e14d27acbbf19a41018a" => :mojave
+    sha256 "e091c662da725242839bc6aa2e8e22189e359482d0e2ea7ad55877c8c847e066" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
