977b53030b66305580d8898a71653775e2cfd3cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-17 18:06:23 +0000

    cgl: update 0.60.3 bottle.

--- Formula/cgl.rb
+++ Formula/cgl.rb
@@ -17,6 +17,7 @@ class Cgl < Formula
     sha256 "6eb179515b4cf06ad8bb484e6384f6cdae99297f523372b082ef079ed84cafd4" => :catalina
     sha256 "acdb179fb8b29973e75a674a12fa24f164b753300c625f1a4dc3ca8f3cd9dcf4" => :mojave
     sha256 "2cc6712792b9b5fec15ec479492f4da50f371ce37af53fece6bce474f93cf336" => :high_sierra
+    sha256 "0ca74ed82d94f377bbeadc70f76bfb049bbea5a1039aaf0bdab76f3a008ec473" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
