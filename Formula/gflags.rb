0f170065745757fd387ed2c5c602174a08334b1c
Michka Popoff <michkapopoff@gmail.com>
2020-12-17 18:49:22 +0100

    gflags: rebuild with gcc6 for grpc
    Closes #21859.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/gflags.rb
+++ Formula/gflags.rb
@@ -4,6 +4,7 @@ class Gflags < Formula
   url "https://github.com/gflags/gflags/archive/v2.2.2.tar.gz"
   sha256 "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf"
   license "BSD-3-Clause"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
@@ -13,10 +14,15 @@ class Gflags < Formula
     sha256 "e3176e449321b1e2070a9fabc796e6820f2f0f1f4db1c3916f58e6cdd52e510e" => :mojave
     sha256 "4beffa84f47bdfd9a1a90d9e591d9af4616db464d63046018ef0c58936d58366" => :high_sierra
     sha256 "6f06466ca55f2174daecbc935e0bca1f2aed9bfb94a92f21d52fb4db1e07cd4a" => :sierra
-    sha256 "cc58b21d997acc3a83f13d98c6211d97adb5301565ab9a2e2f2e1de4c1136aa1" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
+
+  unless OS.mac?
+    fails_with gcc: "4"
+    fails_with gcc: "5"
+    depends_on "gcc@6" => [:build, :test]
+  end
 
   def install
     mkdir "buildroot" do
@@ -50,7 +56,11 @@ class Gflags < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "-L#{lib}", "-lgflags", "test.cpp", "-o", "test"
+    if OS.mac?
+      system ENV.cxx, "-L#{lib}", "-lgflags", "test.cpp", "-o", "test"
+    else
+      system Formula["gcc@6"].bin/"c++-6", "-L#{lib}", "-lgflags", "test.cpp", "-o", "test"
+    end
     assert_match "Hello world!", shell_output("./test")
     assert_match "Foo bar!", shell_output("./test --message='Foo bar!'")
   end
