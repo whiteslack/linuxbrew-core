642087307e43d5971dae69f69bfd6ac3b98d61f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-18 16:18:53 +0000

    acpica: update 20201217 bottle.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -16,6 +16,7 @@ class Acpica < Formula
     sha256 "615e2c2548792f1620e7e0fe6e5b8232a7460b9483c66f539345b2dfbd9ff46f" => :big_sur
     sha256 "e4302dedc1720a8374d32d106fab97b4e9daf28ebacc768646b5cb62d05187ec" => :catalina
     sha256 "893420b8663ccae192b5dbe8dac8221428f17f9c862d1c3ef3e0030bb6d74c3d" => :mojave
+    sha256 "b2daeea19808c8fe06133a700900c73e4ebfe8683f662276ff5938a871bd28b3" => :x86_64_linux
   end
 
   uses_from_macos "bison" => :build
