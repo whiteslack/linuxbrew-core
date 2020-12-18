5fa4fb133722e5ed03dc142bb0ad03eaf3ce9fcd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-18 12:52:12 +0000

    mesa: update 20.3.0_2 bottle.

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -17,6 +17,7 @@ class Mesa < Formula
     sha256 "17102cdc4cb95cfbfdc154a5d0412863c22688caa4978cb1c75c02c1be66d0cc" => :big_sur
     sha256 "083ebe4e77c62675367764e71cb372eda5d03c09960acd3a56b7dbc1fd5ec5d6" => :catalina
     sha256 "e32052ed6993ee7fd970122ca2c97b9379e616fe4e74cd9987737f30811ebe3b" => :mojave
+    sha256 "0583fbf9ddf980c3261e7d8a479f50ba1ee5c50d7b1635bccc0f7e6b6ef27276" => :x86_64_linux
   end
 
   depends_on "meson" => :build
