f72c1c2e8c8a5345941c2b0af3b1be4edbdabc03
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-19 21:40:18 +0000

    gitmoji: update 3.2.16 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -16,6 +16,7 @@ class Gitmoji < Formula
     sha256 "029db9f23be973dc52e2f5928bf4c7464c64f314abf54f1942dab7b07137b811" => :big_sur
     sha256 "07010e9161a7c81d694a8d4c769451edb3ef7a84e3bb03c272c6fe0e97e2a774" => :catalina
     sha256 "cb2deb16eb9acee309cda78e5a336e29fcf99073195ed9aa9bdc244954ce45c9" => :mojave
+    sha256 "b7efaefd4b5562068ded23a6e19308119b920bd97981364febfabe7ef1aa4a1e" => :x86_64_linux
   end
 
   depends_on "node"
