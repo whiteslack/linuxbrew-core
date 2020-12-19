8432ad7ed748926a5638140df9ad2e047aedd74f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-19 21:39:56 +0000

    iozone: update 3.491 bottle.

--- Formula/iozone.rb
+++ Formula/iozone.rb
@@ -15,6 +15,7 @@ class Iozone < Formula
     sha256 "a6f0a32d9de27662d82b075d9bb59944a496c2782e5ce3e8b50228fbf48b1749" => :big_sur
     sha256 "a9865b6a1f2528acd3734a0833853a26a2b66c53c3f0e7f11be333a526c9d29d" => :catalina
     sha256 "9fd8e8232cb83eaeabc297ec4c89ec264e91fab8991d86c9aa57385f7143bf48" => :mojave
+    sha256 "a5bc9a206a9e0b0ed64005e71bc2580a133f572a20880ac7c80830c11cc4989e" => :x86_64_linux
   end
 
   def install
