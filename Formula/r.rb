8504e124105b6855a3e2dabbb49f86bb791feced
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-19 21:59:19 +0000

    r: update 4.0.3_2 bottle.

--- Formula/r.rb
+++ Formula/r.rb
@@ -15,6 +15,7 @@ class R < Formula
     sha256 "01bf5a586b3699a50b5e7ec6b7eebab213ef47dfa94754db497d0f41ac5a6110" => :big_sur
     sha256 "c600906bfe86b80c5c14129148692364a6a5c2d4c1417e7c9f8a4eff5f508ec2" => :catalina
     sha256 "22731d36544e6eb86b88e89638225133c8975377c3c4cdd1cc06e61e72f846b9" => :mojave
+    sha256 "3c7d69c0f29630d9b5c6aaace2e0981b61c00a308853a1cc9c370cc9b7cebfbc" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
