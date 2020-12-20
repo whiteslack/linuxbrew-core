2ad73bbe881d32668f964bcebba06d507aa290fd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-20 12:10:57 +0000

    faust: update 2.27.2 bottle.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -10,6 +10,7 @@ class Faust < Formula
     sha256 "6bca64d09f6bc052c71aaa01b1345294dab92dc30e7e02ec2775cf332d6eb310" => :big_sur
     sha256 "3e17a2b3203fdf2851afde34281373fb2d8fe908e224f380bb1692aac8ee7564" => :catalina
     sha256 "96f641dec558778e6e652bea11d4b446bcc7c61bebec5ec314862479b22989d7" => :mojave
+    sha256 "ff688d8d7f0dc2bdb5bd807437d4c1a17d1a7a9af4879c05d24753fc0ced15fe" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
