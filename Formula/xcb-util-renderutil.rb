24a7254b2940426333d1ead115c5acdb1411730e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-21 14:53:19 +0000

    xcb-util-renderutil: update 0.3.9 bottle.

--- Formula/xcb-util-renderutil.rb
+++ Formula/xcb-util-renderutil.rb
@@ -10,6 +10,7 @@ class XcbUtilRenderutil < Formula
     sha256 "0941200260ef409b5daa61664cad100fe69b08e99b8cb440297079387e2dadff" => :big_sur
     sha256 "5fb7ef030a443af89504e74d04fccf3000ac04bf152798e7d4242247e2378ae2" => :catalina
     sha256 "b0a2c992673650129ee49fcb4fbe6873ef4b8d29a5677ae873a27e05fc7a0d27" => :mojave
+    sha256 "2fe0a7c790b83b5ea49327a59db0af06e02cd11981f693f08c34c0f56733e3a6" => :x86_64_linux
   end
 
   head do
