fd21ce6d8a71a56cf6ef43c32484f3d2c5060976
Michka Popoff <michkapopoff@gmail.com>
2020-12-21 15:49:38 +0100

    qt: rev bump for xcb-util migration

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -8,7 +8,7 @@ class Qt < Formula
   mirror "https://mirrors.ocf.berkeley.edu/qt/archive/qt/5.15/5.15.2/single/qt-everywhere-src-5.15.2.tar.xz"
   sha256 "3a530d1b243b5dec00bc54937455471aaa3e56849d2593edb8ded07228202240"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
-  revision 5 unless OS.mac?
+  revision 6 unless OS.mac?
 
   head "https://code.qt.io/qt/qt5.git", branch: "dev", shallow: false
 
@@ -22,7 +22,6 @@ class Qt < Formula
     sha256 "ac22ab5828d894518e42f00e254f1e36d5be4e5f3f1c08b3cd49b57819daaf2d" => :big_sur
     sha256 "51ab78a99ff3498a236d15d9bed92962ddd2499c4020356469f7ab1090cf6825" => :catalina
     sha256 "25c4a693c787860b090685ac5cbeea18128d4d6361eed5b1bfed1b16ff6e4494" => :mojave
-    sha256 "746a328cc212d3d2a41ea7eee9aec3e66586483a76728351ad15f45ceff3d674" => :x86_64_linux
   end
 
   keg_only "Qt 5 has CMake issues when linked"
@@ -43,16 +42,16 @@ class Qt < Formula
     depends_on "libsm"
     depends_on "libxcomposite"
     depends_on "libdrm"
-    depends_on "linuxbrew/xorg/xcb-util-image"
-    depends_on "linuxbrew/xorg/xcb-util-renderutil"
-    depends_on "linuxbrew/xorg/xcb-util-wm"
     depends_on "mesa"
     depends_on "pulseaudio"
     depends_on "python@3.8"
     depends_on "sdl2"
     depends_on "systemd"
     depends_on "xcb-util"
+    depends_on "xcb-util-image"
     depends_on "xcb-util-keysyms"
+    depends_on "xcb-util-renderutil"
+    depends_on "xcb-util-wm"
     depends_on "zstd"
     depends_on "wayland"
   end
