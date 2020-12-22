2066730f128113c577e8e55a3a48fd650e33efea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-22 17:37:25 +0000

    torsocks: update 2.3.0 bottle.

--- Formula/torsocks.rb
+++ Formula/torsocks.rb
@@ -11,6 +11,7 @@ class Torsocks < Formula
     sha256 "2e7d1f1ed60e53086456447d408f577a8ee8d10c73ea8e01b94f7fdbaf6cd141" => :big_sur
     sha256 "0fe4e287d086c2249645781e187dd12e45526ab6ed45b32051aac2ddb840dc92" => :catalina
     sha256 "8f07df71ce2b8eee8ade8ac9c4de7c5e59acebb880b369dd3bd2d5caf5a93e8e" => :mojave
+    sha256 "9cf20688f410a38a5995f31ed3d907a34755d0e8bdb4369d642c9eddd08cecf8" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
