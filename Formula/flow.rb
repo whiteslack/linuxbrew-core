7ee986aae5ced2f02dd80041b5c3effd2ec933d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-22 20:21:57 +0000

    flow: update 0.141.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -11,6 +11,7 @@ class Flow < Formula
     sha256 "972d2d72805838e317029ead7d3f91b950a1016bd5d47c9bed93f7302b74a6f8" => :big_sur
     sha256 "5bc3b2df39c56538bdbdbf8793c6c6d994e0a6d9c9b88a9779f1f875247e89bf" => :catalina
     sha256 "3ec8c858dcf424ef827f5bc210cda9a0bd9b4a1f374ea36a2eba7f65ddd41213" => :mojave
+    sha256 "7df1f85786111c8d8559358ffd5874e4b3ee0cdce1c956c77cf336cc769e683e" => :x86_64_linux
   end
 
   depends_on "ocaml" => :build
