ca3d125ade3121bee44037af6df83cf6994e94fe
Michka Popoff <michkapopoff@gmail.com>
2020-12-22 23:35:32 +0100

    valgrind: bump revision

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -4,6 +4,7 @@ class Valgrind < Formula
   url "https://sourceware.org/pub/valgrind/valgrind-3.16.1.tar.bz2"
   sha256 "c91f3a2f7b02db0f3bc99479861656154d241d2fdb265614ba918cc6720a33ca"
   license "GPL-2.0"
+  revision 1 unless OS.mac?
 
   livecheck do
     url "https://sourceware.org/pub/valgrind/"
