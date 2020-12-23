12a00249d48e9886fc3ce4d2b1bdcc76f76436ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-23 18:17:56 +0000

    geographiclib: update 1.51 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -15,6 +15,7 @@ class Geographiclib < Formula
     sha256 "330b858cbb533864caa4324b2385399e49807ce93671be290ec6b16c09c874d6" => :big_sur
     sha256 "a8d601014c3a569b8b0ebb48e0f92055c57c2f915354a1e4030a7cda4a1afcfe" => :catalina
     sha256 "145e83f5076511c2d86ec4d8ff4de7536f909411d70b8ba81fa976f7c756d11d" => :mojave
+    sha256 "db9ac8f991a341d1d1a94c1e31754d4d4cb46d4df065c0f915b64015ecc2dd08" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
