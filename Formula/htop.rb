4dc55b4f225043fc7b05e6f094789e91b4a7aa22
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-23 10:09:40 +0000

    htop: update 3.0.4_1 bottle.

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -17,6 +17,7 @@ class Htop < Formula
     sha256 "5b426171d65d806dd20cb54ebc86bba7f3f3093242de8a3a88492a90f07c14c7" => :big_sur
     sha256 "f2d386785f45508a062701ce936e316daf8c312d73da02cb3c58ef79393e3a53" => :catalina
     sha256 "94975a5e046d3a4ee564c7a8d75355f91138f818e6782dcc8ca07e08fb18bf8e" => :mojave
+    sha256 "48bc41bfe1dab0e67a7e464ab0a39f1c7ee4f8beaf04227865bed97bea7305c7" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
