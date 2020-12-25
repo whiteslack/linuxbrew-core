81e9df6243d85800890e7919dcaf6cbc86d68314
Michka Popoff <michkapopoff@gmail.com>
2020-12-25 14:03:47 +0100

    ncmpc: Fix build for Linuxbrew
    Closes #21946.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -30,7 +30,8 @@ class Ncmpc < Formula
     depends_on "gcc" if DevelopmentTools.clang_build_version <= 800
   else
     fails_with gcc: "5"
-    depends_on "gcc@6" => :build
+    fails_with gcc: "6"
+    depends_on "gcc@7"
   end
 
   fails_with :clang do
