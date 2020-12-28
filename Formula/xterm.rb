39836c5f2024f0ec4c121bc7ef19227354a6e6c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-28 19:43:26 +0000

    xterm: update 363 bottle.

--- Formula/xterm.rb
+++ Formula/xterm.rb
@@ -16,6 +16,7 @@ class Xterm < Formula
     sha256 "d5c6fa1a6c8ad5f943f19577ca5c7da12a45248548411f7918887151dcd78656" => :arm64_big_sur
     sha256 "4331f8c3953e7dc90e11b2e900a93d0271f9b71cccf9112012df1f3df0ed17e3" => :catalina
     sha256 "db27bd9910af953635e4e041e0b86c3a127cbb6131090a2059e0e191c3e138dd" => :mojave
+    sha256 "bd4bf5147fc9a9825ca0f85dbefb944c15595e672c9faf9648988bba67983c27" => :x86_64_linux
   end
 
   depends_on "fontconfig"
