541d44b1ac95ce710751732bd26b8ff50e3a1155
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-29 10:21:14 +0000

    tectonic: update 0.4.0 bottle.

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -11,6 +11,7 @@ class Tectonic < Formula
     sha256 "07715970c795bd46c90032cbc9698f14b125b8a6d8e710408b8ae02f08c22f6c" => :arm64_big_sur
     sha256 "22b064c27271ef786660c3269f867f2540dd72ae3e7868a72b4efa9dbea727c0" => :catalina
     sha256 "9a80916a07d052d35f48ac6ed2600dfa240d1bf16ab89fb30cf99dba671889b0" => :mojave
+    sha256 "d8b2829580e3f4b32c957abe7318ed3b38e2f23c555b5185085a15c6280428f4" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
