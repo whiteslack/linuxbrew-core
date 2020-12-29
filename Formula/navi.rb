94e09020f344980a2a20cc44d7e9d8011a05097a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-29 14:48:42 +0000

    navi: update 2.13.1 bottle.

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -12,6 +12,7 @@ class Navi < Formula
     sha256 "7affb1cf4c22b2b68444a0db788c445c2b8c6d8dfea17d57adf65db1789af517" => :arm64_big_sur
     sha256 "4932e2fd37997f92a27a437d845220c9889cfbc432e200a269e286ec893b8394" => :catalina
     sha256 "f9b398bd630b0abdd40de61ca56048de57fc7fbafb1c58e0aef5002bd1e06b89" => :mojave
+    sha256 "63172dc722f35ece5082460f81667bdf7bec41c322463bdadc86a41cb22c610b" => :x86_64_linux
   end
 
   depends_on "rust" => :build
