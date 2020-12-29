283f3580de8098c7a5cbd980203aba41fb4b7b22
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-29 14:41:29 +0000

    mpop: update 1.4.12 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -11,6 +11,7 @@ class Mpop < Formula
     sha256 "3aaad5f552e6c7b1dc2826e17fde83b873f63d540607a83edf8f0d9466dd4be3" => :arm64_big_sur
     sha256 "8fa53695d0f74cb8575a6eb1b3582ef2e06814c1540c8c3ad20be5f9667f617e" => :catalina
     sha256 "8c30103713ed7538025b297aaf50fd072dfdfe3fe803ee0712f8d06dddb094f3" => :mojave
+    sha256 "12b498c3d2042619f8287fb0e8b757096859dd6c917cefa7fd35dd004444d19f" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
