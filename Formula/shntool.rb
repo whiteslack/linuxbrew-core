a60bd60a89e9c03e54ffe2f0d30fd06e9ab683b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-29 13:24:55 +0000

    shntool: update 3.0.10 bottle.

--- Formula/shntool.rb
+++ Formula/shntool.rb
@@ -13,6 +13,7 @@ class Shntool < Formula
     sha256 "1dfa65178720559cebc5500eb9f32d4ca2606a4f1b6a94b9d175ceded8fae2f0" => :arm64_big_sur
     sha256 "e140337ce89f886c0044ac6eaf75dda3711622f9da418932e8a02337213785ca" => :catalina
     sha256 "8f318573fa965da7dc5fcff667f2f9ee3295e2034a6c877a9d182459f08308f8" => :mojave
+    sha256 "4f55845d173d30eec943802a1c1e40da1d233f4a2a5620c33385fef9cac32b8d" => :x86_64_linux
   end
 
   def install
