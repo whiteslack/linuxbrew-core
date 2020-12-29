4ded1d8d7404d5ee6caaf6cab725d8d9a74a7cbe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-29 06:35:25 +0000

    mariadb@10.3: update 10.3.27 bottle.

--- Formula/mariadb@10.3.rb
+++ Formula/mariadb@10.3.rb
@@ -15,6 +15,7 @@ class MariadbAT103 < Formula
     sha256 "61e47efd4d5bca270f249a6c08aae649485af3cf8feb1dcbd311816a5e78934d" => :big_sur
     sha256 "bd1202d5fda64ede587a9b1034dc1f6a71b1dcd8e73fa282aaae9915f16bf98c" => :catalina
     sha256 "4a51b97b37f8e073823c86b2832acacd4a8b770f234a817a967e381897405e67" => :mojave
+    sha256 "b6a5a954257d696212c434e3551f4932ce20a81913216f7f281a18329abf04b6" => :x86_64_linux
   end
 
   keg_only :versioned_formula
