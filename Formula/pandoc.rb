9b1bea650f45789442de430bc56416799c2bf073
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-12-30 10:00:09 +0000

    pandoc: update 2.11.3.2 bottle.

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -15,6 +15,7 @@ class Pandoc < Formula
     sha256 "a28331398b1a4f5d9bb7d69ad26873a631ac1e7fd21340557203c753d2cb9ebd" => :big_sur
     sha256 "659efbfcc719ee51465ac4f5045eb2f4b8aff89e4eebdeb07663b4cbd2deeecf" => :catalina
     sha256 "fc024d2d3d8aba91bd4e7c5f0650e09080086c380fadee566ec7c3889b5909c3" => :mojave
+    sha256 "1491f2123fc9fe5afa29aa21628bda042601e67b37e6741a594829350cda767e" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
